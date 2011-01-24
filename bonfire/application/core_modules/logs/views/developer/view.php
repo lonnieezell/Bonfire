6a49954326775bfc34c8656ff373c9e6dcd5d9d3
lonnieezell <lonnieje@gmail.com>
2011-01-24 15:30:57 -0600

    Added filtering to log file display

--- bonfire/application/core_modules/logs/views/developer/view.php
+++ bonfire/application/core_modules/logs/views/developer/view.php
@@ -3,30 +3,65 @@
 
 <h2>Viewing: <?php echo $log_file ?></h2>
 
-<br/>
-
 <?php if (!isset($log_content) || empty($log_content)) : ?>
 <div class="notification attention">
 	<p>Either the log file could not be located, or it was empty.</p>
 </div>
 <?php else : ?>
 
-	<?php foreach ($log_content as $row) : ?>
-	<?php 
-		$class = '';
-		
-		if (strpos($row, 'ERROR') !== false)
-		{
-			$class="error";
-		} else
-		if (strpos($row, 'DEBUG') !== false)
-		{
-			$class="attention";
-		}		
-	?>
-	<div style="border-bottom: 1px solid #ccc; color: #222;" <?php echo 'class="'. $class .'"' ?>>
-		<?php echo $row; ?>
+	<p>View &nbsp;&nbsp;
+		<select id="filter">
+			<option value="all">All entries</option>
+			<option value="error">Errors only</option>
+		</select>
+	</p>
+
+	<div id="log">
+		<?php foreach ($log_content as $row) : ?>
+		<?php 
+			$class = '';
+			
+			if (strpos($row, 'ERROR') !== false)
+			{
+				$class="error";
+			} else
+			if (strpos($row, 'DEBUG') !== false)
+			{
+				$class="attention";
+			}		
+		?>
+		<div style="border-bottom: 1px solid #999; padding: 5px 10px; color: #222;" <?php echo 'class="'. $class .'"' ?>>
+			<?php echo $row; ?>
+		</div>
+		<?php endforeach; ?>
 	</div>
-	<?php endforeach; ?>
 
 <?php endif; ?>
+
+<script>
+head.ready(function(){
+	// Filter Hook
+	$('#filter').change(function(){
+		// Are we filtering at all? 
+		var filter = $(this).val();
+	
+		$('#log div').each(function(){
+		
+			switch (filter)
+			{
+				case 'all':
+					$(this).css('display', 'block');
+					break;
+				case 'error':
+					if ($(this).hasClass('error'))
+					{
+						$(this).css('display', 'block');
+					} else
+					{
+						$(this).css('display', 'none');
+					}
+			}
+		});
+	});
+});
+</script>
