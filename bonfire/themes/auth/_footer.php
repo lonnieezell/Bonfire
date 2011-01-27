d0abbf8aebeeca3b99f7d1e55e5fb036ee671cb8
lonnieezell <lonnieje@gmail.com>
2011-01-27 09:23:15 -0600

    Updates to login page style. Now properly records IP address on login

---
+++ bonfire/themes/auth/_footer.php
@@ -0,0 +1,9 @@
+	<div class="foot">
+		<?php echo anchor('forgot_password', 'Forgot your Password?'); ?>
+	</div>
+
+	<script>
+	head.js("<?php echo base_url() .'assets/js/jquery-1.4.4.min.js'; ?>");
+	</script>
+</body>
+</html>
