82fe87af196a6940c2a1ca0d57c16c917e49640f
lonnieezell <lonnieje@gmail.com>
2011-01-31 23:27:34 -0600

    Got a start on the documentation

---
+++ bonfire/application/core_modules/user_guide/views/general/contribute.php
@@ -0,0 +1,5 @@
+<h1>Contribute</h1>
+
+<p>If you find this software helpful and would like too see it continue to grow, please consider donating your time, energy and/or expertise by <a href="mailto:info@cibonfire.com">sending me a message</a> and letting me know how you'd like to help out, or just sending your suggestions.</p>
+
+<p>If you like to get down and dirty with the code, you can fork the project on <a href="https://github.com/lonnieezell/bonfire" target="_blank">GitHub</a>, do some work, and shoot back a pull request.</p>
---
+++ bonfire/themes/user_guide/_header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	
+	<title>Bonfire User Guide</title>
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo Template::theme_url('screen.css') ?>" />
+	
+	<meta http-equiv='expires' content='-1' /> 
+	<meta http-equiv= 'pragma' content='no-cache' /> 
+	<meta name='robots' content='all' /> 
+</head>
+</html>
+<body>
+
+	<!-- START NAVIGATION --> 
+	<div id="nav"><div id="nav_inner"></div></div> 
+	<div id="nav2"><a name="top">&nbsp;</a></div> 
+	<div id="masthead"> 
+		<table cellpadding="0" cellspacing="0" border="0" style="width:100%"> 
+			<tr> 
+				<td><h1>Bonfire User Guide Version <?php echo BONFIRE_VERSION ?></h1></td> 
+			</tr> 
+		</table> 
+	</div> 
+	<!-- END NAVIGATION --> 
+	 
+	 
+	<!-- START BREADCRUMB --> 
+	<table cellpadding="0" cellspacing="0" border="0" style="width:100%"> 
+	<tr> 
+		<td id="breadcrumb"> 
+			<a href="http://cibonfire.com/">Bonfire Home</a> &nbsp;&#8250;&nbsp;
+			<a href="<?php echo site_url('user_guide') ?>">User Guide Home</a> &nbsp;&#8250;&nbsp;
+			<?php echo isset($page_title) ? $page_title : '' ?>
+		</td> 
+		<td id="searchbox"><form method="get" action="http://www.google.com/search"><input type="hidden" name="as_sitesearch" id="as_sitesearch" value="cibonfire.com/user_guide/" />Search User Guide&nbsp; <input type="text" class="input" style="width:200px;" name="q" id="q" size="31" maxlength="255" value="" />&nbsp;<input type="submit" class="submit" name="sa" value="Go" /></form>
+		</td> 
+	</tr> 
+	</table> 
+	<!-- END BREADCRUMB --> 
+	 
+	<br clear="all" /> 
+
+	<div id="content">
+	
