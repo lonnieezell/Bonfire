0907a2632fefd3138735b99a979c0c4bd6ed65ac
lonnieezell <lonnieje@gmail.com>
2011-02-02 22:48:47 -0600

    Updated migrations to actually allow migrating a database now, though it doens't end pretty. Also updated to latest CI 2.0 code.

--- bonfire/application/core_modules/migrations/controllers/developer.php
+++ bonfire/application/core_modules/migrations/controllers/developer.php
@@ -26,7 +26,32 @@ class Developer extends Admin_Controller {
 	
 	//--------------------------------------------------------------------
 	
-
+	public function migrate_to($version=0) 
+	{
+		if ($this->input->post('submit') == 'Migrate Database')
+		{
+			$result = $this->migrations->version($version);
+			
+			if ($result)
+			{
+				Template::set_message('Successfully migrated database to version '. $result, 'success');
+			} else 
+			{
+				Template::set_message('There was an error migrating the database.', 'error');
+			}
+			redirect('admin/developer/migrations');
+		}
+	
+		Template::set('installed_version', $this->migrations->get_schema_version());
+		Template::set('latest_version', $this->migrations->get_latest_version());
+		Template::set('migrations', $this->migrations->get_available_versions());
+	
+		Template::set('toolbar_title', 'Database Migrations');
+		Template::render();
+	}
+	
+	//--------------------------------------------------------------------
+	
 }
 
 // End Migrations Developer Class
