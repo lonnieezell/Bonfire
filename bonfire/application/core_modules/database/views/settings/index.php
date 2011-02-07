bddf96f667782e999dd8b1a89ec5bb6238905b55
lonnieezell <lonnieje@gmail.com>
2011-02-07 13:16:12 -0600

    Added at-a-glance into to database settings index

--- bonfire/application/core_modules/database/views/settings/index.php
+++ bonfire/application/core_modules/database/views/settings/index.php
@@ -10,19 +10,25 @@
 				<div class="list-item" data-id="dev">
 					<img src="<?php echo Template::theme_url('images/database.png') ?>" />
 					
-					<p><b>Development</b></p>
+					<p><b>Development</b><br/>
+					<?php echo $settings['dev']['hostname'] .'/'. $settings['dev']['database'] ?>
+					</p>
 				</div>
 				
 				<div class="list-item" data-id="test">
 					<img src="<?php echo Template::theme_url('images/database.png') ?>" />
 					
-					<p><b>Staging/Test</b></p>
+					<p><b>Staging/Test</b><br/>
+					<?php echo $settings['test']['hostname'] .'/'. $settings['test']['database'] ?>
+					</p>
 				</div>
 				
 				<div class="list-item" data-id="prod">
 					<img src="<?php echo Template::theme_url('images/database.png') ?>" />
 					
-					<p><b>Production</b></p>
+					<p><b>Production</b><br/>
+					<?php echo $settings['prod']['hostname'] .'/'. $settings['prod']['database'] ?>
+					</p>
 				</div>
 			
 			</div>
@@ -37,6 +43,57 @@
 			
 				<div class="notification information">
 					<p>You are currently running on the <b><?php echo ENVIRONMENT ?></b> server.</p>
+				</div>
+				
+				<br />
+				
+				<h2>Databases At-a-glance</h2>
+				
+				<div class="row">
+					<div class="column size1of3">
+						<h3>Development</h3>
+						
+						<ul>
+							<li>Hostname: <b><?php echo $settings['dev']['hostname'] ?></b></li>
+							<li>Database: <b><?php echo $settings['dev']['database'] ?></b></li>
+							<li>User: <b><?php echo $settings['dev']['username'] ?></b></li>
+							<li>Driver: <b><?php echo $settings['dev']['dbdriver'] ?></b></li>
+							<li>Prefix: <b><?php echo $settings['dev']['dbprefix'] ?></b></li>	
+							<li>Debug On?: <b><?php echo $settings['dev']['db_debug'] == 1 ? 'Yes' : 'No' ?></b></li>
+							<li>Persistent Connection: <b><?php echo $settings['dev']['pconnect'] == 1 ? 'Yes' : 'No' ?></b></li>	
+							<li>Strict Mode?: <b><?php echo $settings['dev']['stricton'] = 1 ? 'Yes' : 'No' ?></b></li>
+						</ul>
+					</div>
+					
+					<div class="column size1of3">
+						<h3>Staging</h3>
+						
+						<ul>
+							<li>Hostname: <b><?php echo $settings['test']['hostname'] ?></b></li>
+							<li>Database: <b><?php echo $settings['test']['database'] ?></b></li>
+							<li>User: <b><?php echo $settings['test']['username'] ?></b></li>
+							<li>Driver: <b><?php echo $settings['test']['dbdriver'] ?></b></li>
+							<li>Prefix: <b><?php echo $settings['test']['dbprefix'] ?></b></li>	
+							<li>Debug On?: <b><?php echo $settings['test']['db_debug'] == 1 ? 'Yes' : 'No' ?></b></li>
+							<li>Persistent Connection: <b><?php echo $settings['test']['pconnect'] == 1 ? 'Yes' : 'No' ?></b></li>	
+							<li>Strict Mode?: <b><?php echo $settings['test']['stricton'] = 1 ? 'Yes' : 'No' ?></b></li>
+						</ul>
+					</div>
+					
+					<div class="column size1of3">
+						<h3>Development</h3>
+						
+						<ul>
+							<li>Hostname: <b><?php echo $settings['prod']['hostname'] ?></b></li>
+							<li>Database: <b><?php echo $settings['prod']['database'] ?></b></li>
+							<li>User: <b><?php echo $settings['prod']['username'] ?></b></li>
+							<li>Driver: <b><?php echo $settings['prod']['dbdriver'] ?></b></li>
+							<li>Prefix: <b><?php echo $settings['prod']['dbprefix'] ?></b></li>	
+							<li>Debug On?: <b><?php echo $settings['prod']['db_debug'] == 1 ? 'Yes' : 'No' ?></b></li>
+							<li>Persistent Connection: <b><?php echo $settings['prod']['pconnect'] == 1 ? 'Yes' : 'No' ?></b></li>	
+							<li>Strict Mode?: <b><?php echo $settings['prod']['stricton'] = 1 ? 'Yes' : 'No' ?></b></li>
+						</ul>
+					</div>
 				</div>
 				
 			</div>	<!-- /inner -->
