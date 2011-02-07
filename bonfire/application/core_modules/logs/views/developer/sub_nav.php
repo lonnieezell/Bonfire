74bea5c235d97967776743808323a82b614473a5
lonnieezell <lonnieje@gmail.com>
2011-02-07 08:24:37 -0600

    Misc. small changes

--- bonfire/application/core_modules/logs/views/developer/sub_nav.php
+++
@@ -1,4 +0,0 @@
-<div class="subnav rounded-5">
-	<a href="<?php echo site_url('admin/developer/logs') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> >Logs</a>
-	<a href="<?php echo site_url('admin/developer/logs/enable') ?>" <?php echo $this->uri->segment(4) == 'enable' ? 'class="current"' : '' ?> >Threshold</a>
-</div>
--- bonfire/application/logs/log-2011-01-25.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2011-01-25 07:36:20 --> 404 Page Not Found --> content/index
-ERROR - 2011-01-25 07:37:11 --> 404 Page Not Found --> content/index
-ERROR - 2011-01-25 07:49:58 --> 404 Page Not Found --> content/create
