3c53d0fd66ce46873dfe4edd9e558c2b08987464
lonnieezell <lonnieje@gmail.com>
2011-02-08 10:28:05 -0600

    Documented classes in MY_Controller

--- bonfire/application/core/MY_Controller.php
+++ bonfire/application/core/MY_Controller.php
@@ -1,5 +1,20 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+	Class: Base_Controller
+	
+	This controller provides a controller that your controllers can extend
+	from. This allows any tasks that need to be performed sitewide to be 
+	done in one place.
+	
+	Since it extends from MX_Controller, any controller in the system
+	can be used in the HMVC style, using modules::run(). See the docs 
+	at: https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc/wiki/Home
+	for more detail on the HMVC code used in Bonfire.
+	
+	Extends:
+		MX_Controller
+*/
 class Base_Controller extends MX_Controller {
 	
 	protected $previous_page;
@@ -22,6 +37,15 @@ class Base_Controller extends MX_Controller {
 
 //--------------------------------------------------------------------
 
+/*
+	Class: Front_Controller
+	
+	This class provides a common place to handle any tasks that need to
+	be done for all public-facing controllers.
+	
+	Extends:
+		Base_Controller
+*/
 class Front_Controller extends Base_Controller {
 	
 	//--------------------------------------------------------------------
@@ -38,6 +62,18 @@ class Front_Controller extends Base_Controller {
 // End Front_Controller class
 
 //--------------------------------------------------------------------
+
+/*
+	Class: Admin_Controller
+	
+	This class provides a base class for all admin-facing controllers. 
+	It automatically loads the form, form_validation and pagination
+	helpers/libraries, sets defaults for pagination and sets our 
+	Admin Theme.
+	
+	Extends:
+		Base_controller	
+*/
 
 class Admin_Controller extends Base_Controller {
 	
