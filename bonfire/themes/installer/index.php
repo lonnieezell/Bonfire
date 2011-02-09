8b3f193509a87b00be51fdf994d6daf55d7820e1
lonnieezell <lonnieje@gmail.com>
2011-02-09 10:01:06 -0600

    Welcome page created and styled for installer.

---
+++ bonfire/themes/installer/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	
+	<title>Bonfire Installer</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Template::theme_url('screen.css') ?>" />
+
+	<script src="/assets/js/jquery-1.5.min.js"></script>	
+</head>
+<body>
+
+	<div class="page">
+	
+		<!-- Header -->
+		<div class="head text-right">
+			<h1>Bonfire</h1>
+		</div>
+		
+		<div class="main">
+			<?php echo Template::message(); ?>
+			<?php echo Template::yield(); ?>
+
+		</div>	<!-- /main -->
+	</div>	<!-- /page -->
+	
+	<div class="foot">
+		<p>Powered by <strong>Bonfire</strong>.</p>
+	</div>
+</body>
+</html>
