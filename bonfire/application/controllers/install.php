87777afcf200792523ba4109f4c6a75c11138081
lonnieezell <lonnieje@gmail.com>
2011-02-09 10:01:06 -0600

    Welcome page created and styled for installer.

--- bonfire/application/controllers/install.php
+++ bonfire/application/controllers/install.php
@@ -7,13 +7,29 @@ class Install extends Base_Controller {
 		parent::__construct();
 		
 		Template::set_theme('installer');
+		
+		$this->load->helper('form');
+		$this->load->library('form_validation');
 	}
 	
 	//--------------------------------------------------------------------
 	
-
 	public function index() 
 	{
+		if ($this->input->post('submit'))
+		{
+			$this->form_validation->set_rules('site_title', 'Site Title', 'required|trim|strip_tags|min_length[1]|xss_clean');
+			$this->form_validation->set_rules('username', 'Username', 'required|trim|strip_tags|xss_clean');
+			$this->form_validation->set_rules('password', 'Password', 'required|trim|strip_tags|alpha_dash|min_length[8]|xss_clean');
+			$this->form_validation->set_rules('pass_confirm', 'Password (again)', 'required|trim|matches[password]');
+			$this->form_validation->set_rules('email', 'Email', 'required|trim|strip_tags|valid_email|xss_clean');
+			
+			if ($this->form_validation->run() !== false)
+			{
+			
+			}
+		}
+	
 		Template::render();
 	}
 	
