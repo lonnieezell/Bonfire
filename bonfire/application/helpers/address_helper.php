f13a3bc6a9bfd57179e2695112b12bc013323c12
lonnieezell <lonnieje@gmail.com>
2011-02-10 00:19:03 -0600

    Documented the address_helper file.

--- bonfire/application/helpers/address_helper.php
+++ bonfire/application/helpers/address_helper.php
@@ -1,5 +1,24 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+	File: Address Helper
+	
+	Provides various helper functions when working with address in forms.
+*/
+
+
+/*
+	Function: state_select()
+	
+	Creates a US-based state dropdown form input.
+	
+	Parameters:
+		$selected_id	- The value of the item that should be selected when the dropdown is drawn.
+		$default_abbr	- The value of the item that should be selected if no other matches are found.
+		
+	Return:
+		A string withe the full html for the select input. 
+*/
 if (!function_exists('state_select'))
 {
 
