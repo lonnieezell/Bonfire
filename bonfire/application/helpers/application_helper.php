65b1a1e8fc8d32c0e0432703756e752f81495c6b
lonnieezell <lonnieje@gmail.com>
2011-02-10 00:30:08 -0600

    Added docs for address and application helpers.

--- bonfire/application/helpers/application_helper.php
+++ bonfire/application/helpers/application_helper.php
@@ -1,6 +1,8 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
+	File: Application Helper
+
 	Pulls together various helper functions from across the core modules
 	to ease editing and minimize physical files that need loaded.
 */
@@ -11,11 +13,16 @@
 // !USER HELPERS
 //--------------------------------------------------------------------
 
-/** 
- * A convenient shorthand for checking user permissions.
- * 
- * @param	string	$permission	- The permission to check for, ie 'Site.Signin.Allow'
- * @return	true/false
+/*
+	Function: has_permission()
+	
+	A convenient shorthand for checking user permissions.
+	
+	Parameters:
+		$permission	- The permission to check for, ie 'Site.Signin.Allow'
+	
+	Return:
+		true/false
  */ 
 function has_permission($permission=null)
 {
@@ -31,10 +38,26 @@ function has_permission($permission=null)
 
 //--------------------------------------------------------------------
 
-/**
- * Creates an image link based on Gravatar for the specified user.
- * It will default to the site's generic image if none is found for
- * the user.
+/*
+	Function: gravatar_link()
+
+	Creates an image link based on Gravatar for the specified email address.
+	It will default to the site's generic image if none is found for
+	the user.
+	
+	Note that if gravatar does not have an image that matches the criteria,
+	it will return a link to an image under *your_theme/images/user.png*.
+	
+	Parameters:
+		$email	- The email address to check for.
+		$size	- The width (and height) of the resulting image to grab.
+		$alt	- Alt text to be put in the link tag.
+		$title	- The title text to be put in the link tag.
+		$class	- Any class(es) that should be assigned to the link tag.
+		$id		- The id (if any) that shoudl put in the link tag.
+		
+	Return:
+		The resulting image tag.
  */
 function gravatar_link($email=null, $size=48, $alt='', $title='', $class='', $id='') 
 {
@@ -69,12 +92,12 @@ function gravatar_link($email=null, $size=48, $alt='', $title='', $class='', $id
 // !MODULE HELPERS
 //--------------------------------------------------------------------
 
-/**
- * This helper provides tools for working with modules, using
- * the Modular Separation HMVC modules library.
- *
- */
+/*
+	Function: module_folders();
 
+	Returns an array of the folders that modules are allowed to be stored in. 
+	These are set in *bonfire/application/third_party/MX/Modules.php*.
+*/
 function module_folders()
 {
 	return array_keys(modules::$locations);
@@ -82,9 +105,11 @@ function module_folders()
 
 //--------------------------------------------------------------------
 
-/** 
- * Returns a list of all modules in the system.
- */
+/*
+	Function: module_list()
+ 
+	Returns a list of all modules in the system.
+*/
 function module_list()
 {
 	if (!function_exists('directory_map'))
@@ -105,11 +130,17 @@ function module_list()
 
 //--------------------------------------------------------------------
 
-/**
- * Determines whether a controller exists for a module.
- *
- * @param	string	$controller	The name of the controller to looke for (without the .php)
- * @param	string	$module		The name of module to look in.
+/*
+	Function: module_controller_exists()
+
+	Determines whether a controller exists for a module.
+	
+	Parameters:
+	$controller	- The name of the controller to looke for (without the .php)
+	$module		- The name of module to look in.
+	
+	Return: 
+		true/false
  */
 function module_controller_exists($controller=null, $module=null)
 {
@@ -132,9 +163,9 @@ function module_controller_exists($controller=null, $module=null)
 
 //--------------------------------------------------------------------
 
-/**
- * Returns the path to the module's icon.
- */
+/*
+	Returns the path to the module's icon.
+*/
 function module_icon($module=null)
 {
 	if (empty($module))
