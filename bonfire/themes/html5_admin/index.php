734e7a6c7f638cb5d6cab5d6991d05cf1519f84a
lonnieezell <lonnieje@gmail.com>
2011-02-16 08:19:42 -0600

    Fixed head.js script loading error in admin theme

--- bonfire/themes/html5_admin/index.php
+++ bonfire/themes/html5_admin/index.php
@@ -69,11 +69,11 @@
 	</div>	<!-- /page -->
 
 	<script>
-		head.js(	<?php echo site_url('assets/js/jquery-1.5.min.js'); ?>,
-					<?php echo Template::theme_url('js/jquery.form.js'); ?>,
-					<?php echo Template::theme_url('js/common.js'); ?>,
-					<?php echo Template::theme_url('js/jquery.ajaxify.min.js'); ?>,
-					<?php echo Template::theme_url('js/ui.js'); ?>
+		head.js(	"<?php echo site_url('assets/js/jquery-1.5.min.js'); ?>",
+					"<?php echo Template::theme_url('js/jquery.form.js'); ?>",
+					"<?php echo Template::theme_url('js/common.js'); ?>",
+					"<?php echo Template::theme_url('js/jquery.ajaxify.min.js'); ?>",
+					"<?php echo Template::theme_url('js/ui.js'); ?>"
 		);
 	</script>
 
