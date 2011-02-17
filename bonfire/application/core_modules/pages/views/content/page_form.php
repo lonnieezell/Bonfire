bb1db39c5bf375380539922b2bf31d34a24e6ab4
lonnieezell <lonnieje@gmail.com>
2011-02-17 00:05:56 -0600

    New pages set their defaults from the application config.

--- bonfire/application/core_modules/pages/views/content/page_form.php
+++ bonfire/application/core_modules/pages/views/content/page_form.php
@@ -33,18 +33,18 @@
 			<!-- Rich Text? -->
 			<div>
 				<label>Rich Text?</label>
-				<input type="checkbox" name="rich_text" value="1" <?php echo isset($page) && $page->rich_text == '1' ? 'checked="checked"' : set_checkbox('rich_text', '1'); ?> />
+				<input type="checkbox" name="rich_text" value="1" <?php echo (isset($page) && $page->rich_text == '1') || config_item('pages.default_rich_text') == '1' ? 'checked="checked"' : set_checkbox('rich_text', '1'); ?> />
 			</div>
 			
 			<!-- Searchable? -->
 			<div>
 				<label>Searchable?</label>
-				<input type="checkbox" name="searchable" value="1" <?php echo isset($page) && $page->searchable == '1' ? 'checked="checked"' : set_checkbox('searchable', '1'); ?> />
+				<input type="checkbox" name="searchable" value="1" <?php echo (isset($page) && $page->searchable == '1') || config_item('pages.default_searchable') ? 'checked="checked"' : set_checkbox('searchable', '1'); ?> />
 			</div>
 			<!-- Cachable? -->
 			<div>
 				<label>Cacheable?</label>
-				<input type="checkbox" name="cacheable" value="1" <?php echo isset($page) && $page->cacheable == '1' ? 'checked="checked"' : set_checkbox('cacheable', '1'); ?> />
+				<input type="checkbox" name="cacheable" value="1" <?php echo (isset($page) && $page->cacheable == '1') || config_item('pages.default_cacheable') ? 'checked="checked"' : set_checkbox('cacheable', '1'); ?> />
 			</div>
 			<!-- Deleted? -->
 			<div>
