b0b30c03b3178dbc2a30779eca0badfa28f30e74
lonnieezell <lonnieje@gmail.com>
2011-02-28 00:15:37 -0600

    Tweaked default theme a little. Granted, stolen from Wordpress' 2010, so will have to be changed before release.

--- bonfire/themes/default/_footer.php
+++ bonfire/themes/default/_footer.php
@@ -1,11 +1,26 @@
 		</div>	<!-- /main -->
-	</div>	<!-- /page -->
 	
-	<div class="foot">
-		<p>Powered by <strong>Bonfire</strong>.</p>
-		<?php if (ENVIRONMENT == 'dev') : ?>
-			<p>Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
-		<?php endif; ?>
-	</div>
+		<div class="foot">
+		
+			<div class="align-right">
+				<?php if ($this->auth->is_logged_in()) : ?>
+					<?php echo anchor('logout','Logout'); ?>
+				<?php else : ?>
+					<?php echo anchor('login','Login'); ?>
+				<?php endif; ?>
+			</div>
+		
+			<div class="align-left">
+				<p>Powered by <strong>Bonfire</strong>.</p>
+				<?php if (ENVIRONMENT == 'dev') : ?>
+					<p class="small">Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
+				<?php endif; ?>
+			</div>
+			
+		</div>
+		
+		<div class="clearfix"></div>
+	
+	</div>	<!-- /page -->
 </body>
 </html>
