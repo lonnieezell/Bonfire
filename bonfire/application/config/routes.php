121572bc0898a2e14d6d811557792c2ea23259c1
lonnieezell <lonnieje@gmail.com>
2011-02-27 23:52:29 -0600

    Basic page display for front-end in place. Checks pub-date but shows if you have manage permissions. Only works with root aliases for now.

--- bonfire/application/config/routes.php
+++ bonfire/application/config/routes.php
@@ -39,7 +39,7 @@
 */
 
 $route['default_controller'] = "home";
-$route['404_override'] = '';
+$route['404_override'] = 'pages';
 
 // Authorization
 $route['login']				= 'users/login';
--- bonfire/themes/default/index.php
+++ bonfire/themes/default/index.php
@@ -1,5 +1,5 @@
 <?php echo theme_view('_header'); ?>
 
-<?php echo Template::yield(); ?>
+<?php echo isset($body_content) ? $body_content : Template::yield(); ?>
 
 <?php echo theme_view('_footer'); ?>
