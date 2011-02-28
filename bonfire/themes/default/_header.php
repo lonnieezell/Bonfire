7219ae41c6a22595d6493478b42786176c8708f6
lonnieezell <lonnieje@gmail.com>
2011-02-28 00:15:37 -0600

    Tweaked default theme a little. Granted, stolen from Wordpress' 2010, so will have to be changed before release.

--- bonfire/themes/default/_header.php
+++ bonfire/themes/default/_header.php
@@ -3,24 +3,17 @@
 <head>
 	<meta charset="UTF-8" />
 	
-	<title>Crust.Me Administration</title>
+	<title><?php echo config_item('site.title') ?></title>
 	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url() .'assets/css/front.css' ?>" />
+	<?php Assets::css(); ?>
 	
 	<script src="<?php echo base_url() .'assets/js/head.min.js'; ?>"></script>
 </head>
 <body>
 
 	<div class="body">
-	
-		<!-- Header -->
-		<div class="head text-right">
-			<?php if ($this->auth->is_logged_in()) : ?>
-				<?php echo anchor('logout','Logout'); ?>
-			<?php else : ?>
-				<?php echo anchor('login','Login'); ?>
-			<?php endif; ?>
-		</div>
+		
+		<h1 id="site-title"><?php echo config_item('site.title') ?></h1>
 
 		<?php echo Template::message(); ?>
 		
