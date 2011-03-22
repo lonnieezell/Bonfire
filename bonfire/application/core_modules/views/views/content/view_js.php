e55da140f79ae194222ef14e2fc0276886a015f1
lonnieezell <lonnieje@gmail.com>
2011-03-21 23:06:19 -0500

    Removed View module after rethinking what the core of Bonfire really is. Editing pages/views is not it.

--- bonfire/application/core_modules/views/views/content/view_js.php
+++
@@ -1,7 +0,0 @@
-$.subscribe('list-view/list-item/click', function(id_str) {
-	var parts = id_str.split(':');
-	
-	var url = '<?php echo site_url('admin/content/views/edit/') ?>/'+ parts[0] +'/'+ parts[1];
-	
-	$('#content').load(url);
-});
