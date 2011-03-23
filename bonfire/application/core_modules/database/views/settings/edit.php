8f6593199750ab0d44450ea01b9d0f8f1e0ccfee
lonnieezell <lonnieje@gmail.com>
2011-03-22 23:43:47 -0500

    Datbase settings updated to work with CI 2.0 environments.

--- bonfire/application/core_modules/database/views/settings/edit.php
+++ bonfire/application/core_modules/database/views/settings/edit.php
@@ -6,19 +6,19 @@
 	
 	<div>
 		<label for="hostname">Hostname</label>
-		<input type="text" name="hostname" id="hostname" value="<?= isset($db_settings['hostname']) ? $db_settings['hostname'] : ''; ?>" />
+		<input type="text" name="hostname" id="hostname" value="<?= isset($db_settings['default']['hostname']) ? $db_settings['default']['hostname'] : ''; ?>" />
 	</div>
 	<div>
 		<label for="database">Database Name</label>
-		<input type="text" name="database" id="database" value="<?= isset($db_settings['database']) ? $db_settings['database'] : ''; ?>" />
+		<input type="text" name="database" id="database" value="<?= isset($db_settings['default']['database']) ? $db_settings['default']['database'] : ''; ?>" />
 	</div>
 	<div>
 		<label for="username">Username</label>
-		<input type="text" name="username" id="username" value="<?= isset($db_settings['username']) ? $db_settings['username'] : ''; ?>" />
+		<input type="text" name="username" id="username" value="<?= isset($db_settings['default']['username']) ? $db_settings['default']['username'] : ''; ?>" />
 	</div>
 	<div>
 		<label for="password">Password</label>
-		<input type="text" name="password" id="password" value="<?= isset($db_settings['password']) ? $db_settings['password'] : ''; ?>" />
+		<input type="text" name="password" id="password" value="<?= isset($db_settings['default']['password']) ? $db_settings['default']['password'] : ''; ?>" />
 	</div>
 	
 	<fieldset class="collapsible small">
@@ -27,35 +27,35 @@
 		<div>
 			<label for="pconnect">Persistant Connection?</label>
 			<select name="pconnect">
-				<option value="TRUE" <?php echo isset($db_settings['pconnect']) && $db_settings['pconnect'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
-				<option value="FALSE" <?php echo isset($db_settings['pconnect']) && $db_settings['pconnect'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
+				<option value="TRUE" <?php echo isset($db_settings['default']['pconnect']) && $db_settings['default']['pconnect'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
+				<option value="FALSE" <?php echo isset($db_settings['default']['pconnect']) && $db_settings['default']['pconnect'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
 			</select>
 		</div>
 		
 		<div>
 			<label for="db_debug">Display Database Errors?</label>
 			<select name="db_debug">
-				<option value="TRUE" <?php echo isset($db_settings['db_debug']) && $db_settings['db_debug'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
-				<option value="FALSE" <?php echo isset($db_settings['db_debug']) && $db_settings['db_debug'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
+				<option value="TRUE" <?php echo isset($db_settings['default']['db_debug']) && $db_settings['default']['db_debug'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
+				<option value="FALSE" <?php echo isset($db_settings['default']['db_debug']) && $db_settings['default']['db_debug'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
 			</select>
 		</div>
 		
 		<div>
 			<label for="cache_on">Enable Query Caching?</label>
 			<select name="cache_on">
-				<option value="TRUE" <?php echo isset($db_settings['cache_on']) && $db_settings['cache_on'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
-				<option value="FALSE" <?php echo isset($db_settings['cache_on']) && $db_settings['cache_on'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
+				<option value="TRUE" <?php echo isset($db_settings['default']['cache_on']) && $db_settings['default']['cache_on'] == TRUE ? 'selected="selected"' : ''; ?>>Yes</option>
+				<option value="FALSE" <?php echo isset($db_settings['default']['cache_on']) && $db_settings['default']['cache_on'] == FALSE ? 'selected="selected"' : ''; ?>>No</option>
 			</select>
 		</div>
 		
 		<div>
 			<label for="cachedir">Cache Directory</label>
-			<input type="text" name="cachedir" id="cachedir" value="<?= isset($db_settings['cachedir']) ? $db_settings['cachedir'] : ''; ?>" />
+			<input type="text" name="cachedir" id="cachedir" value="<?= isset($db_settings['default']['cachedir']) ? $db_settings['default']['cachedir'] : ''; ?>" />
 		</div>
 		
 		<div>
 			<label for="dbprefix">Prefix</label>
-			<input type="text" name="dbprefix" id="dbprefix" value="<?= isset($db_settings['dbprefix']) ? $db_settings['dbprefix'] : ''; ?>" style="width: 100px" />
+			<input type="text" name="dbprefix" id="dbprefix" value="<?= isset($db_settings['default']['dbprefix']) ? $db_settings['default']['dbprefix'] : ''; ?>" style="width: 100px" />
 		</div>
 	</fieldset>
 		
