0e848e36496b095df8dff57d88f578bf3f840194
lonnieezell <lonnieje@gmail.com>
2011-03-23 00:55:30 -0500

    Updated welcome page.

--- bonfire/application/views/home/index.php
+++ bonfire/application/views/home/index.php
@@ -1,1 +1,26 @@
 <h2>Welcome to Bonfire.</h2>
+
+<p>Ready to pour some kerosene on the logs and start a Bonfire?</p>
+
+<p>If you're new to Bonfire, but familiar with <a href="http://www.codeigniter.com" target="_blank">CodeIgniter</a>, then you should be up an running within the system in no time. If you're new to CodeIgniter, make sure you read through and understand the latest <a href="http://codeigniter.com/user_guide/" target="_blank">CodeIgniter User Guide</a> before diving into Bonfire. Your headaches will thank you.</p>
+
+<br/>
+
+<p>If you would like to edit this page you'll find it located at:</p>
+
+<div class="notification information">
+	<p><code>bonfire/application/views/home/index.php</code></p>
+</div>
+
+<p>The corresponding controller for this page is found at:</p>
+
+<div class="notification information">
+	<p><code>bonfire/application/controllers/home.php</code></p>
+</div>
+
+<p>If you are new to Bonfire, you should start by reading the <?php echo anchor('/docs', 'docs') ?>.</p>
+
+<p style="text-align: center">
+	<?php echo anchor('/login', 'Login'); ?>
+</p>
+
--- bonfire/themes/default/index.php
+++ bonfire/themes/default/index.php
@@ -24,6 +24,10 @@
 	</div>	<!-- /page -->
 	
 	<div class="foot">
+		<?php if (ENVIRONMENT == 'development') :?>
+			<p style="float: right; margin-right: 80px;">Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
+		<?php endif; ?>
+		
 		<p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION ?></a></p>
 	</div>
 </body>
