8b83d7c792a556679bead726f51a8a2c1e1512d5
lonnieezell <lonnieje@gmail.com>
2011-04-15 09:13:16 -0500

    Install module fully localized.

--- bonfire/application/core_modules/install/views/account.php
+++ bonfire/application/core_modules/install/views/account.php
@@ -1,19 +1,15 @@
-<h2>Welcome</h2>
-
-<p>Welcome to the Bonfire installation process! Just fill in the fields below, and before you know it you will be creating CodeIgniter 2.0 based web apps faster than ever.</p>
+<?php echo lang('in_intro'); ?>
 
 <?php if (isset($startup_errors) && !empty($startup_errors)) :?>
 
-	<h2>Files/Folders Not Writeable</h2>
+	<h2><?php echo lang('in_not_writeable_heading'); ?></h2>
 	
 	<?php echo $startup_errors; ?>
 	
 	<p style="text-align: right; margin-top: 3em;"><?php echo anchor('installer.php', 'Reload Page'); ?></p>
 
 <?php else : ?>
-	<h2>Information Needed</h2>
-	
-	<p>Please provide the following information.</p>
+	<?php echo lang('in_account_heading'); ?>
 	
 	
 	<?php if (validation_errors()) : ?>
@@ -25,38 +21,38 @@
 	<?php echo form_open(current_url()) ?>
 	
 		<div>
-			<label>Site Title</label>
+			<label><?php echo lang('in_site_title'); ?></label>
 			<input type="text" name="site_title" id="site_title" placeholder="My Great Bonfire App" value="<?php echo set_value('site_title') ?>" />
 		</div>
 		
 		<div>
-			<label>Username</label>
+			<label><?php echo lang('bf_username'); ?></label>
 			<input type="text" name="username" id="username" value="<?php echo set_value('username') ?>" />
 		</div>
 		
 		<br />
 		
 		<div>
-			<label>Password</label>
+			<label><?php echo lang('bf_password'); ?></label>
 			<input type="password" name="password" id="password" value="" />
-			<p class="small">Minimum length: 8 characters.</p>
+			<p class="small"><?php echo lang('in_password_note'); ?></p>
 		</div>
 		
 		<div>
-			<label>Password (again)</label>
+			<label><?php echo lang('in_password_again'); ?></label>
 			<input type="password" name="pass_confirm" id="pass_confirm" value="" />
 		</div>
 		
 		<br/>
 		
 		<div>
-			<label>Your Email</label>
+			<label><?php echo lang('in_email'); ?></label>
 			<input type="email" name="email" id="email" placeholder="me@home.com" value="<?php echo set_value('email') ?>" />
-			<p class="small">Please double-check your email before continuing.</p>
+			<p class="small"><?php echo lang('in_email_note'); ?></p>
 		</div>
 		
 		<div class="submits">
-			<input type="submit" name="submit" id="submit" value="Install Bonfire" />
+			<input type="submit" name="submit" id="submit" value="<?php echo lang('in_install_button'); ?>" />
 		</div>
 	
 	<?php echo form_close(); ?>
