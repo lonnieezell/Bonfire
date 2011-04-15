1d6521f941800303427a1a182b2abe728caf43b1
lonnieezell <lonnieje@gmail.com>
2011-04-15 08:54:08 -0500

    Emailer module fully localized.

--- bonfire/application/core_modules/emailer/views/settings/_sub_nav.php
+++ bonfire/application/core_modules/emailer/views/settings/_sub_nav.php
@@ -1,4 +1,4 @@
 <div id="sub-nav" class="button-group">
-	<a href="<?php echo site_url('admin/settings/emailer') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> >Settings</a>
+	<a href="<?php echo site_url('admin/settings/emailer') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> ><?php echo lang('bf_context_settings'); ?></a>
 	<a href="<?php echo site_url('admin/settings/emailer/template') ?>" <?php echo $this->uri->segment(4) == 'template' ? 'class="current"' : '' ?> >Template</a>
 </div>
