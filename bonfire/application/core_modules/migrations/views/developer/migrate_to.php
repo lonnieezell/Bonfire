18c0f2b63ec26ed26bd1831840049b88d0d16b32
lonnieezell <lonnieje@gmail.com>
2011-04-17 23:20:42 -0500

    Migrations module localized.

--- bonfire/application/core_modules/migrations/views/developer/migrate_to.php
+++ bonfire/application/core_modules/migrations/views/developer/migrate_to.php
@@ -1,14 +1,14 @@
 <div class="notification attention">
-	<p>Performing migrations <b>WILL</b> change your database structure, possibly ending in disaster. If you are not comfortable with your migrations, please verify them before continuing.</p>
+	<p><?php echo lang('mig_migrate_note'); ?></p>
 </div>
 
 <!-- Migration Confirmation -->
-<h2>Migrate database to version <?php echo $latest_version ?>?</h2>
+<h2><?php echo lang('mig_migrate_to'); ?> <?php echo $latest_version ?>?</h2>
 
 <?php echo form_open($this->uri->uri_string(), 'class="constrained"'); ?>
 
 	<p>
-		Or choose another migration: 
+		<?php echo lang('mig_choose_migration'); ?> 
 		<select name="migration">
 		<?php foreach ($migrations as $migration) :?>
 			<option value="<?php echo (int)substr($migration, 0, 3) ?>" <?php echo ((int)substr($migration, 0, 3) == $this->uri->segment(5)) ? 'selected="selected"' : '' ?>><?php echo $migration ?></option>
@@ -17,6 +17,6 @@
 	</p>
 
 	<div class="submits">
-		<input type="submit" name="submit" value="Migrate Database" /> or <?php echo anchor('admin/developer/migrations', 'Cancel'); ?>
+		<input type="submit" name="submit" value="<?php echo lang('mig_migrate_button'); ?>" /> or <?php echo anchor('admin/developer/migrations', lang('bf_action_cancel')); ?>
 	</div>
 <?php echo form_close(); ?>
