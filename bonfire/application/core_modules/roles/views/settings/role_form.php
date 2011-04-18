f928040ce67ee9cc6fe030b8f3434ce9ed5f817e
lonnieezell <lonnieje@gmail.com>
2011-04-17 23:34:31 -0500

    Role module now localized.

--- bonfire/application/core_modules/roles/views/settings/role_form.php
+++ bonfire/application/core_modules/roles/views/settings/role_form.php
@@ -7,27 +7,27 @@
 <?php echo form_open($this->uri->uri_string(), 'class="constrained ajax-form"'); ?>
 
 	<div>
-		<label>Role Name</label>
+		<label><?php echo lang('role_name'); ?></label>
 		<input type="text" name="role_name" class="medium" value="<?php echo isset($role) ? $role->role_name : '' ?>" />
 	</div>
 	
 	<div style="vertical-align: top">
-		<label>Description</label>
+		<label><?php echo lang('bf_description'); ?></label>
 		<textarea name="description" rows="3"><?php echo isset($role) ? $role->description : '' ?></textarea>
-		<p class="small indent">Max. 255 characters.</p>
+		<p class="small indent"><?php echo lang('role_max_desc_length'); ?></p>
 	</div>
 	
 	<div>
-		<label>Default Role?</label>
+		<label><?php echo lang('role_default_role'); ?>?</label>
 		<input type="checkbox" name="default" value="1" <?php echo isset($role) && $role->default == 1 ? 'checked="checked"' : '' ?> />
-		<p class="small" style="display: inline">Check if this role should be assigned to all new users.</p>
+		<p class="small" style="display: inline"><?php echo lang('role_default_note'); ?></p>
 	</div>
 	
 	<!-- Permissions -->
 	<fieldset>
-		<legend>Permissions</legend>
+		<legend><?php echo lang('role_permissions'); ?></legend>
 		
-		<p>Check all permissions that apply to this Role.</p>
+		<p><?php echo lang('role_permissions_check_note'); ?></p>
 	
 		<?php echo modules::run('roles/settings/matrix'); ?>
 	
@@ -35,18 +35,18 @@
 	
 	<div class="text-right">
 		<br/>
-		<input type="submit" name="submit" value="Save Role" /> or <?php echo anchor('admin/settings/roles', 'Cancel'); ?>
+		<input type="submit" name="submit" value="<?php echo lang('role_save_role'); ?>" /> or <?php echo anchor('admin/settings/roles', lang('bf_action_cancel')); ?>
 	</div>
 
 	<br/>
 
 	<?php if (isset($role)) : ?>
 	<div class="box delete rounded">
-		<a class="button" id="delete-me" href="<?php echo site_url('admin/settings/roles/delete/'. $role->role_id); ?>" onclick="return confirm('Are you sure you want to delete this role?')">Delete this Role</a>
+		<a class="button" id="delete-me" href="<?php echo site_url('admin/settings/roles/delete/'. $role->role_id); ?>" onclick="return confirm('Are you sure you want to delete this role?')"><?php echo lang('role_delete_role'); ?></a>
 		
-		<h3>Delete this Role</h3>
+		<h3><?php echo lang('role_delete_role'); ?></h3>
 		
-		<p>Deleting this role will convert all users that are currently assigned it to the site's default role.</p>
+		<p><?php echo lang('role_delete_note'); ?></p>
 	</div>
 	<?php endif; ?>
 <?php echo form_close(); ?>
