a6ee39425562f3c4370fbd01682187b758382660
lonnieezell <lonnieje@gmail.com>
2011-04-17 23:20:42 -0500

    Migrations module localized.

---
+++ bonfire/application/core_modules/migrations/language/english/migrations_lang.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['mig_intro']					= 'Migrations help you keep your database up to date and synced between development and production servers by providing a simple way to keep &lsquo;version control&rsquo; on your database.';
+$lang['mig_not_enabled']			= 'Migrations are not enabled.';
+$lang['mig_installed_version']		= 'Installed Version:';
+$lang['mig_latest_version']			= 'Latest Available Version:';
+$lang['mig_db_not_current']			= 'Your database is not up to date.';
+$lang['mig_install_latest']			= 'Install Latest Version';
+
+$lang['mig_migrate_note']			= 'Performing migrations <b>WILL</b> change your database structure, possibly ending in disaster. If you are not comfortable with your migrations, please verify them before continuing.';
+$lang['mig_migrate_to']				= 'Migrate database to version';
+$lang['mig_choose_migration']		= 'Or choose another migration:';
+$lang['mig_migrate_button']			= 'Migrate Database';
--- bonfire/application/core_modules/migrations/views/developer/index.php
+++ bonfire/application/core_modules/migrations/views/developer/index.php
@@ -1,22 +1,22 @@
 <br/>
-<p>Migrations help you keep your database up to date and synced between development and production servers by providing a simple way to keep 'version control' on your database.</p>
+<p><?php echo lang('mig_intro'); ?></p>
 
 <?php if ($this->config->item('migrations_enabled') === false) :?>
 
 	<div class="notification attention">
-		<p>Migrations are not enabled.</p>
+		<p><?php echo lang('mig_not_enabled'); ?></p>
 	</div>
 
 <?php else : ?>
 
 	<br />
-	<p><b>Installed Version:</b> <?php echo $installed_version; ?></p>
-	<p><b>Latest Available Version:</b> <?php echo $latest_version ?></p>
+	<p><b><?php echo lang('mig_installed_version'); ?></b> <?php echo $installed_version; ?></p>
+	<p><b><?php echo lang('mig_latest_version'); ?></b> <?php echo $latest_version ?></p>
 	
 	<br />
 	<?php if ($latest_version > $installed_version) : ?>
 	<div class="notification attention">
-		<p>Your database is not up to date. <?php echo anchor('admin/developer/migrations/migrate_to/'. $latest_version, 'Install Latest Version'); ?></p>
+		<p><?php echo lang('mig_db_not_current'); ?> <?php echo anchor('admin/developer/migrations/migrate_to/'. $latest_version, lang('mig_install_latest')); ?></p>
 	</div>
 	<?php endif; ?>
 
