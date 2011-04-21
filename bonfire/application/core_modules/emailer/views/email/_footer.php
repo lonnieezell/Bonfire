fe46fa910e2bb77d09684402798e18364552ac95
lonnieezell <lonnieje@gmail.com>
2011-04-21 00:45:57 -0500

    Basic color modifications for email template.

--- bonfire/application/core_modules/emailer/views/email/_footer.php
+++ bonfire/application/core_modules/emailer/views/email/_footer.php
@@ -2,7 +2,7 @@
 					</tr>
 			
 					<tr>
-						<td style="background-color:#FFFFCC;border-top:10px solid #FFFFFF;" valign="top">
+						<td style="background-color:#fff;border-top:1px solid #ccc;" valign="top">
 							Copyright (C) 2007  All rights reserved.
 						</td>
 					</tr>
