f2d986c6a430ebb33cc0f4fc1b4548241fd3f5fc
lonnieezell <lonnieje@gmail.com>
2011-05-12 14:12:06 -0500

    Ooops! Forgot to update the version in constants.php to reflect 0.11

--- bonfire/application/config/constants.php
+++ bonfire/application/config/constants.php
@@ -37,7 +37,7 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
-define('BONFIRE_VERSION', '0.1');
+define('BONFIRE_VERSION', '0.11');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
