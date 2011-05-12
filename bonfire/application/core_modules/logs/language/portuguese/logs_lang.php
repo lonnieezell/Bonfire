7bbd3de04664c829089b7253ea85ae4b8abc3a38
daK76 <costa.ti@gmail.com>
2011-05-12 23:22:14 +0000

    All core-modules translated Not en-pt revised at all, just translated for dev purposes

--- bonfire/application/core_modules/logs/language/portuguese/logs_lang.php
+++ bonfire/application/core_modules/logs/language/portuguese/logs_lang.php
@@ -21,22 +21,22 @@
 	THE SOFTWARE.
 */
 
-$lang['log_no_logs'] 			= 'No logs found.';
-$lang['log_not_enabled']		= 'Logging is not currently enabled.';
-$lang['log_the_following']		= 'Log the following:';
-$lang['log_what_0']				= '0 - Nothing';
-$lang['log_what_1']				= '1 - Error Message (including PHP Errors)';
-$lang['log_what_2']				= '2 - Debug Messages';
-$lang['log_what_3']				= '3 - Information Messages';
-$lang['log_what_4']				= '4 - All Messages';
-$lang['log_what_note']			= 'The higher log values also include all messages from the lower numbers. So, logging 2 - Debug Messages also logs 1 - Error Messages.';
+$lang['log_no_logs'] 			= 'Não existem logs.';
+$lang['log_not_enabled']		= 'Logging não está activo.';
+$lang['log_the_following']		= 'Logs disponíveis:';
+$lang['log_what_0']				= '0 - Sem log';
+$lang['log_what_1']				= '1 - Msg Error(incluindo PHP Errors)';
+$lang['log_what_2']				= '2 - Msg Debug';
+$lang['log_what_3']				= '3 - Msg Information';
+$lang['log_what_4']				= '4 - Todas as Msgs';
+$lang['log_what_note']			= 'O Tipo de Log escolhido inclui o log anterior, ie, log do tipo 3 inclui 1 e 2.';
 
-$lang['log_save_button']		= 'Save Log Settings';
-$lang['log_delete_button']		= 'Delete Log Files';
+$lang['log_save_button']		= 'Gravar definições de LOG';
+$lang['log_delete_button']		= 'Apagar ficheiros de LOG';
 
-$lang['log_big_file_note']		= 'Logging can rapidly create very large files, if you log too much information. For live sites, you should probably log only Errors.';
-$lang['log_delete_note']		= '<h3>Delete all log files?</h3><p>Deleting log files is permanent. There is no going back, so please make sure.</p>';
+$lang['log_big_file_note']		= 'Atenção ao tamanho dos ficheiros de LOG quando são escolhidos valores elevados.';
+$lang['log_delete_note']		= '<h3>Apagar os ficheiros de LOG?</h3><p>Opção não reversível.</p>';
 
-$lang['log_not_found']			= 'Either the log file could not be located, or it was empty.';
-$lang['log_show_all_entries']	= 'All entries';
-$lang['log_show_errors']		= 'Errors only';
+$lang['log_not_found']			= 'Log não encontrado ou vazio.';
+$lang['log_show_all_entries']	= 'Mostrar todas as entradas';
+$lang['log_show_errors']		= 'Apenas errors';
