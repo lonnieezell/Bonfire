0498b7f753d2bb9687919a115ea10d34613db68a
lonnieezell <lonnieje@gmail.com>
2011-05-13 08:46:16 -0500

    Fixed urls in Emailer dropdown menu. I should probably not commit when about ready to fall asleep at the keyboard.

--- bonfire/application/core_modules/emailer/views/settings/menu.php
+++ bonfire/application/core_modules/emailer/views/settings/menu.php
@@ -1,4 +1,4 @@
 <ul>
-	<li><a href="<?php echo site_url('admin/developer/emailer') ?>">Settings</a></li>
-	<li><a href="<?php echo site_url('admin/developer/emailer/template') ?>">Template</a></li>
+	<li><a href="<?php echo site_url('admin/settings/emailer') ?>">Settings</a></li>
+	<li><a href="<?php echo site_url('admin/settings/emailer/template') ?>">Template</a></li>
 </ul>
