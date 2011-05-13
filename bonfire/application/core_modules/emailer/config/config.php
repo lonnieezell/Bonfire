0634da452f78685be523fed40302898330c59a8a
lonnieezell <lonnieje@gmail.com>
2011-05-13 00:12:57 -0500

    Modules can now have a settings array that allows for drop down menus, custom names and more.

---
+++ bonfire/application/core_modules/emailer/config/config.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['module_config'] = array(
+	'menus'	=> array(
+		'settings'	=> 'emailer/settings/menu'
+	),
+);
---
+++ bonfire/application/core_modules/emailer/views/settings/menu.php
@@ -0,0 +1,4 @@
+<ul>
+	<li><a href="<?php echo site_url('admin/developer/emailer') ?>">Settings</a></li>
+	<li><a href="<?php echo site_url('admin/developer/emailer/template') ?>">Template</a></li>
+</ul>
