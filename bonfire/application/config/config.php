12ebea2764223e9809864daf73a9f49201152f1e
lonnieezell <lonnieje@gmail.com>
2011-05-12 21:50:50 -0500

    Reset language to english after last merge.

--- bonfire/application/config/config.php
+++ bonfire/application/config/config.php
@@ -75,7 +75,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'portuguese';
+$config['language']	= 'english';
 
 /*
 |--------------------------------------------------------------------------
