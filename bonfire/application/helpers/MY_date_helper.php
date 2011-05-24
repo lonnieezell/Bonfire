b697e20b5d7ca064a7740a3e23b7e9268f7cffb7
lonnieezell <lonnieje@gmail.com>
2011-05-23 23:52:31 -0500

    Added license and docs to MY_date_helper

--- bonfire/application/helpers/MY_date_helper.php
+++ bonfire/application/helpers/MY_date_helper.php
@@ -1,5 +1,46 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+	Copyright (c) 2011 Lonnie Ezell
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+/*
+	File: MY_date_helper
+	
+	Includes additional date-related functions helpful in Bonfire development.
+*/
+
+
+/*
+	Function: relative_time()
+	
+	Takes a UNIX timestamp and returns a string representing how long ago
+	that date was, like "moments ago", "2 weeks ago", etc.
+	
+	Parameters:
+		$timestamp	- A UNIX timestamp
+		
+	Returns: 
+		string	- a human-readable amount of time 'ago'
+*/
 if (!function_exists('relative_time'))
 {
 	function relative_time($timestamp)
