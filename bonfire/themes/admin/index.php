f3f290eeb7516cacaea045521cbc0580baecfb10
lonnieezell <lonnieje@gmail.com>
2011-05-26 15:22:53 -0500

    Fixed bug in blank screens in Opera due to overflow hidden. Also fixes most browsers for horizontal scrollbar introduced due to box-shadow quirks. Fixes #58

--- bonfire/themes/admin/index.php
+++ bonfire/themes/admin/index.php
@@ -37,43 +37,45 @@
 		<?php echo Template::message(); ?>
 	</div>
 	
-	<!-- Nav Bar -->
-	<div id="toolbar">
-		<div id="toolbar-right">
-			<a href="<?php echo site_url('admin/settings/users/edit/'. $this->auth->user_id()) ?>" id="tb_email"><?php echo $this->auth->email() ?></a>
-			<a href="<?php echo site_url('logout') ?>" id="tb_logout" title="Logout">Logout</a>
+	<div id="header">
+		<!-- Nav Bar -->
+		<div id="toolbar">
+			<div id="toolbar-right">
+				<a href="<?php echo site_url('admin/settings/users/edit/'. $this->auth->user_id()) ?>" id="tb_email"><?php echo $this->auth->email() ?></a>
+				<a href="<?php echo site_url('logout') ?>" id="tb_logout" title="Logout">Logout</a>
+			</div>
+		
+			<h1><a href="<?php echo site_url(); ?>" target="_blank"><?php echo config_item('site.title') ?></a></h1>
+			
+			<div id="toolbar-left">
+				<?php if (has_permission('Site.Content.View')) :?>
+					<a href="<?php echo site_url('admin/content') ?>" <?php echo check_class('content') ?> id="tb_content" title="Content">Content</a>
+				<?php endif; ?>
+				<?php if (has_permission('Site.Statistics.View')) :?>
+					<a href="<?php echo site_url('admin/stats') ?>" <?php echo check_class('stats') ?> id="tb_stats" title="Statistics">Statistics</a>
+				<?php endif; ?>
+				<?php if (has_permission('Site.Appearance.View')) :?>
+					<a href="<?php echo site_url('admin/appearance') ?>" <?php echo check_class('appearance') ?> id="tb_appearance" title="Appearance">Appearance</a>
+				<?php endif; ?>
+				<?php if (has_permission('Site.Settings.View')) :?>
+					<a href="<?php echo site_url('admin/settings') ?>" <?php echo check_class('settings') ?> id="tb_settings" title="Settings">Settings</a>
+				<?php endif; ?>
+				<?php if (has_permission('Site.Developer.View')) :?>
+					<a href="<?php echo site_url('admin/developer') ?>" <?php echo check_class('developer') ?> id="tb_developer" title="Developer Tools">Developer Tools</a>
+				<?php endif; ?>
+			</div>	<!-- /toolbar-left -->
 		</div>
-	
-		<h1><a href="<?php echo site_url(); ?>" target="_blank"><?php echo config_item('site.title') ?></a></h1>
 		
-		<div id="toolbar-left">
-			<?php if (has_permission('Site.Content.View')) :?>
-				<a href="<?php echo site_url('admin/content') ?>" <?php echo check_class('content') ?> id="tb_content" title="Content">Content</a>
-			<?php endif; ?>
-			<?php if (has_permission('Site.Statistics.View')) :?>
-				<a href="<?php echo site_url('admin/stats') ?>" <?php echo check_class('stats') ?> id="tb_stats" title="Statistics">Statistics</a>
-			<?php endif; ?>
-			<?php if (has_permission('Site.Appearance.View')) :?>
-				<a href="<?php echo site_url('admin/appearance') ?>" <?php echo check_class('appearance') ?> id="tb_appearance" title="Appearance">Appearance</a>
-			<?php endif; ?>
-			<?php if (has_permission('Site.Settings.View')) :?>
-				<a href="<?php echo site_url('admin/settings') ?>" <?php echo check_class('settings') ?> id="tb_settings" title="Settings">Settings</a>
-			<?php endif; ?>
-			<?php if (has_permission('Site.Developer.View')) :?>
-				<a href="<?php echo site_url('admin/developer') ?>" <?php echo check_class('developer') ?> id="tb_developer" title="Developer Tools">Developer Tools</a>
-			<?php endif; ?>
-		</div>	<!-- /toolbar-left -->
-	</div>
+		<?php echo modules::run('subnav/subnav/index', $this->uri->segment(2)); ?>
 	
-	<?php echo modules::run('subnav/subnav/index', $this->uri->segment(2)); ?>
-
-	<div id="nav-bar">
-		<?php if (isset($toolbar_title)) : ?>
-			<h1><?php echo $toolbar_title ?></h1>
-		<?php endif; ?>
-		
-		<?php Template::block('sub_nav', ''); ?>
-	</div>
+		<div id="nav-bar">
+			<?php if (isset($toolbar_title)) : ?>
+				<h1><?php echo $toolbar_title ?></h1>
+			<?php endif; ?>
+			
+			<?php Template::block('sub_nav', ''); ?>
+		</div>
+	</div> <!-- /header -->
 
 	<div class="content-main <?php echo isset(Template::$blocks['nav_bottom']) ? 'with-bottom-bar' : '' ?>">
 			<?php echo Template::yield(); ?>
