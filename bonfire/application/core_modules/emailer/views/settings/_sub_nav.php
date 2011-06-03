a8ce196838df82ec31a9da1b907494c1ba487620
lonnieezell <lonnieje@gmail.com>
2011-06-02 23:25:42 -0500

    Moved emailer stats functionality to the settings context so that the stats context is no longer required by core.

--- bonfire/application/core_modules/emailer/views/settings/_sub_nav.php
+++ bonfire/application/core_modules/emailer/views/settings/_sub_nav.php
@@ -1,4 +1,5 @@
 <div id="sub-nav" class="button-group">
 	<a href="<?php echo site_url('admin/settings/emailer') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> ><?php echo lang('bf_context_settings'); ?></a>
 	<a href="<?php echo site_url('admin/settings/emailer/template') ?>" <?php echo $this->uri->segment(4) == 'template' ? 'class="current"' : '' ?> >Template</a>
+	<a href="<?php echo site_url('admin/settings/emailer/queue') ?>" <?php echo $this->uri->segment(4) == 'queue' ? 'class="current"' : '' ?> >Queue</a>
 </div>
--- bonfire/application/core_modules/emailer/views/settings/menu.php
+++ bonfire/application/core_modules/emailer/views/settings/menu.php
@@ -1,4 +1,5 @@
 <ul>
 	<li><a href="<?php echo site_url('admin/settings/emailer') ?>">Settings</a></li>
 	<li><a href="<?php echo site_url('admin/settings/emailer/template') ?>">Template</a></li>
+	<li><a href="<?php echo site_url('admin/settings/emailer/queue') ?>">View Queue</a></li>
 </ul>
