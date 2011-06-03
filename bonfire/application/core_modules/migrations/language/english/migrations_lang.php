782326dabf637cc5ebca9842af4d7c1242396214
lonnieezell <lonnieje@gmail.com>
2011-06-03 10:16:48 -0500

    Migrations now separated into application specific and bonfire core migrations.

--- bonfire/application/core_modules/migrations/language/english/migrations_lang.php
+++ bonfire/application/core_modules/migrations/language/english/migrations_lang.php
@@ -5,8 +5,12 @@ $lang['mig_not_enabled']			= 'Migrations are not enabled.';
 $lang['mig_installed_version']		= 'Installed Version:';
 $lang['mig_latest_version']			= 'Latest Available Version:';
 $lang['mig_db_not_current']			= 'Your database is not up to date.';
+$lang['mig_no_migrations']			= 'There are no available migrations.';
 
 $lang['mig_migrate_note']			= 'Performing migrations <b>WILL</b> change your database structure, possibly ending in disaster. If you are not comfortable with your migrations, please verify them before continuing.';
 $lang['mig_migrate_to']				= 'Migrate database to version';
 $lang['mig_choose_migration']		= 'Migrate to version:';
 $lang['mig_migrate_button']			= 'Migrate Database';
+
+$lang['mig_app_migrations']			= "Application Migrations";
+$lang['mig_core_migrations']		= "Bonfire Core Migrations";
