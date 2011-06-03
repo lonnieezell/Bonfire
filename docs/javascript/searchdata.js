65eab1c53270fb09b313fcea4476862a649541a6
lonnieezell <lonnieje@gmail.com>
2011-06-02 23:36:10 -0500

    Updated docs to include info about the module_config array.

--- docs/javascript/searchdata.js
+++ docs/javascript/searchdata.js
@@ -110,7 +110,7 @@ var indexSectionsWithContent = {
       "P": false,
       "Q": false,
       "R": false,
-      "S": false,
+      "S": true,
       "T": false,
       "U": true,
       "V": false,
