2ec1308e58d1a5995f6cccf5d4ec66b5600d924b
lonnieezell <lonnieje@gmail.com>
2011-06-03 00:10:15 -0500

    Cleaned up contexts. Developer index page now lists installed module using module config information.

---
+++ bonfire/application/core_modules/roles/config/config.php
@@ -0,0 +1,6 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['module_config'] = array(
+	'description'	=> 'Provides Role-Based Access Control for users.',
+	'author'		=> 'Bonfire Team'
+);
---
+++ bonfire/application/core_modules/subnav/config/config.php
@@ -0,0 +1,6 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['module_config'] = array(
+	'description'	=> 'Generates navigation bar based on installed modules.',
+	'author'		=> 'Bonfire Team'
+);
