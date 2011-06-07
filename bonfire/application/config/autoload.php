5ab1d99b6eb19e6a4198ec6e4f7dfccb89ee03b7
Gary Pearman <gpearman@gmail.com>
2011-06-07 22:09:19 +0100

    Autoload database

--- bonfire/application/config/autoload.php
+++ bonfire/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('template', 'assets', 'events');
+$autoload['libraries'] = array('template', 'assets', 'events', 'database');
 
 
 /*
