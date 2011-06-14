dcb2a1791f34184cec0fe415e01e2e20c345acba
lonnieezell <lonnieje@gmail.com>
2011-06-14 00:35:32 -0500

    Initial UI changes for new migrations interface. Adds per-module migrations to UI. Now just have to implement the actual migrations.

--- bonfire/application/core_modules/migrations/views/developer/index.php
+++ bonfire/application/core_modules/migrations/views/developer/index.php
@@ -1,3 +1,7 @@
+<style>
+td { vertical-align: baseline; }
+tr:hover { background: #f6f6f6; border-top: 1px solid #e7e7e7; border-bottom: 1px solid #e7e7e7; }
+</style>
 <br/>
 <p><?php echo lang('mig_intro'); ?></p>
 
@@ -9,63 +13,122 @@
 
 <?php else : ?>
 
-	<!-- Application Migrations -->
-	<h2><?php echo lang('mig_app_migrations'); ?></h2>
+	<div id="tabs">
+		<ul>
+			<li><a href="#app-tab">Application</a></li>
+			<li><a href="#mod-tab">Modules</a></li>
+			<li><a href="#core-tab">Bonfire</a></li>
+		</ul>
 		
-	<div class="notification information">
-		<p><?php echo lang('mig_installed_version'); ?> <b><?php echo $installed_version; ?></b> / 
-		<?php echo lang('mig_latest_version'); ?> <b><?php echo $latest_version ?></b></p>
-	</div>
+		<!-- Application Migrations -->
+		<div id="app-tab">
+			<h2><?php echo lang('mig_app_migrations'); ?></h2>
 		
-	<?php echo form_open($this->uri->uri_string(), 'class="constrained"'); ?>
-		<input type="hidden" name="core_only" value="0" />
-	
-		<?php if (count($app_migrations)) : ?>
-		<p>
-			<?php echo lang('mig_choose_migration'); ?> 
-			<select name="migration">
-			<?php foreach ($app_migrations as $migration) :?>
-				<option value="<?php echo (int)substr($migration, 0, 3) ?>" <?php echo ((int)substr($migration, 0, 3) == $this->uri->segment(5)) ? 'selected="selected"' : '' ?>><?php echo $migration ?></option>
-			<?php endforeach; ?>
-			</select>
-		</p>
-	
-		<div class="submits">
-			<input type="submit" name="submit" value="<?php echo lang('mig_migrate_button'); ?>" /> or <?php echo anchor('admin/developer/migrations', lang('bf_action_cancel')); ?>
+			<div class="notification information">
+				<p><?php echo lang('mig_installed_version'); ?> <b><?php echo $installed_version; ?></b> / 
+				<?php echo lang('mig_latest_version'); ?> <b><?php echo $latest_version ?></b></p>
+			</div>
+				
+			<?php echo form_open($this->uri->uri_string(), 'class="constrained"'); ?>
+				<input type="hidden" name="core_only" value="0" />
+			
+				<?php if (count($app_migrations)) : ?>
+				<p>
+					<?php echo lang('mig_choose_migration'); ?> 
+					<select name="migration">
+					<?php foreach ($app_migrations as $migration) :?>
+						<option value="<?php echo (int)substr($migration, 0, 3) ?>" <?php echo ((int)substr($migration, 0, 3) == $this->uri->segment(5)) ? 'selected="selected"' : '' ?>><?php echo $migration ?></option>
+					<?php endforeach; ?>
+					</select>
+				</p>
+			
+				<div class="submits">
+					<input type="submit" name="submit" value="<?php echo lang('mig_migrate_button'); ?>" /> or <?php echo anchor('admin/developer/migrations', lang('bf_action_cancel')); ?>
+				</div>
+				<?php else: ?>
+					<p><?php echo lang('mig_no_migrations') ?></p>
+				<?php endif; ?>
+			<?php echo form_close(); ?>
 		</div>
-		<?php else: ?>
-			<p><?php echo lang('mig_no_migrations') ?></p>
-		<?php endif; ?>
-	<?php echo form_close(); ?>
-	
-	
-	<!-- Core Migrations -->
-	<h2><?php echo lang('mig_core_migrations'); ?></h2>
 		
-	<div class="notification information">
-		<p><?php echo lang('mig_installed_version'); ?> <b><?php echo $core_installed_version; ?></b> / 
-		<?php echo lang('mig_latest_version'); ?> <b><?php echo $core_latest_version ?></b></p>
-	</div>
+		<!-- Module Migrations -->
+		<div id="mod-tab">
+			<h2><?php echo lang('mig_mod_migrations'); ?></h2>
 	
-	<?php echo form_open($this->uri->uri_string(), 'class="constrained"'); ?>
-		<input type="hidden" name="core_only" value="1" />
-	
-		<?php if (count($core_migrations)) : ?>
-		<p>
-			<?php echo lang('mig_choose_migration'); ?> 
-			<select name="migration">
-			<?php foreach ($core_migrations as $migration) :?>
-				<option value="<?php echo (int)substr($migration, 0, 3) ?>" <?php echo ((int)substr($migration, 0, 3) == $this->uri->segment(5)) ? 'selected="selected"' : '' ?>><?php echo $migration ?></option>
-			<?php endforeach; ?>
-			</select>
-		</p>
-	
-		<div class="submits">
-			<input type="submit" name="submit" value="<?php echo lang('mig_migrate_button'); ?>" /> or <?php echo anchor('admin/developer/migrations', lang('bf_action_cancel')); ?>
+			<?php if (isset($mod_migrations) && is_array($mod_migrations)) :?>
+				<table>
+					<tbody>
+						<?php foreach ($mod_migrations as $module => $migrations) : ?>
+							<?php echo form_open(site_url('admin/developer/migrations/migrate_module/'. $module)); ?>
+								<input type="hidden" name="is_module" value="1" />
+						<tr>
+							<td><?php echo ucfirst($module) ?></td>
+							<td style="width: 25em; text-align: right;">
+								<select name="">
+								<?php foreach ($migrations as $migration) : ?>
+									<?php foreach ($migration as $filename) :?>
+										<option><?php echo $filename; ?></option>
+									<?php endforeach; ?>
+								<?php endforeach; ?>
+								</select>
+							</td>
+							<td style="width: 10em">
+								<input type="submit" name="submit" value="Migrate Module" />
+							</td>
+						</tr>
+						</form>
+						<?php endforeach; ?>
+					</tbody>
+				</table>
+			
+			<?php else : ?>
+				<div class="notification information">
+					<p>No modules have any migrations available.</p>
+				</div>
+			<?php endif; ?>
 		</div>
-		<?php else: ?>
-			<p><?php echo lang('mig_no_migrations') ?></p>
-		<?php endif; ?>
-	<?php echo form_close(); ?>
+		
+		<!-- Bonfire Migrations -->
+		<div id="core-tab">
+			<h2><?php echo lang('mig_core_migrations'); ?></h2>
+		
+			<div class="notification information">
+				<p><?php echo lang('mig_installed_version'); ?> <b><?php echo $core_installed_version; ?></b> / 
+				<?php echo lang('mig_latest_version'); ?> <b><?php echo $core_latest_version ?></b></p>
+			</div>
+			
+			<?php echo form_open($this->uri->uri_string(), 'class="constrained"'); ?>
+				<input type="hidden" name="core_only" value="1" />
+			
+				<?php if (count($core_migrations)) : ?>
+				<p>
+					<?php echo lang('mig_choose_migration'); ?> 
+					<select name="migration">
+					<?php foreach ($core_migrations as $migration) :?>
+						<option value="<?php echo (int)substr($migration, 0, 3) ?>" <?php echo ((int)substr($migration, 0, 3) == $this->uri->segment(5)) ? 'selected="selected"' : '' ?>><?php echo $migration ?></option>
+					<?php endforeach; ?>
+					</select>
+				</p>
+			
+				<div class="submits">
+					<input type="submit" name="submit" value="<?php echo lang('mig_migrate_button'); ?>" /> or <?php echo anchor('admin/developer/migrations', lang('bf_action_cancel')); ?>
+				</div>
+				<?php else: ?>
+					<p><?php echo lang('mig_no_migrations') ?></p>
+				<?php endif; ?>
+			<?php echo form_close(); ?>
+		</div>
+		
+	</div>
 
 <?php endif; ?>
+
+
+
+<script>
+head.ready(function(){
+	
+	$( "#tabs" ).tabs();
+	
+});
+</script>
