a12567da3d905adecf866978efb9e0c042ebb3d4
Sean Downey <sean@considerweb.com>
2011-06-15 21:47:10 +0100

    fix #78 Adding Xinha editor - it must be installed to /themes/admin/js/editors/xinha/

---
+++ bonfire/themes/admin/js/editors/xinha_conf.js
@@ -0,0 +1,4 @@
+_editor_url  = "/bonfire/themes/admin/js/editors/xinha/";
+_editor_lang = "en";       // And the language we need to use in the editor.
+_editor_skin = "silva";    // If you want use a skin, add the name (of the folder) here
+_editor_icons = "classic"; // If you want to use a different iconset, add the name (of the folder, under the `iconsets` folder) here
