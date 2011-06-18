b1e65b5653e6fcaf8f07a7661098decdd315621a
Sean Downey <sean@considerweb.com>
2011-06-18 01:56:11 +0100

    Moduble Builder - fix the case of the class name

--- bonfire/application/core_modules/modulebuilder/views/files/model.php
+++ bonfire/application/core_modules/modulebuilder/views/files/model.php
@@ -1,18 +1,17 @@
 <?php
-$model =<<<EOF
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+$model = '<?php if (!defined(\'BASEPATH\')) exit(\'No direct script access allowed\');
 
-class {$controller_name}_model extends BF_Model {
+class '.ucfirst($controller_name).'_model extends BF_Model {
 
-	protected \$table		= "{$controller_name}";
-	protected \$key			= "{$primary_key_field}";
-	protected \$soft_deletes	= false;
-	protected \$date_format	= "datetime";
-	protected \$set_created	= true;
-	protected \$set_modified = false;
+	protected $table		= "'.$controller_name.'";
+	protected $key			= "'.$primary_key_field.'";
+	protected $soft_deletes	= false;
+	protected $date_format	= "datetime";
+	protected $set_created	= true;
+	protected $set_modified = false;
 
 }
-EOF;
+';
 
 echo $model;
 ?>
