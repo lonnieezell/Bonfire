4aa4440c1eb74f7ec94ae84af7263fd61b3c0158
daK76 <costa.ti@gmail.com>
2011-06-19 22:33:17 +0000

    Working on extended "Use usernames" We now have a radio with 3 options display logged user

--- bonfire/application/controllers/admin/settings.php
+++ bonfire/application/controllers/admin/settings.php
@@ -84,7 +84,7 @@ class Settings extends Admin_Controller {
 			
 			'auth.allow_register'	=> isset($_POST['allow_register']) ? 1 : 0,
 			'auth.login_type'		=> $this->input->post('login_type'),
-			'auth.use_usernames'	=> isset($_POST['use_usernames']) ? 1 : 0,
+			'auth.use_usernames'	=> isset($_POST['use_usernames']) ? $this->input->post('use_usernames') : 0,
 			'auth.allow_remember'	=> isset($_POST['allow_remember']) ? 1 : 0,
 			'auth.remember_length'	=> (int)$this->input->post('remember_length'),
 			
