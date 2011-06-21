8be7a3f2bbe4c15c221eca9d52001ef4f00dd85a
lonnieezell <lonnieje@gmail.com>
2011-06-20 23:26:56 -0500

    Introduced a login destination url for roles. Fixes #75, #96

---
+++ bonfire/application/db/migrations/core/004_Add_login_destination.php
@@ -0,0 +1,30 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Add_login_destination extends Migration {
+	
+	public function up() 
+	{
+		$prefix = $this->db->dbprefix;
+		
+		$this->dbforge->add_column('roles',
+			'login_destination'	=> array(
+				'type'			=> 'VARCHAR',
+				'constraint'	=> 255,
+				'null'			=> false,
+				'default'		=> '/'
+			)
+		);
+	}
+	
+	//--------------------------------------------------------------------
+	
+	public function down() 
+	{
+		$prefix = $this->db->dbprefix;
+
+		$this->dbforge->drop_column('roles', 'login_destination');
+	}
+	
+	//--------------------------------------------------------------------
+	
+}
