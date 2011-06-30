ed1239ad924452f861adbcca7e8606ed8c288f07
daK76 <costa.ti@gmail.com>
2011-06-30 01:24:37 +0000

    Improved previous solution to not touch Home controller Don't know if it's too dirty

--- bonfire/application/controllers/home.php
+++ bonfire/application/controllers/home.php
@@ -6,10 +6,6 @@ class Home extends Front_Controller {
 	
 	public function index() 
 	{	
-		
-		$cookie = unserialize($this->input->cookie($this->config->item('sess_cookie_name')));
-		
-		Template::set('logged_in', isset ($cookie['logged_in']));
 		Template::render();
 	}
 	
