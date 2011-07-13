e2a88cccfc0736ee36d1aa66856b75634c598d02
Icehawg <icehawg@gmail.com>
2011-07-13 12:14:21 -0400

    Added check for cURL in developer tools.
    Signed-off-by: Icehawg <icehawg@gmail.com>

--- bonfire/application/core_modules/update/controllers/update.php
+++ bonfire/application/core_modules/update/controllers/update.php
@@ -43,7 +43,7 @@ class Update extends Admin_Controller {
 	*/
 	public function update_check() 
 	{ 	
-		if (!$this->config->item('updates.do_check'))
+		if (!$this->config->item('updates.do_check') || !function_exists('curl_version'))
 		{
 			return;
 		}
