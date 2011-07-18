182b9397b9dc20b3d16af2971a52131101a72585
Lonnie Ezell <lonnieje@gmail.com>
2011-07-18 00:50:14 -0500

    Added a few more files to the install script to improve upgrade safety. Fixes #106

--- index.php
+++ index.php
@@ -201,14 +201,20 @@
 
 /*
  * -------------------------------------------------------------------
- *  Do a quick check to make sure the config.php file exists. If not,
+ *  Do a quick check to make sure the config files exist. If not,
  * copy it over. This is primarily for the installer, but should not
  * create a performance hit, since PHP caches results from file_exists().
  * -------------------------------------------------------------------
  */
+// config.php
 if (!file_exists(APPPATH .'config/config.php'))
 {
 	copy(APPPATH .'config/config.example.php', APPPATH .'config/config.php');
+}
+// application.php
+if (!file_exists(APPPATH .'config/applicaiton.php'))
+{
+	copy(APPPATH .'config/application.example.php', APPPATH .'config/application.php');
 }
 
 /*
