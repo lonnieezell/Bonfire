40d1c3b45103a03644968af44e8587ac9d91b0fe
Lonnie Ezell <lonnieje@gmail.com>
2011-08-08 23:36:41 -0500

    Added Phil Sturgeons dump function to the application helper.

--- bonfire/application/helpers/application_helper.php
+++ bonfire/application/helpers/application_helper.php
@@ -485,3 +485,43 @@ function context_nav($mode='icon')
 	return $nav;
 }
 
+//--------------------------------------------------------------------
+
+/*
+	Function: dump()
+	
+	Outputs the given variables with formatting and loation. 
+	
+	Huge props out to Phil Sturgeon for this one (http://philsturgeon.co.uk/blog/2010/09/power-dump-php-applications)
+	
+	To use, pass in any number of variables as arguments.
+*/
+function dump()
+{
+	list($callee) = debug_backtrace();
+	$arguments = func_get_args();
+	$total_arguments = count($arguments);
+	
+	echo '<fieldset style="background: #fefefe !important; border:2px red solid; padding:5px">';
+    echo '<legend style="background:lightgrey; padding:5px;">'.$callee['file'].' @ line: '.$callee['line'].'</legend><pre>';
+    
+    $i = 0;
+    foreach ($arguments as $argument)
+    {
+        echo '<br/><strong>Debug #'.(++$i).' of '.$total_arguments.'</strong>: ';
+        
+        if ( (is_array($argument) || is_object($argument)) && count($argument))
+        {
+        	print_r($argument);
+        }
+        else
+        {
+        	var_dump($argument);
+        }
+    }
+
+    echo "</pre>";
+    echo "</fieldset>";
+}
+
+//--------------------------------------------------------------------
