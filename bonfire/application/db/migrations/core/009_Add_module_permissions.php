ae99fd82e8ca5a853be3b77897524211088c2041
Icehawg <icehawg@gmail.com>
2011-08-25 14:45:07 -0400

    Fix for migration 009 to give permission to Add/Delete modules right away. (problem from forum)
    Signed-off-by: Icehawg <icehawg@gmail.com>

--- bonfire/application/db/migrations/core/009_Add_module_permissions.php
+++ bonfire/application/db/migrations/core/009_Add_module_permissions.php
@@ -5,9 +5,16 @@ class Migration_Add_module_permissions extends Migration {
 	public function up() 
 	{
 		$prefix = $this->db->dbprefix;
+		
+		$this->load->library('session');
+		
+		// add administrators to module permissions
+		$assign_role = $this->session->userdata('role_id') ? $this->session->userdata('role_id') : 1;
 				
 		$this->db->query("INSERT INTO {$prefix}permissions(name, description) VALUES('Bonfire.Modules.Add','Allow creation of modules with the builder.')");
+		$this->db->query("INSERT INTO {$prefix}role_permissions VALUES(".$assign_role.",".$this->db->insert_id().")");
 		$this->db->query("INSERT INTO {$prefix}permissions(name, description) VALUES('Bonfire.Modules.Delete','Allow deletion of modules.')");
+		$this->db->query("INSERT INTO {$prefix}role_permissions VALUES(".$assign_role.",".$this->db->insert_id().")");
 		
 	}
 	
