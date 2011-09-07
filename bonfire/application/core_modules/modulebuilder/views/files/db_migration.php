d582cd78ae1b3ddc97e123fa786fe30b305db677
Icehawg <icehawg@gmail.com>
2011-09-07 13:30:01 -0400

    Should fix issue #181
    Signed-off-by: Icehawg <icehawg@gmail.com>

--- bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
+++ bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
@@ -1,5 +1,8 @@
 <?php
 
+ // There are no doubt more types where a value/length isn't possible - needs investigating
+$no_length = array('TEXT', 'DATE', 'DATETIME', 'BLOB', 'TINYBLOB', 'TINYTEXT', 'MEDIUMBLOB', 'MEDIUMTEXT', 'LONGBLOB', 'LONGTEXT');
+
 $db_migration = '<?php if (!defined(\'BASEPATH\')) exit(\'No direct script access allowed\');
 
 class Migration_Install_'.$table_name.' extends Migration {
@@ -18,11 +21,11 @@ class Migration_Install_'.$table_name.' extends Migration {
 			}
 
 			$db_migration .= '
-			$this->dbforge->add_field("`'.$module_name_lower.'_'.strtolower(set_value("view_field_name$counter")).'` '.set_value("db_field_type$counter");
+			$this->dbforge->add_field("`'.$module_name_lower.'_'.strtolower(set_value("view_field_name$counter")).'` '.addcslashes(set_value("db_field_type$counter"),'"');
 		
-			if (!in_array(set_value("db_field_type$counter"), array('TEXT', 'DATE', 'DATETIME'))) // There are no doubt more types where a value/length isn't possible - needs investigating
+			if (!in_array(set_value("db_field_type$counter"), $no_length))
 			{
-				$db_migration .= '('.set_value("db_field_length_value$counter").')';
+				$db_migration .= '('.addcslashes(set_value("db_field_length_value$counter"),'"').')';
 			}
 		
 
