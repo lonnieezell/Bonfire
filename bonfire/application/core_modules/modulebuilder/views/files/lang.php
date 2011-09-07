324b0231cc322f75a618071b5ab50aff730563b2
Icehawg <icehawg@gmail.com>
2011-09-07 11:44:51 -0400

    Fixes issue #200
    Signed-off-by: Icehawg <icehawg@gmail.com>

--- bonfire/application/core_modules/modulebuilder/views/files/lang.php
+++ bonfire/application/core_modules/modulebuilder/views/files/lang.php
@@ -4,6 +4,10 @@ $lang = '<?php if ( ! defined(\'BASEPATH\')) exit(\'No direct script access allo
 
 $lang .= '
 $lang[\''.$module_name_lower.'_manage\']			= \'Manage '.$module_name.'\';
+$lang[\''.$module_name_lower.'_edit\']				= \'Edit\';
+$lang[\''.$module_name_lower.'_true\']				= \'True\';
+$lang[\''.$module_name_lower.'_false\']				= \'False\';
+$lang[\''.$module_name_lower.'_create\']			= \'Create\';
 $lang[\''.$module_name_lower.'_edit_text\']			= \'Edit this to suit your needs\';
 $lang[\''.$module_name_lower.'_no_records\']			= \'There aren\\\'t any '.$module_name_lower.' in the system.\';
 $lang[\''.$module_name_lower.'_create_new\']			= \'Create a new '.$module_name.'.\';
