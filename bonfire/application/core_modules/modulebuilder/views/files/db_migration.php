b0f4f90b183e8e717f37903ef78c30aa87bb8483
Icehawg <icehawg@gmail.com>
2011-09-07 14:50:45 -0400

    Adds soft deletes, created and modified fields to module builder. For issue #207.
    Signed-off-by: Icehawg <icehawg@gmail.com>

--- bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
+++ bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
@@ -32,6 +32,30 @@ class Migration_Install_'.$table_name.' extends Migration {
 		$db_migration .= ' NOT NULL");';
 		
 		}
+		
+		// use soft deletes? Add deleted field.
+		if ($this->input->post('use_soft_deletes') == 'true')
+		{
+			$db_migration .= '
+			$this->dbforge->add_field("`deleted` INT(11) NOT NULL DEFAULT \'0\'");';
+		}
+		
+		// use the created field? Add field and custom name if chosen.
+		if ($this->input->post('use_created') == 'true')
+		{
+			$created_field = ($this->input->post('created_field')) ? $this->input->post('created_field') : 'created_on';
+			$db_migration .= '
+			$this->dbforge->add_field("`'.$created_field.'` datetime NOT NULL DEFAULT \'0000-00-00 00:00:00\'");';			
+		}
+		
+		// use the created field? Add field and custom name if chosen.
+		if ($this->input->post('use_modified') == 'true')
+		{
+			$modified_field = ($this->input->post('modified_field')) ? $this->input->post('modified_field') : 'modified_on';
+			$db_migration .= '
+			$this->dbforge->add_field("`'.$modified_field.'` datetime NOT NULL DEFAULT \'0000-00-00 00:00:00\'");';			
+		}
+		
 		$db_migration .= '
 		$this->dbforge->add_key(\''.$primary_key_field.'\', true);
 		$this->dbforge->create_table(\''.$table_name.'\');
