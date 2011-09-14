10c58db2ebcc65306cc106ea8e766b419ca43854
Lonnie Ezell <lonnieje@gmail.com>
2011-09-14 15:00:17 -0500

    Fixed bug that allowed non ints to be passed as user id when logging activities.

--- bonfire/application/core_modules/activities/models/activity_model.php
+++ bonfire/application/core_modules/activities/models/activity_model.php
@@ -91,7 +91,7 @@ class Activity_model extends BF_Model {
 	*/
 	public function log_activity($user_id=null, $activity='', $module='any') 
 	{
-		if (empty($user_id) || !is_numeric($user_id))
+		if (empty($user_id) || !is_integer($user_id))
 		{
 			Template::set_message('You must provide a numeric user id to log activity.','error');
 			return false;
