da0a3eac1833e463dacb451daaf51d47b2020596
Ben Evans <ben.evans@ampfieldelectrical.co.uk>
2011-09-15 19:17:08 +0100

    Removed unnecessary 'v' in default admin contexts index page as the 'v' has been added to the tag/version name

--- bonfire/application/views/admin/content/index.php
+++ bonfire/application/views/admin/content/index.php
@@ -1,6 +1,6 @@
 <h1>This Page Intentionally Left Blank</h1>
 
-<p>As of Bonfire v<?php echo BONFIRE_VERSION ?>, this page has no default content.</p>
+<p>As of Bonfire <?php echo BONFIRE_VERSION ?>, this page has no default content.</p>
 
 <p>This is your playground.</p>
 
