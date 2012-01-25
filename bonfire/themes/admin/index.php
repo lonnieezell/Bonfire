58c50dc1082d284300f9639cb6867c0d08525cb8
Sean Downey <sean@considerweb.com>
2012-01-25 14:45:02 +0000

    Fixing issues with Shortcut keys before the Migration is run

--- bonfire/themes/admin/index.php
+++ bonfire/themes/admin/index.php
@@ -10,7 +10,9 @@
 	'external',
 	true);
 
-	Assets::add_js($this->load->view('ui/shortcut_keys', $shortcut_data, true), 'inline');
+	if (isset($shortcut_data) && is_array($shortcut_data['shortcut_keys'])) {
+		Assets::add_js($this->load->view('ui/shortcut_keys', $shortcut_data, true), 'inline');
+	}
 ?>
 <!doctype html>
 <html lang="en">
@@ -48,7 +50,7 @@
 		<!-- Nav Bar -->
 		<div id="toolbar">
 			<div id="toolbar-right">
-				<?php if(is_array($shortcut_data['shortcut_keys']) && count($shortcut_data['shortcut_keys'])):?><img src="/bonfire/themes/admin/images/keyboard-icon.png" id="shortkeys_show" title="Keyboard Shortcuts" alt="Keyboard Shortcuts"/><?php endif;?>
+				<?php if(isset($shortcut_data) && is_array($shortcut_data['shortcut_keys']) && count($shortcut_data['shortcut_keys'])):?><img src="/bonfire/themes/admin/images/keyboard-icon.png" id="shortkeys_show" title="Keyboard Shortcuts" alt="Keyboard Shortcuts"/><?php endif;?>
 				<a href="<?php echo site_url(SITE_AREA .'/settings/users/edit/'. $this->auth->user_id()) ?>" id="tb_email" title="<?php echo lang('bf_user_settings') ?>"><?php echo $this->settings_lib->item('auth.use_usernames') ? ($this->settings_lib->item('auth.use_own_names') ? $this->auth->user_name() : $this->auth->username()) : $this->auth->email() ?></a>
 				<a href="<?php echo site_url('logout') ?>" id="tb_logout" title="Logout">Logout</a>
 			</div>
@@ -86,11 +88,13 @@
 	<div id="shortkeys_dialog" title="Shortcut Keys" style="display: none">
 		<p>
 			<?php echo lang('bf_keyboard_shortcuts') ?>
+			<?php if (isset($shortcut_data) && is_array($shortcut_data['shortcut_keys'])): ?>
 			<ul>
 			<?php foreach($shortcut_data['shortcut_keys'] as $key => $data): ?>
 				<li><span><?php echo $data?></span> : <?php echo $shortcut_data['shortcuts'][$key]['description']; ?></li>
 			<?php endforeach; ?>
-			</ul
+			</ul>
+			<?php endif;?>
 		</p>
 	</div>
 
