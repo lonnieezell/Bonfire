91cb6259eca602df3f7293a33d51c240d5435572
Sean Downey <sean@considerweb.com>
2012-02-25 17:08:56 +0000

    improving the ui for the edit view in the module builder output

--- bonfire/application/core_modules/modulebuilder/views/files/view_default.php
+++ bonfire/application/core_modules/modulebuilder/views/files/view_default.php
@@ -6,7 +6,7 @@ $view = '
 	<?php echo validation_errors(); ?>
 </div>
 <?php endif; ?>
-<?php // Change the css classes to suit your needs    
+<?php // Change the css classes to suit your needs
 if( isset($'.$module_name_lower.') ) {
 	$'.$module_name_lower.' = (array)$'.$module_name_lower.';
 }
@@ -14,14 +14,18 @@ $id = isset($'.$module_name_lower.'[\''.$primary_key_field.'\']) ? "/".$'.$modul
 ';
 $view .= '?>';
 $view .= '
-<?php echo form_open($this->uri->uri_string(), \'class="constrained ajax-form"\'); ?>';
+<div class="admin-box">
+	<h3>UPDATE TEXT</h3>
+<?php echo form_open($this->uri->uri_string(), \'class="constrained ajax-form"\'); ?>
+	<fieldset>
+';
 $on_click = '';
 $xinha_names = '';
 for($counter=1; $field_total >= $counter; $counter++)
 {
 	$maxlength = NULL; // reset this variable
 
-	// only build on fields that have data entered. 
+	// only build on fields that have data entered.
 	//Due to the requiredif rule if the first field is set the the others must be
 
 	if (set_value("view_field_label$counter") == NULL)
@@ -35,18 +39,18 @@ for($counter=1; $field_total >= $counter; $counter++)
 
 	if ($field_type != 'checkbox') // checkbox appears to the left of the checkbox so I can't add now for a checkbox
 	{
-		
+
 $view .= <<<EOT
 
 {$form_input_delimiters[0]}
         <?php echo form_label('{$field_label}', '{$field_name}'); ?>
 EOT;
-		
+
 	} else {
 $view .= <<<EOT
 
 {$form_input_delimiters[0]}
-	
+
 EOT;
 	}
 
@@ -54,7 +58,7 @@ EOT;
 	$validation_rules = $this->input->post('validation_rules'.$counter);
 
 	if (is_array($validation_rules))
-	{       
+	{
 		// rules have been selected for this fieldset
 
 		foreach($validation_rules as $key => $value)
@@ -65,7 +69,7 @@ EOT;
 			}
 		}
 	}
-                
+
 
 	// field type
 	switch($field_type)
@@ -74,14 +78,14 @@ EOT;
 	// Some consideration has gone into how these should be implemented
 	// I came to the conclusion that it should just setup a mere framework
 	// and leave helpful comments for the developer
-	// Modulebuilder is meant to have a minimium amount of features. 
+	// Modulebuilder is meant to have a minimium amount of features.
 	// It sets up the parts of the form that are repitive then gets the hell out
 	// of the way.
 
 	// This approach maintains these aims/goals
 
 	case('textarea'):
-		
+
 		if (!empty($textarea_editor) )
 		{
 			// if a date field hasn't been included already then add in the jquery ui files
@@ -117,9 +121,9 @@ EOT;
 		$view .= "
 ".$form_input_delimiters[1];
 		break;
-						
+
 	case('radio'):
-                        
+
 		$view .= '
 		<?php // Change or Add the radio values/labels/css classes to suit your needs ?>
 		<input id="'.$field_name.'" name="'.$field_name.'" type="radio" class="" value="option1" <?php echo set_radio(\''.$field_name.'\', \'option1\', TRUE); ?> />
@@ -130,7 +134,7 @@ EOT;
 '.$form_input_delimiters[1].'
 
 ';
-		break;                        
+		break;
 
 	case('select'):
 	// decided to use ci form helper here as I think it makes selects/dropdowns a lot easier
@@ -142,7 +146,7 @@ EOT;
 		$view .= '
 
         <?php // Change the values in this array to populate your dropdown as required ?>
-        
+
 ';
 	 $view .= '<?php $options = array(';
 		foreach( $select_options as $key => $option)
@@ -154,26 +158,26 @@ EOT;
 ); ?>
 
         <?php echo form_dropdown(\''.$field_name.'\', $options, set_value(\''.$field_name.'\', isset($'.$module_name_lower.'[\''.$field_name.'\']) ? $'.$module_name_lower.'[\''.$field_name.'\'] : \'\'))?>
-'.$form_input_delimiters[1].'                                             
+'.$form_input_delimiters[1].'
                         ';
 		break;
-                        
+
 	case('checkbox'):
 
 	$view .= <<<EOT
 
         <?php // Change the values/css classes to suit your needs ?>
-	    <?php echo form_label('{$field_label}', '{$field_name}'); ?>               
-        <input type="checkbox" id="{$field_name}" name="{$field_name}" value="1" <?php echo (isset(\${$module_name_lower}['{$field_name}']) && \${$module_name_lower}['{$field_name}'] == 1) ? 'checked="checked"' : set_checkbox('{$field_name}', 1); ?>> 
-	
-{$form_input_delimiters[1]} 
+	    <?php echo form_label('{$field_label}', '{$field_name}'); ?>
+        <input type="checkbox" id="{$field_name}" name="{$field_name}" value="1" <?php echo (isset(\${$module_name_lower}['{$field_name}']) && \${$module_name_lower}['{$field_name}'] == 1) ? 'checked="checked"' : set_checkbox('{$field_name}', 1); ?>>
+
+{$form_input_delimiters[1]}
 EOT;
 		break;
 
 	case('input'):
 	case('password'):
 	default: // input.. added bit of error detection setting select as default
-						
+
 		if ($field_type == 'input')
 		{
 			$type = 'text';
@@ -221,16 +225,17 @@ if (!empty($on_click))
 $view .= '
 
 
-	<div class="text-right">
+	<div class="form-actions">
 		<br/>
-		<input type="submit" name="submit" value="'.$action_label.' '.$module_name.'"'.$on_click.' /> or <?php echo anchor(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'\', lang(\''.$module_name_lower.'_cancel\')); ?>
+		<input type="submit" name="submit" class="btn primary" value="'.$action_label.' '.$module_name.'"'.$on_click.' /> or <?php echo anchor(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'\', lang(\''.$module_name_lower.'_cancel\')); ?>
 	</div>
+	</fieldset>
 	<?php echo form_close(); ?>
 ';
 if ($xinha_names != '')
 {
-	$view .= '				
-				<script type="text/javascript"> 
+	$view .= '
+				<script type="text/javascript">
 
 				var xinha_plugins =
 				[
@@ -257,15 +262,17 @@ if ($xinha_names != '')
 if($action_name != 'create') {
 $view .= '
 	<div class="box delete rounded">
-		<a class="button" id="delete-me" href="<?php echo site_url(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'/delete/\'. $id); ?>" onclick="return confirm(\'<?php echo lang(\''.$module_name_lower.'_delete_confirm\'); ?>\')"><?php echo lang(\''.$module_name_lower.'_delete_record\'); ?></a>
-		
 		<h3><?php echo lang(\''.$module_name_lower.'_delete_record\'); ?></h3>
-		
+
 		<p><?php echo lang(\''.$module_name_lower.'_edit_text\'); ?></p>
+
+		<a class="btn danger" id="delete-me" href="<?php echo site_url(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'/delete/\'. $id); ?>" onclick="return confirm(\'<?php echo lang(\''.$module_name_lower.'_delete_confirm\'); ?>\')"><?php echo lang(\''.$module_name_lower.'_delete_record\'); ?></a>
 	</div>
 ';
 }
 
+$view .= '
+</div>';
 
 echo $view;
 ?>
