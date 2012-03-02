88801e59768a1b31b51dadb8efaac4ca8857e070
Shawn Crigger <support@s-vizion.com>
2012-03-02 12:29:45 -0500

    Fixing theme

--- bonfire/themes/svizion/index.php
+++ bonfire/themes/svizion/index.php
@@ -1,25 +1,4 @@
 <?php
-	// Setup our default assets to load.
-	Assets::add_js( array(
-		base_url() .'assets/js/bootstrap.js',
-		base_url() .'assets/js/bootstrap-dropdown.js'
-	));
-
-	Assets::add_css( array ( base_url() .'assets/css/bootstrap.css' ));
-
-	$inline = '$(".dropdown-toggle").dropdown();';
-	$inline .= '$(".tooltips").tooltip();';
-
-	Assets::add_js( $inline, 'inline' );
-
-	Template::block('header', 'parts/head');
-
-	$uid = (int) $this->session->userdata('user_id');
-	if ( $uid > 0 )
-	{
-
-	}
-
 	echo theme_view('parts/_header');
 ?>
  <div class="container-fluid"> <!-- Start of Main Container -->
