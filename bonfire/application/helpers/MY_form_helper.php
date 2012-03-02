93c81ecc8ce1af718d97a00d89b80e13babc087d
Shawn Crigger <support@s-vizion.com>
2012-03-02 15:23:02 -0500

    Updated styles to match-up with Bootstrap 2.0

--- bonfire/application/helpers/MY_form_helper.php
+++ bonfire/application/helpers/MY_form_helper.php
@@ -2,14 +2,14 @@
 
 /*
 	File: MY_form_helper
-	
+
 	Creates HTML5 extensions for the standard CodeIgniter form helper.
-	
+
 	These functions also wrap the form elements as necessary to create
 	the styling that the Bootstrap-inspired admin theme requires to
 	make it as simple as possible for a developer to maintain styling
 	with the core. Also makes changing the core a snap.
-	
+
 	All methods (including overriden versions of the originals) now
 	support passing a final 'label' attribute that will create the
 	label along with the field.
@@ -17,41 +17,47 @@
 
 /*
 	Function: _form_common()
-	
+
 	Used by many of the new functions to wrap the input in the correct
 	tags so that the styling is automatic.
-	
+
 	Parameters:
 		$type	- A string with the name of the element type.
-		$data	- Either a string with the element name, or an array of 
+		$data	- Either a string with the element name, or an array of
 				  key/value pairs of all attributes.
 		$value	- Either a string with the value, or blank if an array is
 				  is passed to the $data param.
 		$label	- A string with the label of the element.
 		$extra	- A string with any additional items to include, like Javascript.
-		
+
 	Returns:
 		A string with the formatted input element, label tag and wrapping divs.
 */
 if (!function_exists('_form_common'))
 {
-	function _form_common($type='text', $data='', $value='', $label='', $extra='')
+	function _form_common($type='text', $data='', $value='', $label='', $extra='', $tooltip = '')
 	{
 		$defaults = array('type' => 'text', 'name' => (( ! is_array($data)) ? $data : ''), 'value' => $value);
-		
+
 		// If name is empty at this point, try to grab it from the $data array
 		if (empty($defaults['name']) && is_array($data) && isset($data['name']))
 		{
 			$defaults['name'] = $data['name'];
 			unset($data['name']);
 		}
-		
-		$output  = "<div class='clearfix'>\n";
-		$output .= "<label for='{$defaults['name']}'>$label</label>\n";
-		$output .= "<div class='input'>\n";
-		$output .= '<input '. _parse_form_attributes($data, $defaults) . $extra ." />\n";
-		$output .= "</div>\n</div>";
-		
+		$output = _parse_form_attributes($data, $defaults);
+		$output = <<<EOL
+
+	<div class="control-group">
+		<label class="control-label" for="{$defaults['name']}">{$label}</label>
+		<div class="controls">
+			 <input {$output} {$extra} />
+			{$tooltip}
+		</div>
+	</div>
+
+EOL;
+
 		return $output;
 	}
 }
@@ -60,7 +66,7 @@ if (!function_exists('_form_common'))
 
 if (!function_exists('form_input'))
 {
-	function form_input($data='', $value='', $label='', $extra='')
+	function form_input($data='', $value='', $label='', $extra='', $tooltip = '' )
 	{
 		return _form_common('text', $data, $value, $label, $extra);
 	}
@@ -70,7 +76,7 @@ if (!function_exists('form_input'))
 
 if (!function_exists('form_email'))
 {
-	function form_email($data='', $value='', $label='', $extra='')
+	function form_email($data='', $value='', $label='', $extra='', $tooltip = '' )
 	{
 		return _form_common('email', $data, $value, $label, $extra);
 	}
@@ -80,7 +86,7 @@ if (!function_exists('form_email'))
 
 if (!function_exists('form_password'))
 {
-	function form_password($data='', $value='', $label='', $extra='')
+	function form_password($data='', $value='', $label='', $extra='', $tooltip = '' )
 	{
 		return _form_common('password', $data, $value, $label, $extra);
 	}
@@ -90,7 +96,7 @@ if (!function_exists('form_password'))
 
 if (!function_exists('form_url'))
 {
-	function form_url($data='', $value='', $label='', $extra='')
+	function form_url($data='', $value='', $label='', $extra='', $tooltip = '' )
 	{
 		return _form_common('url', $data, $value, $label, $extra);
 	}
