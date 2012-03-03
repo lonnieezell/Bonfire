f3a1e87f10b5d4e069d88cb24c2cbb24228ae0ba
Sean Downey <sean@considerweb.com>
2012-03-03 21:27:25 +0000

    UI: Fixing the remove shortcut

--- bonfire/application/core_modules/ui/controllers/settings.php
+++ bonfire/application/core_modules/ui/controllers/settings.php
@@ -8,10 +8,10 @@
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in
 	all copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,21 +25,21 @@ class Settings extends Admin_Controller {
 
 	//--------------------------------------------------------------------
 
-	public function __construct() 
+	public function __construct()
 	{
 		parent::__construct();
-		
+
 		$this->auth->restrict('Bonfire.UI.Manage');
 		$this->lang->load('ui');
-		
+
 		Template::set('toolbar_title', 'UI Settings');
-		
+
 		Assets::add_module_js('ui', 'ui.js');
 	}
-	
-	//--------------------------------------------------------------------	
 
-	public function index() 
+	//--------------------------------------------------------------------
+
+	public function index()
 	{
 		if ($this->input->post('add_shortcut'))
 		{
@@ -48,7 +48,7 @@ class Settings extends Admin_Controller {
 				Template::set_message('Your settings were successfully saved.', 'success');
 				redirect(uri_string());
 			}
-			else 
+			else
 			{
 				Template::set_message('There was an error saving your settings.', 'error');
 			}
@@ -60,42 +60,42 @@ class Settings extends Admin_Controller {
 				Template::set_message('Your settings were successfully saved.', 'success');
 				redirect(uri_string());
 			}
-			else 
-			{ 
+			else
+			{
 				Template::set_message('There was an error saving your settings.', 'error');
 			}
 		}
-		
+
 		// Read our current settings from the application config
 		Template::set('current', config_item('ui.current_shortcuts'));
-		
+
 		$settings = $this->settings_lib->item('ui.shortcut_keys');
 		Template::set('settings', unserialize($settings));
 
 		Template::render();
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	// --------------------------------------------------------------------
-	
+
 	/*
 		Method: add()
-				
+
 		Parameter:
-			$role_perm	- A CSV string of the role and the permission to modify	
+			$role_perm	- A CSV string of the role and the permission to modify
 			$action		- boolean ()True = Insert, False = Delete)
-													
+
 		Return:
 			string result
 	*/
-	
+
 	public function add()
 	{
 
 		$this->form_validation->set_rules('action1', lang('ui_actions'), 'required|xss_clean');
 		$this->form_validation->set_rules('shortcut1', lang('ui_shortcuts'), 'required|callback_validate_shortcuts|xss_clean');
-		
+
 		if ($this->form_validation->run() === false)
 		{
 			return false;
@@ -103,7 +103,7 @@ class Settings extends Admin_Controller {
 
 		$action   = $this->input->post('action1');
 		$shortcut = $this->input->post('shortcut1');
-	
+
 		// Read our current settings from the application config
 		$available_actions = config_item('ui.current_shortcuts');
 		$current_settings = unserialize($this->settings_lib->item('ui.shortcut_keys'));
@@ -118,15 +118,15 @@ class Settings extends Admin_Controller {
 		}
 		return false;
 	}
-	
+
 	//--------------------------------------------------------------------
 
-	
+
 	//--------------------------------------------------------------------
 	// !PRIVATE METHODS
 	//--------------------------------------------------------------------
-	
-	public function remove() 
+
+	public function remove()
 	{
 		$this->form_validation->set_rules('remove_action', lang('ui_actions'), 'required|xss_clean');
 
@@ -134,7 +134,7 @@ class Settings extends Admin_Controller {
 		{
 			return false;
 		}
-		die('here');
+
 		$action   = $this->input->post('remove_action');
 
 		// Read our current settings from the application config
@@ -149,25 +149,25 @@ class Settings extends Admin_Controller {
 	}
 
 	//--------------------------------------------------------------------
-	
+
 	private function save_settings($settings)
-	{ 
+	{
 		$updated = $this->settings_lib->set('ui.shortcut_keys', serialize($settings));
 
 		// Log the activity
 		$this->load->model('activities/Activity_model', 'activity_model');
-		
+
 		$this->activity_model->log_activity($this->current_user->id, lang('bf_act_settings_saved').': ' . $this->input->ip_address(), 'ui');
 
 		return $updated;
 	}
 
 	//--------------------------------------------------------------------
-	
+
 	public function validate_shortcuts()
 	{
 		// Make sure that the shortcuts don't have spaces
-		
+
 		$shortcut = $this->input->post('shortcut1');
 		if (stristr($shortcut, " ") !== FALSE)
 		{
