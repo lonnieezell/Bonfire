ac0270b18d22e2c39ea215bdf732d297ebcac357
Shawn Crigger <support@s-vizion.com>
2012-03-04 23:03:36 -0500

    More work on the Admin theme, fixed Javascript issues with the 1.3/2.0 merge

--- bonfire/themes/admin/index.php
+++ bonfire/themes/admin/index.php
@@ -1,18 +1,18 @@
 <?php
-	Assets::add_js(array(
-			'plugins.js',
-			Template::theme_url('js/jwerty.js'),
-		),
-		'external',
-		true
-	);
+		Assets::add_js(array(
+																							Template::theme_url('js/bootstrap.min.js'),
+																							Template::theme_url('js/jwerty.js')
+																						),
+																	'external',
+																	true
+																);
 ?>
 <?php echo theme_view('_header'); ?>
 
 <div class="container-fluid body">
-	<?php echo Template::message(); ?>
+        <?php echo Template::message(); ?>
 
-	<?php echo Template::yield(); ?>
+        <?php echo Template::yield(); ?>
 </div>
 
 <?php echo theme_view('_footer'); ?>
--- bonfire/themes/admin/mobile__footer.php
+++ bonfire/themes/admin/mobile__footer.php
@@ -4,7 +4,7 @@
 				<div class="column size1of2">
 					<a href="<?php echo site_url(SITE_AREA .'/settings/users/edit/'. $this->current_user->id) ?>">My Account</a>
 				</div>
-				
+
 				<div class="column size1of2 last-column">
 					<p>{elapsed_time} seconds. {memory_usage}<br/>
 					Built with <a href="http://cibonfire.com" target="_blank">Bonfire</a></p>
@@ -14,7 +14,12 @@
 	</footer>
 
 	<div id="debug"><!-- Stores the Profiler Results --></div>
-	here
+	<script type="text/javascript">
+			if (typeof jQuery == 'undefined') {
+					document.write(unescape("%3Cscript src='<?php echo base_url(); ?>assets/js/jquery-1.7.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+			}
+	</script>
+
 	<?php echo Assets::js(); ?>
 </body>
 </html>
