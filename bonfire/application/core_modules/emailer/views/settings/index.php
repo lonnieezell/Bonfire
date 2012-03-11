eb68cabf1ff4442796d1bb3b3501849dedcaf759
Shawn Crigger <support@s-vizion.com>
2012-03-11 07:07:01 -0400

    Added classes to labels

--- bonfire/application/core_modules/emailer/views/settings/index.php
+++ bonfire/application/core_modules/emailer/views/settings/index.php
@@ -7,7 +7,7 @@
 		<legend>General Settings</legend>
 
 		<div class="control-group <?php echo form_error('sender_email') ? 'error' : '' ?>">
-			<label for="sender_email"><?php echo lang('em_system_email'); ?></label>
+			<label class="control-label" for="sender_email"><?php echo lang('em_system_email'); ?></label>
 			<div class="controls">
 				<input type="email" name="sender_email" class="input-xlarge" value="<?php echo set_value('sender_email', isset($sender_email) ? $sender_email : '')  ?>" />
 				<?php if (form_error('sender_email')) echo '<span class="help-inline">'. form_error('sender_email') .'</span>'; ?>
@@ -16,7 +16,7 @@
 		</div>
 
 		<div class="control-group">
-			<label for="mailtype"><?php echo lang('em_email_type'); ?></label>
+			<label class="control-label"for="mailtype"><?php echo lang('em_email_type'); ?></label>
 			<div class="controls">
 				<select name="mailtype">
 					<option value="text" <?php echo isset($mailtype) && $mailtype == 'text' ? 'selected="selected"' : ''; ?>>Text</option>
@@ -26,7 +26,7 @@
 		</div>
 
 		<div class="control-group <?php echo form_error('protocol') ? 'error' : ''; ?>">
-			<label for="protocol"><?php echo lang('em_email_server'); ?></label>
+			<label class="control-label"for="protocol"><?php echo lang('em_email_server'); ?></label>
 			<div class="controls">
 				<select name="protocol" id="server_type">
 					<option <?php echo set_select('protocol', 'mail', isset($protocol) && $protocol == 'mail' ? TRUE : FALSE); ?>>mail</option>
@@ -47,7 +47,7 @@
 
 		<!-- Sendmail -->
 		<div id="sendmail" class="control-group <?php echo form_error('mailpath') ? 'error' : ''; ?>" style="padding-top: 27px">
-			<label for="mailpath">Sendmail <?php echo lang('em_location'); ?></label>
+			<label  class="control-label" for="mailpath">Sendmail <?php echo lang('em_location'); ?></label>
 			<div class="controls">
 				<input type="text" name="mailpath" class="input-xlarge" value="<?php echo set_value('mailpath', isset($mailpath) ? $mailpath : '/usr/sbin/sendmail') ?>" />
 				<span class="help-inline"><?php echo form_error('mailpath'); ?></span>
@@ -58,7 +58,7 @@
 		<div id="smtp" style="padding-top: 27px">
 
 			<div class="control-group <?php echo form_error('smtp_host') ? 'error' : ''; ?>">
-				<label for="smtp_host">SMTP <?php echo lang('em_server_address'); ?></label>
+				<label class="control-label" for="smtp_host">SMTP <?php echo lang('em_server_address'); ?></label>
 				<div class="controls">
 					<input type="text" name="smtp_host" class="input-xlarge" value="<?php echo set_value('smtp_host', isset($smtp_host) ? $smtp_host : '') ?>" />
 		    	    <span class="help-inline"><?php echo form_error('smtp_host'); ?></span>
@@ -66,28 +66,28 @@
 			</div>
 
 			<div class="control-group">
-				<label for="smtp_user">SMTP <?php echo lang('bf_username'); ?></label>
+				<label class="control-label" for="smtp_user">SMTP <?php echo lang('bf_username'); ?></label>
 				<div class="controls">
 					<input type="text" name="smtp_user" class="input-xlarge" value="<?php echo set_value('smtp_user', isset($smtp_user) ? $smtp_user : '') ?>" />
 				</div>
 			</div>
 
 			<div class="control-group">
-				<label for="smtp_pass">SMTP <?php echo lang('bf_password'); ?></label>
+				<label class="control-label" for="smtp_pass">SMTP <?php echo lang('bf_password'); ?></label>
 				<div class="controls">
 					<input type="password" name="smtp_pass" class="input-xlarge" value="<?php echo set_value('smtp_pass', isset($smtp_pass) ? $smtp_pass : '') ?>" />
 				</div>
 			</div>
 
 			<div class="control-group">
-				<label for="smtp_port">SMTP <?php echo lang('em_port'); ?></label>
+				<label class="control-label" for="smtp_port">SMTP <?php echo lang('em_port'); ?></label>
 				<div class="controls">
 					<input type="text" name="smtp_port" class="input-xlarge" value="<?php echo set_value('smtp_port', isset($smtp_port) ? $smtp_port : 25) ?>" />
 				</div>
 			</div>
 
 			<div class="control-group">
-				<label for="smptp_timeout">SMTP <?php echo lang('em_timeout_secs'); ?></label>
+				<label class="control-label" for="smptp_timeout">SMTP <?php echo lang('em_timeout_secs'); ?></label>
 				<div class="controls">
 					<input type="text" name="smtp_timeout" class="input-xlarge" value="<?php echo set_value('smtp_timeout', isset($smtp_timeout) ? $smtp_timeout : '') ?>" />
 				</div>
@@ -116,7 +116,7 @@
 
 			<br/>
 			<div class="control-group">
-				<label for="email"><?php echo lang('bf_email'); ?></label>
+				<label class="control-label" for="email"><?php echo lang('bf_email'); ?></label>
 				<div class="controls">
 					<input type="email" name="test_email" id="test-email" value="<?php echo config_item('site.system_email') ?>" />
 					<input type="submit" name="submit" class="btn btn-primary" value="<?php echo lang('em_test_button'); ?>" />
