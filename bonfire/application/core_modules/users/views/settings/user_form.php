4d4a0d1a131c64490d9a57ee714054106ab68622
Shawn Crigger <support@s-vizion.com>
2012-03-11 06:22:16 -0400

    Added class to form labels

--- bonfire/application/core_modules/users/views/settings/user_form.php
+++ bonfire/application/core_modules/users/views/settings/user_form.php
@@ -1,6 +1,6 @@
 <?php if (isset($user) && $user->banned) : ?>
-<div class="alert alert-warning">
-	<?php echo lang('us_banned_admin_note'); ?>
+<div class="alert alert-warning fade in">
+	<h4 class="alert-heading"><?php echo lang('us_banned_admin_note'); ?></h4>
 </div>
 <?php endif; ?>
 
@@ -9,69 +9,69 @@
 	<h3><?php echo $toolbar_title ?></h3>
 
 	<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
-	
+
 	<fieldset>
 		<legend><?php echo lang('us_account_details') ?></legend>
-	
+
 		<div class="control-group <?php echo form_error('email') ? 'error' : '' ?>">
-			<label for="email"><?php echo lang('bf_email') ?></label>
+			<label for="email" class="control-label"><?php echo lang('bf_email') ?></label>
 			<div class="controls">
 				<input type="email" name="email" value="<?php echo isset($user) ? $user->email : set_value('email') ?>">
 				<?php if (form_error('email')) echo '<span class="help-inline">'. form_error('email') .'</span>'; ?>
 			</div>
 		</div>
-		
+
 		<div class="control-group <?php echo form_error('username') ? 'error' : '' ?>">
-			<label for="username"><?php echo lang('bf_username') ?></label>
+			<label for="username" class="control-label"><?php echo lang('bf_username') ?></label>
 			<div class="controls">
 				<input type="text" name="username" value="<?php echo isset($user) ? $user->username : set_value('username') ?>">
 				<?php if (form_error('username')) echo '<span class="help-inline">'. form_error('username') .'</span>'; ?>
 			</div>
 		</div>
-		
+
 		<div class="control-group <?php echo form_error('display_name') ? 'error' : '' ?>">
-			<label for="display_name"><?php echo lang('bf_display_name') ?></label>
+			<label for="display_name" class="control-label"><?php echo lang('bf_display_name') ?></label>
 			<div class="controls">
 				<input type="text" name="display_name" value="<?php echo isset($user) ? $user->display_name : set_value('display_name') ?>">
 				<?php if (form_error('display_name')) echo '<span class="help-inline">'. form_error('display_name') .'</span>'; ?>
 			</div>
 		</div>
-		
+
 		<div class="control-group <?php echo form_error('password') ? 'error' : '' ?>">
-			<label for="username"><?php echo lang('bf_password') ?></label>
+			<label for="username" class="control-label"><?php echo lang('bf_password') ?></label>
 			<div class="controls">
 				<input type="password" name="password" value="">
 				<?php if (form_error('password')) echo '<span class="help-inline">'. form_error('password') .'</span>'; ?>
 			</div>
 		</div>
-		
+
 		<div class="control-group <?php echo form_error('pass_confirm') ? 'error' : '' ?>">
-			<label for="username"><?php echo lang('bf_password_confirm') ?></label>
+			<label class="control-label" for="username"><?php echo lang('bf_password_confirm') ?></label>
 			<div class="controls">
 				<input type="password" name="pass_confirm" value="">
 				<?php if (form_error('pass_confirm')) echo '<span class="help-inline">'. form_error('pass_confirm') .'</span>'; ?>
 			</div>
 		</div>
-		
+
 		<?php if (has_permission('Bonfire.Roles.Manage')) :?>
 		<fieldset>
 			<legend><?php echo lang('us_role'); ?></legend>
-			
+
 			<div class="control-group">
-				<label for="role_id"><?php echo lang('us_role'); ?></label>
+				<label for="role_id" class="control-label"><?php echo lang('us_role'); ?></label>
 				<div class="controls">
 					<select name="role_id">
 					<?php if (isset($roles) && is_array($roles) && count($roles)) : ?>
 						<?php foreach ($roles as $role) : ?>
-						
+
 							<?php if (has_permission('Permissions.'. ucfirst($role->role_name) .'.Manage')) : ?>
-							
+
 							<option value="<?php echo $role->role_id ?>" <?php echo isset($user) && $user->role_id == $role->role_id ? 'selected="selected"' : '' ?> <?php echo !isset($user) && $role->default == 1 ? 'selected="selected"' : ''; ?>>
 								<?php echo ucfirst($role->role_name) ?>
 							</option>
-							
+
 							<?php endif; ?>
-						
+
 						<?php endforeach; ?>
 					<?php endif; ?>
 					</select>
@@ -79,13 +79,13 @@
 			</div>
 		</fieldset>
 		<?php endif; ?>
-		
-		<?php 
+
+		<?php
 			// Allow modules to render custom fields
 			Events::trigger('render_user_form');
 		?>
-		
-		
+
+
 		<?php if (isset($user) && has_permission('Permissions.'. ucfirst($user->role_name).'.Manage') && $user->id != $this->auth->user_id() && ($user->banned || $user->deleted)) : ?>
 		<fieldset>
 			<legend><?php echo lang('us_account_status') ?></legend>
@@ -99,7 +99,7 @@
 					</label>
 				</div>
 			</div>
-			
+
 			<?php elseif ($user->banned) :?>
 			<div class="control-group">
 				<div class="controls">
@@ -110,15 +110,15 @@
 				</div>
 			</div>
 			<?php endif; ?>
-			
+
 		</fieldset>
 		<?php endif; ?>
-		
-		
+
+
 		<div class="form-actions">
 			<input type="submit" name="submit" class="btn btn-primary" value="<?php echo lang('bf_action_save') .' '. lang('bf_user') ?> " /> <?php echo lang('bf_or') ?> <?php echo anchor(SITE_AREA .'/settings/users', lang('bf_action_cancel')); ?>
 		</div>
-	
+
 	<?php echo form_close(); ?>
 
 </div>
