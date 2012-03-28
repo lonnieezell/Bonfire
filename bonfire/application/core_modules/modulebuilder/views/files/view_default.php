8947595f70d55310f9f9d4768ee7d04ee63dc8a0
Shawn Crigger <support@s-vizion.com>
2012-03-28 19:06:36 -0400

    Fixing wrong lang field on delete

--- bonfire/application/core_modules/modulebuilder/views/files/view_default.php
+++ bonfire/application/core_modules/modulebuilder/views/files/view_default.php
@@ -205,7 +205,7 @@ if($action_name != 'create') {
 
 			or <a class="btn btn-danger" id="delete-me" href="
 			<?php echo SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'/\'delete/\'. $id;?>" onclick="return confirm(\'<?php echo lang(\''.$module_name_lower.'_delete_confirm\'); ?>\')" name="delete-me">
-			<i class="icon-trash icon-white">&nbsp;</i>&nbsp;<?php echo lang(\''.$module_name_lower.'_delete\'); ?>
+			<i class="icon-trash icon-white">&nbsp;</i>&nbsp;<?php echo lang(\''.$module_name_lower.'_delete_record\'); ?>
 			</a>
 
 	<?php endif; ?>
