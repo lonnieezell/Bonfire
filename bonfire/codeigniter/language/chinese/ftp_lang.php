087df1aee7ee5672969dbdbc16a47d66b4ac8474
Eric Yang <yl.fighter@gmail.com>
2012-04-22 00:09:14 +0800

    add ci chinese

---
+++ bonfire/codeigniter/language/chinese/ftp_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['ftp_no_connection'] = '无法定位有效的连接ID. 在执行文件操作前,请先确认您已连接成功.';
+$lang['ftp_unable_to_connect'] = '无法连接FTP服务器.';
+$lang['ftp_unable_to_login'] = '无法登录FTP服务器.  请检查用户名和密码.';
+$lang['ftp_unable_to_makdir'] = '无法创建您指定的目录.';
+$lang['ftp_unable_to_changedir'] = '无法切换目录.';
+$lang['ftp_unable_to_chmod'] = '无法设置文件权限. 请检查您的路径. 注意:此功能仅适用与PHP5及以上版本.';
+$lang['ftp_unable_to_upload'] = '无法上传指定的文件. 请检查路径.';
+$lang['ftp_unable_to_download'] = '无法下载指定的文件. 请检查路径.';
+$lang['ftp_no_source_file'] = '无法定位源文件. 请检查路径.';
+$lang['ftp_unable_to_rename'] = '无法更改文件名称.';
+$lang['ftp_unable_to_delete'] = '无法删除文件.';
+$lang['ftp_unable_to_move'] = '无法移动文件. 请确认目标目录是否存在.';
+?>
---
+++ bonfire/codeigniter/language/chinese/upload_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['upload_userfile_not_set'] = '无法在post中找到名为userfile的变量.';
+$lang['upload_file_exceeds_limit'] = '上传文件超出PHP配置文件中允许的最大长度.';
+$lang['upload_file_exceeds_form_limit'] = '上传文件超出表单允许的最大长度.';
+$lang['upload_file_partial'] = '此文件仅有一部分被上传了.';
+$lang['upload_no_temp_directory'] = '找不到临时目录.';
+$lang['upload_unable_to_write_file'] = '文件无法写入磁盘.';
+$lang['upload_stopped_by_extension'] = '此文件的扩展名在禁止上传之列.';
+$lang['upload_no_file_selected'] = '您没有选择要上传的文件.';
+$lang['upload_invalid_filetype'] = '此文件的类型在禁止上传之列.';
+$lang['upload_invalid_filesize'] = '此文件超出允许上传的最大长度.';
+$lang['upload_invalid_dimensions'] = '您要上传的图片高度或宽度超出了限制.';
+$lang['upload_destination_error'] = '在移动上传文件到目标位置时发生了错误.';
+$lang['upload_no_filepath'] = '上传路径无效.';
+$lang['upload_no_file_types'] = '您还没有指定允许的文件类型.';
+$lang['upload_bad_filename'] = '服务器上已存在与您上传的文件同名的文件.';
+$lang['upload_not_writable'] = '上传目录不可写入.';
+?>
