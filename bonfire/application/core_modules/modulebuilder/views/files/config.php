26d3cccc472fca71f4e945f6bc5994dc2879c520
Sean Downey <sean@considerweb.com>
2012-05-01 22:25:14 +0100

    fixes #419 Escaping description string in module config file

--- bonfire/application/core_modules/modulebuilder/views/files/config.php
+++ bonfire/application/core_modules/modulebuilder/views/files/config.php
@@ -3,7 +3,7 @@
 $build_config = '<?php if ( ! defined(\'BASEPATH\')) exit(\'No direct script access allowed\');';
 
 $build_config .= '$config[\'module_config\'] = array(
-	\'description\'	=> \''.$module_description.'\',
+	\'description\'	=> \''.addslashes($module_description).'\',
 	\'name\'		=> \''.$module_name.'\',
 	\'version\'		=> \'0.0.1\',
 	\'author\'		=> \''.$username.'\'
