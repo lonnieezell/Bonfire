16b1ec93bb2741487656ff76424a94582241b78d
Mat Whitney <mwhitney@mail.sdsu.edu>
2012-06-01 11:32:02 -0700

    Replaced raw SQL fields in ModuleBuilder DB Migration
    with fields array passed to dbforge->add_field().
    
    Also added a conditional to allow nullable fields, but disabled it until
    it can be implemented/tested at a later date.

--- bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
+++ bonfire/application/core_modules/modulebuilder/views/files/db_migration.php
@@ -6,7 +6,7 @@ $no_length = array('TEXT', 'BOOL', 'DATE', 'DATETIME', 'TIMESTAMP', 'BLOB', 'TIN
 if(!$table_as_field_prefix)
 {
 	$module_name_lower = '';
-} 
+}
 else
 {
 	$module_name_lower = $module_name_lower .'_';
@@ -15,12 +15,18 @@ else
 $db_migration = '<?php if (!defined(\'BASEPATH\')) exit(\'No direct script access allowed\');
 
 class Migration_Install_'.$table_name.' extends Migration {
-	
-	public function up() 
+
+	public function up()
 	{
 		$prefix = $this->db->dbprefix;
 
-		$this->dbforge->add_field(\'`'.$primary_key_field.'` int(11) NOT NULL AUTO_INCREMENT\');';
+		$fields = array(
+			\''.$primary_key_field.'\' => array(
+				\'type\' => \'INT\',
+				\'constraint\' => 11,
+				\'auto_increment\' => TRUE,
+			),';
+
 	for($counter=1; $field_total >= $counter; $counter++)
 	{
 		//Due to the requiredif rule if the first field is set the the others must be
@@ -30,37 +36,53 @@ class Migration_Install_'.$table_name.' extends Migration {
 		}
 
 		$db_migration .= '
-		$this->dbforge->add_field("`'.$module_name_lower.set_value("view_field_name$counter").'` '.addcslashes(set_value("db_field_type$counter"),'"');
-		
+			\''.$module_name_lower.set_value("view_field_name$counter").'\' => array(
+				\'type\' => \''.addcslashes(set_value("db_field_type$counter"),'"').'\',
+				';
+
 		if (!in_array(set_value("db_field_type$counter"), $no_length))
 		{
-			$db_migration .= '('.addcslashes($this->input->post("db_field_length_value$counter"),'"').')';
+			$db_migration .= '\'constraint\' => '.addcslashes($this->input->post("db_field_length_value$counter"),'"').',
+				';
 		}
 
-		$db_migration .= ' NOT NULL';
+		// NOT NULL is the default when using the fields array,
+		// but should probably be set based on user input rather than assumed,
+		// replace FALSE with a proper conditional
+		if (FALSE) {
+			$db_migration .= '\'null\' => TRUE,
+				';
+		}
 
 		// set defaults for certain field types
 		switch (set_value("db_field_type$counter"))
 		{
 			case 'DATE':
-				$db_migration .= " DEFAULT '0000-00-00'";
+				$db_migration .= '\'default\' => \'0000-00-00\',
+				';
 				break;
 			case 'DATETIME':
-				$db_migration .= " DEFAULT '0000-00-00 00:00:00'";
+				$db_migration .= '\'default\' => \'0000-00-00 00:00:00\',
+				';
 				break;
 			default:
 				break;
 		}
 
-		$db_migration .= '");';
+		$db_migration .= '
+			),';
 
 	}
-		
+
 	// use soft deletes? Add deleted field.
 	if ($this->input->post('use_soft_deletes') == 'true')
 	{
 		$db_migration .= '
-		$this->dbforge->add_field("`deleted` TINYINT(1) NOT NULL DEFAULT \'0\'");';
+			\'deleted\' => array(
+				\'type\' => \'TINYINT\',
+				\'constraint\' => 1,
+				\'default\' => \'0\',
+			),';
 	}
 
 	// use the created field? Add field and custom name if chosen.
@@ -68,7 +90,10 @@ class Migration_Install_'.$table_name.' extends Migration {
 	{
 		$created_field = ($this->input->post('created_field')) ? $this->input->post('created_field') : 'created_on';
 		$db_migration .= '
-		$this->dbforge->add_field("`'.$created_field.'` datetime NOT NULL DEFAULT \'0000-00-00 00:00:00\'");';
+			\''.$created_field.'\' => array(
+				\'type\' => \'datetime\',
+				\'default\' => \'0000-00-00 00:00:00\',
+			),';
 	}
 
 	// use the created field? Add field and custom name if chosen.
@@ -76,27 +101,32 @@ class Migration_Install_'.$table_name.' extends Migration {
 	{
 		$modified_field = ($this->input->post('modified_field')) ? $this->input->post('modified_field') : 'modified_on';
 		$db_migration .= '
-		$this->dbforge->add_field("`'.$modified_field.'` datetime NOT NULL DEFAULT \'0000-00-00 00:00:00\'");';
+			\''.$modified_field.'\' => array(
+				\'type\' => \'datetime\',
+				\'default\' => \'0000-00-00 00:00:00\',
+			),';
 	}
 
 	$db_migration .= '
+		);
+		$this->dbforge->add_field($fields);
 		$this->dbforge->add_key(\''.$primary_key_field.'\', true);
 		$this->dbforge->create_table(\''.$table_name.'\');
 
 	}
-	
+
 	//--------------------------------------------------------------------
-	
-	public function down() 
+
+	public function down()
 	{
 		$prefix = $this->db->dbprefix;
 
 		$this->dbforge->drop_table(\''.$table_name.'\');
 
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 }';
 
 echo $db_migration;
