62cc0b4740dac21295565a8683b03fd2be212385
Shawn Crigger <support@s-vizion.com>
2012-06-05 12:35:40 -0400

    Added settings text into lang files for easier translation.

--- bonfire/application/core_modules/settings/language/english/settings_lang.php
+++ bonfire/application/core_modules/settings/language/english/settings_lang.php
@@ -3,3 +3,5 @@
 $lang['set_allow_name_change_note']	= 'Allow users to change their display name after registering?';
 $lang['set_name_change_frequency']	= ' Changes every';
 $lang['set_days']					= ' days';
+$lang['settings_saved_success']     = 'Your settings were successfully saved.';
+$lang['settings_error_success']     = 'There was an error saving your settings.';
