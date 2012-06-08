94f1d93a5ec3761efbf9b9092109e82a1f78bbce
Mat Whitney <mwhitney@mail.sdsu.edu>
2012-06-08 09:51:33 -0700

    ModuleBuilder fixes
    - db migration: add prefix to table name in create_table and drop_table
    statements
    - view_default: remove / before site_url in delete button
    - view_index: fix colspan on table footer when using soft deletes,
    created, or modified fields

--- bonfire/application/core_modules/modulebuilder/views/files/view_default.php
+++ bonfire/application/core_modules/modulebuilder/views/files/view_default.php
@@ -212,7 +212,7 @@ if($action_name != 'create') {
     $delete = PHP_EOL . '
     <?php if ($this->auth->has_permission(\''.$delete_permission.'\')) : ?>
 
-            or <a class="btn btn-danger" id="delete-me" href="/<?php echo site_url(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'/delete/\'. $id);?>" onclick="return confirm(\'<?php echo lang(\''.$module_name_lower.'_delete_confirm\'); ?>\')" name="delete-me">
+            or <a class="btn btn-danger" id="delete-me" href="<?php echo site_url(SITE_AREA .\'/'.$controller_name.'/'.$module_name_lower.'/delete/\'. $id);?>" onclick="return confirm(\'<?php echo lang(\''.$module_name_lower.'_delete_confirm\'); ?>\')" name="delete-me">
             <i class="icon-trash icon-white">&nbsp;</i>&nbsp;<?php echo lang(\''.$module_name_lower.'_delete_record\'); ?>
             </a>
 
