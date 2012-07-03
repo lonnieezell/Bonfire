b4f6a483c6428fe7b1c6accffd6ee36a897e8ebc
Sean <sean@considerweb.com>
2012-07-03 18:43:46 +0100

    ModuleBuilder: Adding an id to the subnav to allow for the new shortcut

--- bonfire/application/core_modules/modulebuilder/views/files/view_sub_nav.php
+++ bonfire/application/core_modules/modulebuilder/views/files/view_sub_nav.php
@@ -4,7 +4,7 @@ $controller_name_lower = strtolower($controller_name);
 $view = <<<END
 <ul class="nav nav-pills">
 	<li <?php echo \$this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/{$controller_name_lower}/{$module_name_lower}') ?>"><?php echo lang('{$module_name_lower}_list'); ?></a>
+		<a href="<?php echo site_url(SITE_AREA .'/{$controller_name_lower}/{$module_name_lower}') ?>" id="list"><?php echo lang('{$module_name_lower}_list'); ?></a>
 	</li>
 	<?php if (\$this->auth->has_permission('{create_permission}')) : ?>
 	<li <?php echo \$this->uri->segment(4) == 'create' ? 'class="active"' : '' ?> >
