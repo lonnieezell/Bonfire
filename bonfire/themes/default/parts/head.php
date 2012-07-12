a646ecad8b616d66f46cc888a9144ac82cc7d2e6
Sean <sean@considerweb.com>
2012-07-12 12:08:35 +0100

    Fixing incorrect path to favicon.ico which generated a 404

--- bonfire/themes/default/parts/head.php
+++ bonfire/themes/default/parts/head.php
@@ -11,7 +11,7 @@
 				<?php echo Assets::css(); ?>
 
     <!-- iPhone and Mobile favicon's and touch icons -->
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico">
     <link rel="apple-touch-icon" href="<?php echo base_url(); ?>assets/images/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>assets/images/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>assets/images/apple-touch-icon-114x114.png">
