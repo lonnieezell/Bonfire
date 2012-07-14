0d1eb320c3b072298e3756e8876964d705c39727
Steve Wicks <swicks@devicesoftware.com>
2012-07-14 11:55:21 +0100

    Updated todo tutorial
    Signed-off-by: Steve Wicks <swicks@devicesoftware.com>

--- bonfire/themes/admin/partials/_header.php
+++ bonfire/themes/admin/partials/_header.php
@@ -21,6 +21,11 @@
 	<?php echo Assets::css(null, true); ?>
 
 	<script src="<?php echo Template::theme_url('js/modernizr-2.5.3.js'); ?>"></script>
+    
+    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo js_path(); ?>jquery-1.7.2.min.js"><\/script>')</script>
+
 </head>
 <body class="desktop">
 <!--[if lt IE 7]>
