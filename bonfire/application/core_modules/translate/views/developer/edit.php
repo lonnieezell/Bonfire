16d9fab3c67d670cdf832b8158b86376fce8f0f3
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-07-25 12:40:06 +0100

    translate: cleanup to not use input->get() and input->post() together
    It seems to work fine, but it's surprising to see forms submitted to
    urls which have query strings in them.  Let's replace the query string
    parameters with normal CodeIgniter parameters.
    
    This means the parameter can't be handled in shared code in the
    constructor.  But it was only shared by two methods, and the real
    common code was limited to a single line.  Moving code into index()
    allows duplication to be reduced, so there's an overall reduction in
    lines of code.
    
    This also removes several & characters which had not been correctly
    escaped for HTML.

--- bonfire/application/core_modules/translate/views/developer/edit.php
+++ bonfire/application/core_modules/translate/views/developer/edit.php
@@ -9,7 +9,7 @@ form .form-actions { padding-left: 43% !important; }
 
 <?php if (isset($orig) && is_array($orig) && count($orig)) : ?>
 
-	<?php echo form_open(current_url() .'?lang='. htmlentities($trans_lang) .'&file='. $lang_file, 'class="form-horizontal"'); ?>
+	<?php echo form_open(current_url(), 'class="form-horizontal"'); ?>
 		<input type="hidden" name="trans_lang" value="<?php e($trans_lang) ?>" />
 		
 		<fieldset>
@@ -26,7 +26,7 @@ form .form-actions { padding-left: 43% !important; }
 		
 		<div class="form-actions">
 			<input type="submit" name="submit" class="btn btn-primary" value="<?php e(lang('bf_action_save')) ?>" /> <?php e(lang('bf_or')) ?> 
-			<a href="<?php echo site_url(SITE_AREA .'/developer/translate?lang='. $trans_lang); ?>">
+			<a href="<?php echo site_url(SITE_AREA .'/developer/translate/index/'. $trans_lang); ?>">
 				<?php e(lang('bf_action_cancel'), 'class="btn btn-danger"'); ?>
 			</a>
 		</div>
