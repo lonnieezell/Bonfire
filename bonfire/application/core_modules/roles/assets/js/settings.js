e4f428f96fde9c17aef47fdcd8f539753e923286
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-07-25 10:18:18 +0100

    roles settings: fix permission matrix clickable headers
    Detected by HTML5 validator.
    (cellIndex is not a valid HTML attribute).
    
    We were also setting it to the wrong values,
    so clicking on the header affected the wrong column.
    
    Since we're using jquery .eq() to select the cells in the table column,
    let's use jquery .index() to determine the index of our column.

--- bonfire/application/core_modules/roles/assets/js/settings.js
+++ bonfire/application/core_modules/roles/assets/js/settings.js
@@ -60,7 +60,7 @@ $('.matrix th a').click(function(){
 	var checked 	= false;
 	var found		= false;
 	var checkbox	= false;
-	var columnIndex	= $(this).parent('th').attr('cellIndex');
+	var columnIndex	= $(this).parent('th').index();
 			
 	for (i=0; i < rows.length; i++)
 	{
