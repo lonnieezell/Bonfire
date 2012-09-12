78274bd83ed86b3f85cc10f601d0d738ac6cb3db
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-09-12 11:58:23 +0100

    builder generating view_default: code readability: use more heredocs
    Some types of field were already generated using heredocs so this makes
    it more consistent, and it's easier to read .

--- bonfire/application/core_modules/builder/views/files/view_default.php
+++ bonfire/application/core_modules/builder/views/files/view_default.php
@@ -94,27 +94,26 @@ EOT;
                 }
 
             }
-            $view .= "
-            <?php echo form_textarea( array( 'name' => '$field_name', 'id' => '$field_name', 'rows' => '5', 'cols' => '80', 'value' => set_value('$field_name', isset(\${$module_name_lower}['{$field_name}']) ? \${$module_name_lower}['{$field_name}'] : '') ) )?>";
-            $view .= '
-            <span class="help-inline"><?php echo form_error(\''.$field_name.'\'); ?></span>';
-            $view .= "
-        ".$form_input_delimiters[1];
+            $view .= <<<EOT
+            <?php echo form_textarea( array( 'name' => '{$field_name}', 'id' => '{$field_name}', 'rows' => '5', 'cols' => '80', 'value' => set_value('$field_name', isset(\${$module_name_lower}['{$field_name}']) ? \${$module_name_lower}['{$field_name}'] : '') ) )?>
+            <span class="help-inline"><?php echo form_error('{$field_name}'); ?></span>
+        {$form_input_delimiters[1]}
+EOT;
             break;
 
         case('radio'):
 
-            $view .= '
+            $view .= <<<EOT
         <label class="radio">
-            <input id="'.$field_name.'" name="'.$field_name.'" type="radio" class="" value="option1" <?php echo set_radio(\''.$field_name.'\', \'option1\', TRUE); ?> />
-            '. form_label('Radio option 1', $field_name) .'
-            <input id="'.$field_name.'" name="'.$field_name.'" type="radio" class="" value="option2" <?php echo set_radio(\''.$field_name.'\', \'option2\'); ?> />
-            '. form_label('Radio option 2', $field_name) .'
-            <span class="help-inline"><?php echo form_error(\''.$field_name.'\'); ?></span>
+            <input id="{$field_name}" name="{$field_name}" type="radio" class="" value="option1" <?php echo set_radio('{$field_name}', 'option1', TRUE); ?> />
+            <?php echo form_label('Radio option 1', '{$field_name}') ?>
+            <input id="'.$field_name.'" name="{$field_name}" type="radio" class="" value="option2" <?php echo set_radio('{$field_name}', 'option2'); ?> />
+            <?php echo form_label('Radio option 2', '{$field_name}') ?>
+            <span class="help-inline"><?php echo form_error('{$field_name}'); ?></span>
             </label>
-        '.$form_input_delimiters[1].'
+        {$form_input_delimiters[1]}
 
-';
+EOT;
             break;
 
         case('select'):
@@ -135,10 +134,11 @@ EOT;
                 $view .= '
                 '.strip_slashes($option).' => '.strip_slashes($option).',';
             }
-            $view .= '
+            $view .= <<<EOT
 ); ?>
 
-        <?php echo form_dropdown(\''.$field_name.'\', $options, set_value(\''.$field_name.'\', isset($'.$module_name_lower.'[\''.$field_name.'\']) ? $'.$module_name_lower.'[\''.$field_name.'\'] : \'\'), \''.$field_label.'\''.$required.')?>';
+        <?php echo form_dropdown('{$field_name}', \$options, set_value('{$field_name}', isset(\${$module_name_lower}['{$field_name}']) ? \${$module_name_lower}['{$field_name}'] : ''), '{$field_label}'{$required})?>
+EOT;
             break;
 
         case('checkbox'):
