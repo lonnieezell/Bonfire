686f349477419425248eddaee341e97e9a953732
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-09-27 09:36:37 +0100

    install: more readable delimiters when using preg_replace() on a url
    Use an alternative delimiter to avoid having to escape slashes in the url.
    PHP code often uses '#' as an alternative delimiter but I think '{' is
    more readable in this case, to avoid having the string end with '$#'.
    
    http://stackoverflow.com/a/6076567/799204
    > Damian Conway in "Perl Best Practices" asserts that m{} is the only
    > alternate delimiter that ought to be used, and this is reinforced by
    > Perl::Critic (on CPAN). While you can get away with using a variety of
    > alternate delimiter characters, // and {} seem to be the clearest to
    > decipher later on.

--- install/views/install/success.php
+++ install/views/install/success.php
@@ -6,6 +6,6 @@
 <p><?php echo lang('in_success_rebase_msg').$rebase;?></p>
 <?php endif;?>
 
-<?php echo lang('in_success_msg'). ' <a href="'. preg_replace('/install\/$/', '', site_url()) .'">Bonfire</a>.'; ?>
+<?php echo lang('in_success_msg'). ' <a href="'. preg_replace('{install/$}', '', site_url()) .'">Bonfire</a>.'; ?>
 
 <?php $this->load->view('footer'); ?>
