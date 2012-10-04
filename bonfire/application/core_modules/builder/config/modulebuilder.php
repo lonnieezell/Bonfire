d622bd48d39e6350daec191f805a7ff8009cfb3a
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-10-04 14:30:52 +0100

    modulebuilder: remove the xss_clean option
    xss_clean is removed from the list of validation options in
    modulebuilder.
    
    As mentioned previouslly, it has very limited scope, and since it
    doesn't actually work we should not be endorsing it.
    
    (I haven't stopped modulebuilder using xss_clean internally though ;).

--- bonfire/application/core_modules/builder/config/modulebuilder.php
+++ bonfire/application/core_modules/builder/config/modulebuilder.php
@@ -16,7 +16,7 @@ $config[ 'modulebuilder' ][ 'form_action_options' ] = array('index' => 'List',
 /*
  * valiation rules which will appear
  */
-$config[ 'modulebuilder' ][ 'validation_rules' ] = array('required', 'unique', 'trim', 'xss_clean');
+$config[ 'modulebuilder' ][ 'validation_rules' ] = array('required', 'unique', 'trim');
 $config[ 'modulebuilder' ][ 'validation_limits' ] = array('alpha', 'is_numeric', 'alpha_numeric', 'alpha_dash', 'valid_email', 'integer', 'is_decimal', 'is_natural', 'is_natural_no_zero','valid_ip','valid_base64','alpha_extra');
 														
 /*
