5411e450bfdb2e2df42a201441414fedf849faa2
Sean <sean@considerweb.com>
2012-10-16 08:57:20 +0100

    Fixing the migrations path

--- bonfire/modules/migrations/config/migrations.php
+++ bonfire/modules/migrations/config/migrations.php
@@ -19,13 +19,13 @@ $config['migrations_enabled'] = true;
 | Migrations version
 |--------------------------------------------------------------------------
 |
-| This is used to set the default migration for this code base. 
+| This is used to set the default migration for this code base.
 | Sometimes you want the system to automaticly migrate the database
 | to the most current migration. Or there might be higher migrations
-| that are not part of the migration-> env. Setting the migration does 
+| that are not part of the migration-> env. Setting the migration does
 | does nothing here. It is a way for a programer to check the config.
 |
-| On login you might want to do something like this 
+| On login you might want to do something like this
 | $this->migration->version($this->config->item('migrations_version'));
 |
 */
@@ -42,4 +42,4 @@ $config['migrations_version'] = 1;
 | Also, writing permission is required within the migrations path.
 |
 */
-$config['migrations_path'] = APPPATH . 'db/migrations/';
+$config['migrations_path'] = APPPATH .'../bonfire/migrations/';
