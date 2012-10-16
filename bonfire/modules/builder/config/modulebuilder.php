6344773564bc07fa9650f650d23dcce0ad907f3c
Sean <sean@considerweb.com>
2012-10-16 09:13:36 +0100

    Adding a modules folder to the application and changing the path when the Builder creates the modules

--- bonfire/modules/builder/config/modulebuilder.php
+++ bonfire/modules/builder/config/modulebuilder.php
@@ -3,13 +3,13 @@
 /*
  * Output path of the modules
  */
-$config[ 'modulebuilder' ]['output_path'] = APPPATH."../modules/";
+$config[ 'modulebuilder' ]['output_path'] = APPPATH."/modules/";
 /*
  * form actions which will appear in the list
  */
-$config[ 'modulebuilder' ][ 'form_action_options' ] = array('index' => 'List', 
+$config[ 'modulebuilder' ][ 'form_action_options' ] = array('index' => 'List',
 														'create' => 'Create',
-														'edit' => 'Edit', 
+														'edit' => 'Edit',
 														'delete' => 'Delete');
 
 
@@ -18,7 +18,7 @@ $config[ 'modulebuilder' ][ 'form_action_options' ] = array('index' => 'List',
  */
 $config[ 'modulebuilder' ][ 'validation_rules' ] = array('required', 'unique', 'trim', 'xss_clean');
 $config[ 'modulebuilder' ][ 'validation_limits' ] = array('alpha', 'is_numeric', 'alpha_numeric', 'alpha_dash', 'valid_email', 'integer', 'is_decimal', 'is_natural', 'is_natural_no_zero','valid_ip','valid_base64','alpha_extra');
-														
+
 /*
  * default primary key field
  */
