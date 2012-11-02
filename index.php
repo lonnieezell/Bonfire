39b243cac7a877cf6416dfdca621ea1882470c43
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-02 10:30:26 +0000

    Use "index.php? style urls in installer, to prevent #490
    "installer infinite loop redirect"
    
    Suggested by @svizion
    Prompted by @stasoft
    Cause confirmed by @thewebwing
    
    The installer tells you how to set RewriteBase for your Bonfire install.
    Therefore it was wrong for the installer to assume that url rewriting
    had already been set up (i.e. RewriteBase in install/.htaccess).
    
    I believe this style of urls will breaks HTML forms which are sent by
    GET.  All the installer forms should be sent by POST, which is fine.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (is_dir(dirname(__FILE__).'/install') == true)
 	$base_url .= '://'. $_SERVER['HTTP_HOST'];
 	$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
-	header("Location: {$base_url}install");
+	header("Location: {$base_url}install/index.php");
 	exit;
 }
 
