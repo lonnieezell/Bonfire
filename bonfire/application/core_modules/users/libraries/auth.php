3510a2a8c32d3ab270da235132d8ccb2db5a808a
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-05 15:29:44 +0000

    users auth: Add escaping with e() in another place where it might be needed

--- bonfire/application/core_modules/users/libraries/auth.php
+++ bonfire/application/core_modules/users/libraries/auth.php
@@ -181,7 +181,7 @@ class Auth
 		// check if the account has been soft deleted.
 		if ($user->deleted >= 1) // in case we go to a unix timestamp later, this will still work.
 		{
-			Template::set_message(sprintf(lang('us_account_deleted'), settings_item("site.system_email")), 'error');
+			Template::set_message(sprintf(lang('us_account_deleted'), html_escape(settings_item("site.system_email"))), 'error');
 			return FALSE;
 		}
 
