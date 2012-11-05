3662fa6da6f8ecfcafebe643db83d576dbcb6564
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-05 09:22:44 +0000

    user settings form: remove "Required fields are in bold" - fix #636

--- bonfire/application/core_modules/users/views/settings/user_form.php
+++ bonfire/application/core_modules/users/views/settings/user_form.php
@@ -8,7 +8,6 @@
 	<div class="span8 offset2">
 		<div class="alert alert-info fade in">
 		  <a data-dismiss="alert" class="close">&times;</a>
-			<h4 class="alert-heading"><?php echo lang('bf_required_note'); ?></h4>
 			<?php if (isset($password_hints) ) echo $password_hints; ?>
 		</div>
 	</div>
