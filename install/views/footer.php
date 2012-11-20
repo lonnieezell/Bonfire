2d5570e8c2774b674dfd9faa9d1117ffacc0af70
Lonnie Ezell <lonnieje@gmail.com>
2012-11-20 12:39:27 -0600

    Removing performance specs from footer

--- install/views/footer.php
+++ install/views/footer.php
@@ -2,8 +2,6 @@
 
 		<div class="foot">
 			<div class="inner">
-				<p style="float: right; margin-right: 80px;">Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
-
 				<p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION ?></a></p>
 			</div>
 		</div>
