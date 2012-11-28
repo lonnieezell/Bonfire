7cad59448fdd34010290460980cc4d802ce85de4
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-28 10:17:15 +0000

    Folowup the reverted translation of core_modules/sysinfo/views/developer/index.php
    081ee2a9 forgot to remove the keys in the lang file.

--- bonfire/application/core_modules/sysinfo/language/english/sysinfo_lang.php
+++ bonfire/application/core_modules/sysinfo/language/english/sysinfo_lang.php
@@ -1,32 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['si.system_info']			= 'System Information';
-$lang['si.system']				= 'System';
-$lang['si.modules']				= 'Modules';
-$lang['si.php']					= 'PHP';
+$lang['si.system_info']		= 'System Information';
+$lang['si.system']			= 'System';
+$lang['si.modules']			= 'Modules';
+$lang['si.php']				= 'PHP';
 
-$lang['si.installed_mods']		= 'Installed Modules';
-$lang['si.php_info']			= 'PHP Information';
-
-$lang['sys_bonfire_ver']		= 'Bonfire Version';
-$lang['sys_codeigniter_ver']	= 'CodeIgniter Version';
-$lang['sys_php_ver']			= 'PHP Version';
-
-$lang['sys_server_time']		= 'Server Time';
-$lang['sys_local_time']			= 'Local Time';
-
-$lang['sys_databse_name']		= 'Database Name';
-$lang['sys_databse_server']		= 'Database Server';
-$lang['sys_databse_ver']		= 'Database Version';
-$lang['sys_databse_char']		= 'Database Charset';
-$lang['sys_databse_col_char']	= 'Database Collation Charset';
-
-$lang['sys_base_path']			= 'BASE PATH';
-$lang['sys_app_path']			= 'APP PATH';
-$lang['sys_app_url']			= 'SITE URL';
-$lang['sys_app_environment']	= 'ENVIRONMENT';
-
-$lang['sys_mod_name']			= 'Name';
-$lang['sys_mod_ver']			= 'Version';
-$lang['sys_mod_desc']			= 'Description';
-$lang['sys_mod_author']			= 'Author';
+$lang['si.installed_mods']	= 'Installed Modules';
+$lang['si.php_info']		= 'PHP Information';
