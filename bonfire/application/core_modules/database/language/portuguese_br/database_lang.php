fbbbbb47d5820d092aa1a004d33572a207044054
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-28 11:41:00 +0000

    database languages: remove duplicate key

--- bonfire/application/core_modules/database/language/portuguese_br/database_lang.php
+++ bonfire/application/core_modules/database/language/portuguese_br/database_lang.php
@@ -104,4 +104,3 @@ $lang['db_sql_query']			= 'SQL Query';
 $lang['db_total_results']		= 'Resultado Total';
 $lang['db_no_rows']				= 'Não foram encontrados dados para a tabela.';
 $lang['db_browse']				= 'Navegar';
-$lang['db_apply']               = 'Aplicar';
