8844b290e8d6157a33b4bf5e76e8c0073cb8c706
Lonnie Ezell <lonnieje@gmail.com>
2012-11-28 12:16:32 -0600

    Fixed links on complete view.

--- install/views/install/complete.php
+++ install/views/install/complete.php
@@ -9,10 +9,10 @@
 <h3><?php echo lang('in_complete_next') ?></h3>
 
 <ul>
-	<li><?php echo lang('in_complete_visit') ?> <a href="<?php echo site_url('/admin') ?>"><?php echo lang('in_admin_area') ?></a></li>
-	<li><?php echo lang('in_complete_visit') ?> <a href="<?php echo site_url('/') ?>"><?php echo lang('in_site_front') ?></a></li>
-	<li><?php echo lang('in_read') ?> <a href="https://github.com/ci-bonfire/Bonfire/wiki"><?php echo lang('in_bf_docs') ?></a></li>
-	<li><?php echo lang('in_read') ?> <a href="http://ellislab.com/codeigniter/user-guide/"><?php echo lang('in_ci_docs') ?></a></li>
+	<li><?php echo lang('in_complete_visit') ?> <a href="/admin"><?php echo lang('in_admin_area') ?></a></li>
+	<li><?php echo lang('in_complete_visit') ?> <a href="/"><?php echo lang('in_site_front') ?></a></li>
+	<li><?php echo lang('in_read') ?> <a href="https://github.com/ci-bonfire/Bonfire/wiki" target="_blank"><?php echo lang('in_bf_docs') ?></a></li>
+	<li><?php echo lang('in_read') ?> <a href="http://ellislab.com/codeigniter/user-guide/" target="_blank"><?php echo lang('in_ci_docs') ?></a></li>
 </ul>
 
 <br/>
