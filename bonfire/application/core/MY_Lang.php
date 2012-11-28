58e06d7b235b43c969046d9f2ae1668687b96bae
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2012-11-28 13:49:34 +0000

    translate edit: fix runtime fallback to English
    [a0c54994ea2af0caa37b63a7b5ebd75e79b8651c]
    
    Code works better if you test it :).

--- bonfire/application/core/MY_Lang.php
+++ bonfire/application/core/MY_Lang.php
@@ -15,22 +15,47 @@
 
 // ------------------------------------------------------------------------
 
-class MY_Lang extends CI_Lang
+class MY_Lang extends MX_Lang
 {
 	public function load($langfile, $lang = '', $return = FALSE, $add_suffix = TRUE, $alt_path = '', $_module = '')
 	{
-		$success = TRUE;
+		if (is_array($langfile))
+		{
+			foreach ($langfile as $file)
+			{
+				$this->load($file);
+			}
+			return $this->language;
+		}
+
+		if ($lang == '')
+		{
+			$lang = CI::$APP->config->item('language');
+		}
+
+		if (in_array($langfile, $this->is_loaded, TRUE))
+		{
+			return $this->language;
+		}
 
 		// Fallback to english (rather than the dumb keys) for missing translations
 		// Bonfire used to do this in the translate editor,
 		// but that has obvious maintenance problems.
+		$result = array();
+
 		if ($lang != 'english')
 		{
-			$success &= parent::load($langfile, $lang, $return, $add_suffix, $alt_path);
+			// We need to use $return = TRUE so we can load the same $langfile twice,
+			// but with different languages
+			$result = parent::load($langfile, 'english', TRUE, $add_suffix, $alt_path);
 		}
+		$result = array_merge($result, parent::load($langfile, $lang, TRUE, $add_suffix, $alt_path));
 
-		$success &= parent::load($langfile, $lang, $return, $add_suffix, $alt_path);
-
-		return $success;
+		if (!$return)
+		{
+			$this->language = array_merge($this->language, $result);
+			$this->is_loaded[] = $langfile;
+		}
+		return $result;
 	}
 }
