29157fb3f22ad4cb9c3aece9b41b4c19b85d6a92
Abdi Aleh <aleh.mail@gmail.com>
2013-01-30 18:38:17 +0700

    Update public/themes/admin/partials/_header.php
    Add editor tinyMCE with skin Bootstrap twitter to admin template,
    1. - download the latest TinyMCE package from 
         http://www.tinymce.com/download/download.php
       - Unzip put the folder "tiny_mce" from tinymce/jscripts/ to the admin theme path 
         (./bonfire/themes/admin/js/) and put the css file "content.css"
         from tinymce/examples/css/ to ./bonfire/themes/admin/css/tinymce/
    
    download the skin from :
    - https://github.com/gtraxx/tinymce-skin-bootstrap
    - unzip and put to admin theme path (./bonfire/themes/admin/js/tiny_mce/themes/advanced/skins) 
    
    put this script bitween <head> tag or you can put in global.js
    <script src="<?php echo Template::theme_url('js/tiny_mce/tiny_mce.js'); ?>"></script>
    
    	<script type="text/javascript">
    	    tinyMCE.init({
    	       // General options
    	       mode : "textareas",
    	       theme : "advanced",
    	       skin : "bootstrap",
    	       plugins : "table,inlinepopups",
    	       // Theme options
    	       theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselec",
    	       theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
    	       theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
    	       theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
    	       theme_advanced_toolbar_location : "top",
    	       theme_advanced_toolbar_align : "left",
    	       theme_advanced_statusbar_location : "bottom",
    	       theme_advanced_resizing : true,
    	       // Example content CSS (should be your site CSS)
    	       content_css : "<?php echo Template::theme_url('css/tinymce/content.css'); ?>"
    	    });
        </script>
    
    Thank's to gtraxx (https://github.com/gtraxx/)

--- public/themes/admin/partials/_header.php
+++ public/themes/admin/partials/_header.php
@@ -21,6 +21,32 @@
 	<?php echo Assets::css(null, true); ?>
 
 	<script src="<?php echo Template::theme_url('js/modernizr-2.5.3.js'); ?>"></script>
+	<script src="<?php echo Template::theme_url('js/tiny_mce/tiny_mce.js'); ?>"></script>
+	
+	<!-- Option Theme of TinuMCE or you can put this scripts in global.js-->
+	<script type="text/javascript">
+	    tinyMCE.init({
+	       // General options
+	       mode : "textareas",
+	       theme : "advanced",
+	       skin : "bootstrap",
+	       plugins : "table,inlinepopups",
+	       // Theme options
+	       theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselec",
+	       theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+	       theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+	       theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+	       theme_advanced_toolbar_location : "top",
+	       theme_advanced_toolbar_align : "left",
+	       theme_advanced_statusbar_location : "bottom",
+	       theme_advanced_resizing : true,
+	       // Example content CSS (should be your site CSS)
+	       content_css : "<?php echo Template::theme_url('css/tinymce/content.css'); ?>"
+	    });
+    </script>
+	
+	
+	
 </head>
 <body class="desktop">
 <!--[if lt IE 7]>
