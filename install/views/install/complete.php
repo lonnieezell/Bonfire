7d2adfe2c330006145a33dd13ef8015f776a3ebb
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2013-02-28 17:44:08 +0000

    install: fix new links back to main Bonfire
    i.e. for subdirectory installs.
    
    I used base_url() instead of site_url() to avoid adding the /index.php/
    segment that the installer uses.

--- install/views/install/complete.php
+++ install/views/install/complete.php
@@ -9,8 +9,10 @@
 <h3><?php echo lang('in_complete_next') ?></h3>
 
 <ul>
-	<li><?php echo lang('in_complete_visit') ?> <a href="/admin"><?php echo lang('in_admin_area') ?></a></li>
-	<li><?php echo lang('in_complete_visit') ?> <a href="/"><?php echo lang('in_site_front') ?></a></li>
+	<li>
+		<?php echo lang('in_complete_visit') ?> <a href="<?php echo preg_replace('{install/$}', '', base_url()) .'admin' ?>"><?php echo lang('in_admin_area') ?></a>
+	</li>
+	<li><?php echo lang('in_complete_visit') ?> <a href="<?php echo preg_replace('{install/$}', '', base_url()) ?>"><?php echo lang('in_site_front') ?></a></li>
 	<li><?php echo lang('in_read') ?> <a href="https://github.com/ci-bonfire/Bonfire/wiki" target="_blank"><?php echo lang('in_bf_docs') ?></a></li>
 	<li><?php echo lang('in_read') ?> <a href="http://ellislab.com/codeigniter/user-guide/" target="_blank"><?php echo lang('in_ci_docs') ?></a></li>
 </ul>
