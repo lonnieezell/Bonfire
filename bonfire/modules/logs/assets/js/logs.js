d65f721df7bcd4a2c26fefb8102d5f4acead4147
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2013-03-14 16:45:06 +0000

    log.js: fix inconsistency on page refresh
     - Change the filter to "errors only"
     - Hit refresh
     -> Filter will remain on "errors only"; the actual filtering should
        reflect that.
    
    We forgot to check the "initial" state of the select box after installing
    the event handler.  Fix that.
    
    I'm not bothering to fix the flash-of-unbehavioured-content right now.
    I think that's a fairly obvious temporary glitch compared to the
    head-scratching inconsistency here.

--- bonfire/modules/logs/assets/js/logs.js
+++ bonfire/modules/logs/assets/js/logs.js
@@ -1,23 +1,30 @@
-// Filter Hook
-$('#filter').change(function(){
-	// Are we filtering at all? 
-	var filter = $(this).val();
+(function(){
 
-	$('#log div').each(function(){
-	
-		switch (filter)
-		{
-			case 'all':
-				$(this).css('display', 'block');
-				break;
-			case 'error':
-				if ($(this).hasClass('alert-error'))
-				{
+	var $filter = $('#filter')
+
+	function filterChange(){
+		// Are we filtering at all?
+		var filter_val = $filter.val();
+
+		$('#log div').each(function(){
+
+			switch (filter_val)
+			{
+				case 'all':
 					$(this).css('display', 'block');
-				} else
-				{
-					$(this).css('display', 'none');
-				}
-		}
-	});
-});
+					break;
+				case 'error':
+					if ($(this).hasClass('alert-error'))
+					{
+						$(this).css('display', 'block');
+					} else
+					{
+						$(this).css('display', 'none');
+					}
+			}
+		});
+	}
+
+	$filter.change(filterChange);
+	filterChange();
+})();
