88d047b672603faaa5d865d36058b96a436da39f
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2013-03-25 11:18:01 +0000

    index.php: fix infinite redirect on 0.7 install (#754)

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@
 // public path for security, or to simply create a new little text file
 // for when we're developing on it, but want to easily install later on.
 //
-if (!file_exists('./install/installed.txt'))
+if (is_dir('./install') && !file_exists('./install/installed.txt'))
 {
 	// Auto $base_url copied from codeigniter/core/Config.php
 	$base_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
