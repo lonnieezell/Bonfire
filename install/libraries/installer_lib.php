f10d029bbd65f67e2be3b1f504f5a307bb3a31fc
Mat Whitney <mwhitney@mail.sdsu.edu>
2013-04-29 10:24:19 -0700

    Add basic support in installer for other DB types
    Added basic support for all DB types for which we currently have
    drivers, plus a placeholder if the MongoDB driver is updated in the
    future.
    
    Note the support for ODBC and PDO is very limited, and many of the
    connection options for Oracle (oci8) are not supported.
    
    None of these have been added to the supported_dbs array in the install
    controller, yet.

--- install/libraries/installer_lib.php
+++ install/libraries/installer_lib.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Installer_lib {
-	
+
 	private $ci;
 
 	public 	$php_version;
@@ -27,39 +27,39 @@ class Installer_lib {
 	public	$BFPATH;
 
 	//--------------------------------------------------------------------
-	
-	public function __construct($config=array()) 
+
+	public function __construct($config=array())
 	{
 		$this->ci =& get_instance();
-		
+
 		$this->curl_update = $this->cURL_enabled();
-		
+
 		$this->FCPATH = realpath(FCPATH . '..') . '/';
 		$this->APPPATH = INSTALLPATH . 'application/';
 		$this->BFPATH = INSTALLPATH . 'bonfire/';
 	}
-	
+
 	//--------------------------------------------------------------------
-	
-	public function php_acceptable($version=null) 
+
+	public function php_acceptable($version=null)
 	{
 		$this->php_version = phpversion();
-		
+
 		return ( version_compare($this->php_version, $version, '>=') ) ? true : false;
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	/**
 	 *	Tests whether the specified database type can even be found.
 	 *
 	 * @access	public
 	 * @return	boolean
 	 */
-	public function db_available() 
+	public function db_available()
 	{
 		$driver = $this->ci->input->post('driver');
-		
+
 		switch ($driver)
 		{
 			case 'mysql':
@@ -68,105 +68,207 @@ class Installer_lib {
 			case 'mysqli':
 				return class_exists('Mysqli');
 				break;
-			default: 
+
+			case 'cubrid':
+				return function_exists('cubrid_connect');
+				break;
+			case 'mongodb': // deprecated
+				return class_exists('Mongo');
+				break;
+			case 'mongoclient': // I don't believe we have a driver for this, yet
+				return class_exists('MongoClient');
+				break;
+			case 'mssql':
+				return function_exists('mssql_connect');
+				break;
+			case 'oci8':
+				return function_exists('oci_connect');
+				break;
+			case 'odbc':
+				return function_exists('odbc_connect');
+				break;
+			case 'pdo':
+				return class_exists('PDO');
+				break;
+			case 'postgre':
+				return function_exists('pg_connect');
+				break;
+			case 'sqlite':
+				return function_exists('sqlite_open');
+				break;
+			case 'sqlsrv':
+				return function_exists('sqlsrv_connect');
+				break;
+			default:
 				return false;
 		}
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	/**
 	 *	Attempts to connect to the database given the existing $_POST vars.
 	 *
 	 * @access	public
 	 * @return	boolean
 	 */
-	public function test_db_connection() 
+	public function test_db_connection()
 	{
+		if ( ! $this->db_available())
+		{
+			return false;
+		}
+
 		$driver	= $this->ci->input->post('driver');
-		
+
 		$hostname	= $this->ci->input->post('hostname');
 		$username	= $this->ci->input->post('username');
 		$password	= $this->ci->input->post('password');
 		$port		= $this->ci->input->post('port');
-		
+		$db_name	= $this->ci->input->post('database');
+
 		switch ($driver)
 		{
 			case 'mysql':
-				return $this->db_available() && @mysql_connect("$hostname:$port", $username, $password);
+				return @mysql_connect("$hostname:$port", $username, $password);
 				break;
 			case 'mysqli':
 				$mysqli = new mysqli($hostname, $username, $password, '', $port);
-
-				if ($mysqli->connect_error)
-				{
-					return false;
-				}
-				else
+				if ( ! $mysqli->connect_error)
 				{
 					return true;
 				}
+				return false;
 				break;
-			default: 
+			case 'cubrid':
+				return @cubrid_connect($hostname, $port, $db_name, $username, $password);
+				break;
+			case 'mongodb': // deprecated
+				$connect_string = $this->get_mongo_connection_string($hostname, $port, $username, $password, $db_name);
+				try
+				{
+					$mongo = new Mongo($connect_string);
+					return true;
+				}
+				catch (MongoConnectionException $e)
+				{
+					show_error('Unable to connect to MongoDB.', 500);
+				}
+				return false;
+				break;
+			case 'mongoclient': // no driver support at this time
+				$connect_string = $this->get_mongo_connection_string($hostname, $port, $username, $password, $db_name);
+				try
+				{
+					$mongo = new MongoClient($connect_string);
+					return true;
+				}
+				catch (MongoConnectionException $e)
+				{
+					show_error('Unable to connect MongoClient.', 500);
+				}
+				return false;
+				break;
+			case 'mssql':
+				return @mssql_connect("$hostname,$port", $username, $password);
+				break;
+			case 'oci8':
+				$connect_string = $this->get_oracle_connection_string($hostname, $port);
+				return @oci_connect($username, $password, $connect_string);
+				break;
+			case 'odbc':
+				$connect_string = $this->get_odbc_connection_string($hostname);
+				return @odbc_connect($connect_string, $username, $password);
+				break;
+			case 'pdo':
+				$connect_string = $this->get_pdo_connection_string($hostname, $db_name);
+				try
+				{
+					$pdo = new PDO($connect_string, $username, $password);
+					return true;
+				}
+				catch (PDOException $e)
+				{
+					show_error('Unable to connect using PDO.', 500);
+				}
+				return false;
+				break;
+			case 'postgre':
+				$connect_string = $this->get_postgre_connection_string($hostname, $port, $username, $password, $db_name);
+				return @pg_connect($connect_string);
+				break;
+			case 'sqlite':
+				if ( ! $sqlite = @sqlite_open($db_name, FILE_WRITE_MODE, $error))
+				{
+					show_error($error, 500);
+					return false;
+				}
+				return $sqlite;
+				break;
+			case 'sqlsrv':
+				$connection = $this->get_sqlsrv_connection($username, $password, $db_name);
+				return sqlsrv_connect($hostname, $connection);
+				break;
+			default:
 				return false;
 		}
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	/*
 		Method: is_installed()
-		
-		Performs some basic checks to see if maybe, just maybe, the 
-		user has already installed the application and just hasn't 
+
+		Performs some basic checks to see if maybe, just maybe, the
+		user has already installed the application and just hasn't
 		moved the install folder....
 	*/
-	public function is_installed() 
+	public function is_installed()
 	{
-		// First check - Does a 'install/installed.txt' file exist? If so, 
-		// then we've likely already installed. 
+		// First check - Does a 'install/installed.txt' file exist? If so,
+		// then we've likely already installed.
 		if (is_file(FCPATH . 'installed.txt'))
 		{
 			return true;
 		}
 
-		// Does the database config exist? 
+		// Does the database config exist?
 		// If not, then we definitely haven't installed yet.
 		if (!is_file($this->APPPATH . 'config/development/database.php'))
 		{
 			return false;
 		}
-		
+
 		require($this->APPPATH . '/config/development/database.php');
-		
+
 		// If the $db['default'] doesn't exist then we can't
 		// load our database.
 		if (!isset($db) || !isset($db['default']))
 		{
 			return false;
 		}
-		
+
 		$this->ci->load->database($db['default']);
-		
+
 		// Does the users table exist?
 		if (!$this->ci->db->table_exists('users'))
 		{
 			return false;
 		}
-		
+
 		// Make sure at least one row exists in the users table.
 		$query = $this->ci->db->get('users');
-		
+
 		if ($query->num_rows() == 0)
 		{
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	/*
 		Method: cURL_check()
 
@@ -180,7 +282,7 @@ class Installer_lib {
 
 
 	//--------------------------------------------------------------------
-	
+
 	/**
 	 * Checks an array of folders to see if they are writeable and
 	 * returns results usable in the requirements check step.
@@ -188,13 +290,13 @@ class Installer_lib {
 	 * @access	public
 	 * @return	array
 	 */
-	public function check_folders($folders) 
+	public function check_folders($folders)
 	{
 		$data = array();
-		
+
 		// Load the file helper
 		$this->ci->load->helper('file');
-	
+
 		foreach ($folders as $folder)
 		{
 			// If it starts with 'public/', then that represents
@@ -208,15 +310,15 @@ class Installer_lib {
 			{
 				$realpath = INSTALLPATH . $folder;
 			}
-			
+
 			$data[$folder] = is_really_writable($realpath);
 		}
-		
+
 		return $data;
 	}
-	
+
 	//--------------------------------------------------------------------
-	
+
 	/**
 	 * Checks an array of files to see if they are writeable and
 	 * returns results usable in the requirements check step.
@@ -224,13 +326,13 @@ class Installer_lib {
 	 * @access	public
 	 * @return	array
 	 */
-	public function check_files($files) 
+	public function check_files($files)
 	{
 		$data = array();
-		
+
 		// Load the file helper
 		$this->ci->load->helper('file');
-	
+
 		foreach ($files as $file)
 		{
 			// If it starts with 'public/', then that represents
@@ -244,15 +346,15 @@ class Installer_lib {
 			{
 				$realpath = INSTALLPATH . $file;
 			}
-			
+
 			$data[$file] = is_really_writable($realpath);
 		}
-		
+
 		return $data;
 	}
-	
+
     //--------------------------------------------------------------------
-    
+
     /*
 		Method: hash_password()
 
@@ -278,7 +380,7 @@ class Installer_lib {
 	}
 
 	//--------------------------------------------------------------------
-	
+
 	/**
 	 * Performs the actuall installation of the database, creates our
 	 * config files, and installs the user account.
@@ -300,11 +402,11 @@ class Installer_lib {
 		$driver		= $this->ci->session->userdata('driver');
 		$prefix		= $this->ci->session->userdata('db_prefix');
 		$port		= $this->ci->session->userdata('port');
-		
+
 		$db_debug	= $environment == 'production' ? FALSE : TRUE;
 
 		$this->ci->load->helper('config_file');
-		
+
 		$db_data = array(
 			'hostname'	=> $hostname,
 			'port'		=> $port,
@@ -329,14 +431,14 @@ class Installer_lib {
 			$str = lang('in_db_config_error');
 			return str_replace('{file}', "config/$environment/database.php", $str);
 		}
-		
+
 
 		/*
 			2. Install default info into our database.
-			
+
 			This happens by running the app, core and module-specific migrations.
 		*/
-		
+
 		// use the entered Database settings to connect before calling the Migrations
 		$this->ci->load->database($db_data);
 
@@ -346,7 +448,7 @@ class Installer_lib {
 		$this->ci->load->library('Migrations', array('migrations_path' => $this->BFPATH .'migrations'));
 
 		if (!$this->ci->migrations->install())
-		{ 
+		{
 			return $this->ci->migrations->error;
 		}
 
@@ -390,13 +492,13 @@ class Installer_lib {
 			'username'	=> $this->ci->session->userdata('user_username'),
 			'active'    => 1,
 		);
-		
+
 		// As of 0.7, we've switched to using phpass for password encryption...
 		require ($this->BFPATH .'modules/users/libraries/PasswordHash.php' );
 
 		$iterations	= $this->ci->config->item('password_iterations');
 		$hasher = new PasswordHash($iterations, false);
-		
+
 		$password = $hasher->HashPassword($this->ci->session->userdata('user_password'));
 
 		$data['password_hash'] = $password;
@@ -494,4 +596,172 @@ class Installer_lib {
 
 
 	//--------------------------------------------------------------------
+
+	/**
+	 * Get the connection string for MongoDB
+	 *
+	 * @param string $hostname The server hostname
+	 * @param string $port     The server port on which MongoDB accepts connections
+	 * @param string $username User name
+	 * @param string $password Password
+	 * @param string $db_name  The name of the database to connect to
+	 *
+	 * @return string    The connection string used to connect to the database
+	 */
+	private function get_mongo_connection_string($hostname, $port='', $username='', $password='', $db_name='')
+	{
+		$connect_string = 'mongodb://';
+
+		if ( ! empty($username) && ! empty($password))
+		{
+			$connect_string .= $username . ':' . $password . '@';
+		}
+		$connect_string .=  $hostname;
+
+		if ( ! empty($port))
+		{
+			$connect_string .= ':' . $port;
+		}
+		if ( ! empty($db_name))
+		{
+			$connect_string .= '/' . $db_name;
+		}
+
+		return trim($connect_string);
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Get the connection string for PostgreSQL
+	 *
+	 * @param string $hostname The server hostname
+	 * @param string $port     The server port on which PostgreSQL accepts connections
+	 * @param string $username User name
+	 * @param string $password Password
+	 * @param string $db_name  The name of the database to connect to
+	 *
+	 * @return string    The connection string used to connect to the database
+	 */
+	private function get_postgre_connection_string($hostname, $port='', $username='', $password='', $db_name='')
+	{
+		$connect_string = '';
+		$components = array(
+			'host'		=> $hostname,
+			'port'		=> $port,
+			'dbname'	=> $db_name,
+			'user'		=> $username,
+			'password'	=> $password,
+		);
+
+		foreach ($components as $key => $val)
+		{
+			if ( ! empty($val))
+			{
+				$connect_string .= ' ' . $key . '=' . $val;
+			}
+		}
+
+		return trim($connect_string);
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Get the connection string for Oracle (10g+ EasyConnect string)
+	 * Note: 10g can also accept a service name (//$hostname:$port/$service_name)
+	 *		11g can also accept a server type and instance name (//$hostname:$port/$service_name:$server_type/$instance_name)
+	 * We don't currently support these options, though
+	 *
+	 * @param string $hostname The server hostname
+	 * @param string $port     The server port on which Oracle accepts connections
+	 *
+	 * @return string    The connection string used to connect to the database
+	 */
+	private function get_oracle_connection_string($hostname, $port='')
+	{
+		$connect_string = '//' . $hostname;
+		if ( ! empty($port))
+		{
+			$connect_string .= ':' . $port;
+		}
+
+		return $connect_string;
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Stub method to handle ODBC Connection strings.
+	 * Currently, the user will have to either setup a DSN connection and input the DSN name
+	 * or input the DSN-less connection string into the hostname field
+	 *
+	 * @param string $hostname The DSN name or DSN-less connection string
+	 *
+	 * @return string    The connection string used to connect to the database
+	 */
+	private function get_odbc_connection_string($hostname)
+	{
+		return $hostname;
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Stub method to handle PDO Connection strings.
+	 * Currently, the user will have to enter the PDO driver as part of the hostname,
+	 * e.g. mysql:host
+	 *
+	 * @param string $hostname The driver and hostname (separated by a colon) or DSN
+	 * @param string $db_name  The name of the database, if not specified in $hostname
+	 *
+	 * @return string    The connection string used to connect to the database
+	 */
+	private function get_pdo_connection_string($hostname, $db_name='')
+	{
+		$connect_string = $hostname;
+		if ( ! empty($db_name))
+		{
+			$connect_string .= ';dbname=' . $db_name;
+		}
+
+		return $connect_string;
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Stub method to handle SQLSrv connection strings
+	 *
+	 * @param string $username User name
+	 * @param string $password Password
+	 * @param string $db_name  The name of the database
+	 * @param string $char_set Character set
+	 * @param bool $pooling  Connection pooling
+	 *
+	 * @return array    The connection settings array used to connect to the database
+	 */
+	private function get_sqlsrv_connection($username, $password, $db_name, $char_set = 'UTF-8', $pooling = false)
+	{
+		$character_set = (0 === strcasecmp('utf8', $char_set)) ? 'UTF-8' : $char_set;
+		$connection = array(
+			'UID'					=> empty($username) ? '' : $username,
+			'PWD'					=> empty($password) ? '' : $password,
+			'Database'				=> $db_name,
+			'ConnectionPooling'		=> $pooling ? 1 : 0,
+			'CharacterSet'			=> $character_set,
+			'ReturnDatesAsStrings'	=> 1,
+		);
+
+		return $connection;
+	}
+
+
+	//--------------------------------------------------------------------
+
 }
