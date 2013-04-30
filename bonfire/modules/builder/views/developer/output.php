9b7e6141ee267fb10d922be4b7d95b8264219893
Mat Whitney <mwhitney@mail.sdsu.edu>
2013-04-30 10:03:16 -0700

    builder: Fix #748 success output message classes
    Classes applied to messages on the module builder output page were not
    working.
    - Added bootstrap classes for alert boxes.
    - Slightly modified error handling in the view, though this view
    shouldn't be displayed if there are errors

--- bonfire/modules/builder/views/developer/output.php
+++ bonfire/modules/builder/views/developer/output.php
@@ -1,70 +1,83 @@
 <?php
-	$cur_url = uri_string();
-	$tot = $this->uri->total_segments();
-	$last_seg = $this->uri->segment( $tot);
 
-	if( is_numeric($last_seg) ) {
-		$cur_url = str_replace('/'.$last_seg, '', $cur_url);
-	}
+$cur_url = uri_string();
+$tot = $this->uri->total_segments();
+$last_seg = $this->uri->segment( $tot);
+
+if (is_numeric($last_seg))
+{
+	$cur_url = str_replace('/' . $last_seg, '', $cur_url);
+}
+
+// $error should never be non-empty on this page...
+if (empty($error)) :
 ?>
-<div class="notification success">
+<div class="alert alert-success">
+	<button type='button' class='close' data-dismiss='alert'>&times;</button>
 	<p><?php echo lang('mb_out_success'); ?></p>
-	<br />
 	<p><b><?php echo lang('mb_out_success_note'); ?></b></p>
 </div>
-
-<div class="notification attention">
+<?php
+else : ?>
+<div class="alert alert-error">
+	<button type='button' class='close' data-dismiss='alert'>&times;</button>
+	<p><?php echo $error; ?></p>
+</div>
+<?php
+endif;
+?>
+<div class="alert">
+	<button type='button' class='close' data-dismiss='alert'>&times;</button>
 	<?php printf(lang($mb_migration_result), anchor(SITE_AREA .'/developer/migrations#mod-tab', 'Migrations')) ;?>
 </div>
-
-<p class="important">
 <?php
-	if (!isset($error)) {
-		echo $error;
-	};
-?> 
-</p>
-
-<?php if($acl_migration): ?>
+if ($acl_migration) : ?>
 <h4><?php echo lang('mb_out_acl'); ?></h4>
-<p><?php echo sprintf(lang('mb_out_acl_path'),$module_name_lower); ?>
-<?php endif; ?>
+<p><?php echo sprintf(lang('mb_out_acl_path'), $module_name_lower); ?></p>
+<?php
+endif;
 
-<?php if($build_config): ?>
+if ($build_config) : ?>
 <h4><?php echo lang('mb_out_config'); ?></h4>
 <p><?php echo lang('mb_out_config_path'); ?></p>
-<?php endif; ?>
-
+<?php
+endif;
+?>
 <h4><?php echo lang('mb_out_controller'); ?></h4>
 <p>
 <?php
-foreach($controllers as $controller_name => $val) {
-	echo sprintf(lang('mb_out_controller_path'),$controller_name).'<br />';
+foreach ($controllers as $controller_name => $val)
+{
+	echo sprintf(lang('mb_out_controller_path'), $controller_name) . '<br />';
 }
 ?>
 </p>
-
-<?php if($lang): ?>
+<?php if ($lang) : ?>
 <h4><?php echo lang('mb_out_lang'); ?></h4>
-<p><?php echo sprintf(lang('mb_out_lang_path'),$module_name_lower); ?></p>
-<?php endif; ?>
+<p><?php echo sprintf(lang('mb_out_lang_path'), $module_name_lower); ?></p>
+<?php
+endif;
 
-<?php if($db_migration): ?>
+if ($db_migration) : ?>
 <h4><?php echo lang('mb_out_migration'); ?></h4>
 <p><?php echo sprintf(lang('mb_out_migration_path'),$db_table); ?></p>
-<?php endif; ?>
+<?php
+endif;
 
-<?php if($model): ?>
+if ($model) : ?>
 <h4><?php echo lang('mb_out_model'); ?></h4>
-<p><?php echo sprintf(lang('mb_out_model_path'),$module_name_lower); ?></p>
-<?php endif; ?>
-
+<p><?php echo sprintf(lang('mb_out_model_path'), $module_name_lower); ?></p>
+<?php
+endif;
+?>
 <h4><?php echo lang('mb_out_view'); ?></h4>
 <p>
 <?php
-foreach($views as $context_name => $context_views){
-	foreach($context_views as $view_name => $val){
-		echo sprintf(lang('mb_out_view_path'),( $context_name == $module_name_lower ? $view_name : $context_name."/".$view_name)).'<br />';
+foreach ($views as $context_name => $context_views)
+{
+	foreach ($context_views as $view_name => $val)
+	{
+		echo sprintf(lang('mb_out_view_path'), ( $context_name == $module_name_lower ? $view_name : $context_name."/".$view_name)) . '<br />';
 	}
 }
 ?>
