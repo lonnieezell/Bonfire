51e5f5434544ca2700b71ef6355336af37b9859d
Lonnie Ezell <lonnieje@gmail.com>
2013-06-02 23:52:06 -0500

    Since HMVC gives us an spl_autoload feature for /core and /library files, we separated out the controllers into their own file for a tiny bit of memory bonus, and better separation of concerns. Also, makes it easier for an end-user to modify a single controller and not worry about the others getting overwritten.

---
+++ application/core/Admin_Controller.php
@@ -0,0 +1,89 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Admin Controller
+ *
+ * This class provides a base class for all admin-facing controllers.
+ * It automatically loads the form, form_validation and pagination
+ * helpers/libraries, sets defaults for pagination and sets our
+ * Admin Theme.
+ *
+ * @package    Bonfire
+ * @subpackage MY_Controller
+ * @category   Controllers
+ * @author     Bonfire Dev Team
+ * @link       http://guides.cibonfire.com/helpers/file_helpers.html
+ *
+ */
+class Admin_Controller extends Authenticated_Controller
+{
+    protected $pager;
+    protected $limit;
+
+    //--------------------------------------------------------------------
+
+    /**
+     * Class constructor - setup paging and keyboard shortcuts as well as
+     * load various libraries
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library('template');
+        $this->load->library('assets');
+        $this->load->library('ui/contexts');
+
+        // Pagination config
+        $this->pager = array(
+            'full_tag_open'     => '<div class="pagination pagination-right"><ul>',
+            'full_tag_close'    => '</ul></div>',
+            'next_link'         => '&rarr;',
+            'prev_link'         => '&larr;',
+            'next_tag_open'     => '<li>',
+            'next_tag_close'    => '</li>',
+            'prev_tag_open'     => '<li>',
+            'prev_tag_close'    => '</li>',
+            'first_tag_open'    => '<li>',
+            'first_tag_close'   => '</li>',
+            'last_tag_open'     => '<li>',
+            'last_tag_close'    => '</li>',
+            'cur_tag_open'      => '<li class="active"><a href="#">',
+            'cur_tag_close'     => '</a></li>',
+            'num_tag_open'      => '<li>',
+            'num_tag_close'     => '</li>',
+        );
+        $this->limit = $this->settings_lib->item('site.list_limit');
+
+        // load the keyboard shortcut keys
+        $shortcut_data = array(
+            'shortcuts' => config_item('ui.current_shortcuts'),
+            'shortcut_keys' => $this->settings_lib->find_all_by('module', 'core.ui'),
+        );
+        Template::set('shortcut_data', $shortcut_data);
+
+        // Profiler Bar?
+        if (ENVIRONMENT == 'development')
+        {
+            if ($this->settings_lib->item('site.show_profiler') AND has_permission('Bonfire.Profiler.View'))
+            {
+                // Profiler bar?
+                if ( ! $this->input->is_cli_request() AND ! $this->input->is_ajax_request())
+                {
+                    $this->load->library('Console');
+                    $this->output->enable_profiler(TRUE);
+                }
+            }
+        }
+
+        // Basic setup
+        Template::set_theme($this->config->item('template.admin_theme'), 'junk');
+    }//end construct()
+
+    //--------------------------------------------------------------------
+
+}
+
+/* End of file Admin_Controller.php */
+/* Location: ./application/core/Admin_Controller.php */
---
+++ application/core/Authenticated_Controller.php
@@ -0,0 +1,48 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Authenticated Controller
+ *
+ * Provides a base class for all controllers that must check user login
+ * status.
+ *
+ * @package    Bonfire\Core\Controllers
+ * @category   Controllers
+ * @author     Bonfire Dev Team
+ * @link       http://guides.cibonfire.com/helpers/file_helpers.html
+ *
+ */
+class Authenticated_Controller extends Base_Controller
+{
+
+    //--------------------------------------------------------------------
+
+    /**
+     * Class constructor setup login restriction and load various libraries
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library('users/auth');
+
+        // Make sure we're logged in.
+        $this->auth->restrict();
+
+        $this->set_current_user();
+
+        // Load additional libraries
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->form_validation->set_error_delimiters('', '');
+        $this->form_validation->CI =& $this;    // Hack to make it work properly with HMVC
+    }//end construct()
+
+    //--------------------------------------------------------------------
+
+
+}
+
+/* End of file Authenticated_Controller.php */
+/* Location: ./application/core/Authenticated_Controller.php */
