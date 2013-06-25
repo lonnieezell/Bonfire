296fd54cdef449800bcd5e9342ea82c42dedd0c7
Lonnie Ezell <lonnieje@gmail.com>
2013-06-24 23:54:00 -0500

    Removing debugging string

--- bonfire/modules/docs/views/index.php
+++ bonfire/modules/docs/views/index.php
@@ -1,7 +1,6 @@
 <?php if (isset($content) && !empty($content)) : ?>
     <div class="page">
         <?php echo $content; ?>
-        <?php var_dump($content); ?>
     </div>
 <?php else: ?>
     <div class="alert">
