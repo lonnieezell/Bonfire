3100fd91c5415898b6a7c69567cf200587e4dec6
Lonnie Ezell <lonnieje@gmail.com>
2013-07-24 23:22:41 -0500

    Home controller now loads the auth library and sets the current user so we can see the link to the Admin area.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -57,9 +57,49 @@ class Home extends CI_Controller
 			redirect( site_url('install') );
 		}
 
+		$this->load->library('users/auth');
+		$this->set_current_user();
+
 		Template::render();
 	}//end index()
 
 	//--------------------------------------------------------------------
 
+	/**
+	 * If the Auth lib is loaded, it will set the current user, since users
+	 * will never be needed if the Auth library is not loaded. By not requiring
+	 * this to be executed and loaded for every command, we can speed up calls
+	 * that don't need users at all, or rely on a different type of auth, like
+	 * an API or cronjob.
+	 *
+	 * Copied from Base_Controller
+	 */
+	protected function set_current_user()
+	{
+		if (class_exists('Auth'))
+		{
+			// Load our current logged in user for convenience
+			if ($this->auth->is_logged_in())
+			{
+				$this->current_user = clone $this->auth->user();
+
+				$this->current_user->user_img = gravatar_link($this->current_user->email, 22, $this->current_user->email, "{$this->current_user->email} Profile");
+
+				// if the user has a language setting then use it
+				if (isset($this->current_user->language))
+				{
+					$this->config->set_item('language', $this->current_user->language);
+				}
+			}
+
+			// Make the current user available in the views
+			if (!class_exists('Template'))
+			{
+				$this->load->library('Template');
+			}
+			Template::set('current_user', $this->current_user);
+		}
+	}
+
+	//--------------------------------------------------------------------
 }//end class
