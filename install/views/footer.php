36b6e899b30d244f7f046c8c40189e5b1ca584df
Lonnie Ezell <lonnieje@gmail.com>
2013-07-25 00:15:06 -0500

    Removed the install folder and all of it's files.

--- install/views/footer.php
+++
@@ -1,9 +0,0 @@
-			</div>	<!-- main -->
-
-		<div class="foot">
-			<div class="inner">
-				<p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION ?></a></p>
-			</div>
-		</div>
-</body>
-</html>
