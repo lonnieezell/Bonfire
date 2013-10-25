992961c528a279e65d4148aa0445b95d1a331b7e
Lonnie Ezell <lonnieje@gmail.com>
2013-10-24 23:33:36 -0500

    Updated the model file created by the module builder to include additional utility class vars provided by BF_Model.

--- bonfire/modules/builder/views/files/model.php
+++ bonfire/modules/builder/views/files/model.php
@@ -7,6 +7,9 @@ class '.ucfirst($controller_name).'_model extends BF_Model {
 	protected $key			= "'.$primary_key_field.'";
 	protected $soft_deletes	= '.$this->input->post('use_soft_deletes').';
 	protected $date_format	= "datetime";
+
+	protected $log_user 	= FALSE;
+
 	protected $set_created	= '.$this->input->post('use_created').';
 	protected $set_modified = '.$this->input->post('use_modified').';';
 
@@ -27,6 +30,37 @@ class '.ucfirst($controller_name).'_model extends BF_Model {
 
 	$model .=
 '
+
+	/*
+		Customize the operations of the model without recreating the insert, update,
+		etc methods by adding the method names to act as callbacks here.
+	 */
+	protected $before_insert 	= array();
+	protected $after_insert 	= array();
+	protected $before_update 	= array();
+	protected $after_update 	= array();
+	protected $before_find 		= array();
+	protected $after_find 		= array();
+	protected $before_delete 	= array();
+	protected $after_delete 	= array();
+
+	/*
+		For performance reasons, you may require your model to NOT return the
+		id of the last inserted row as it is a bit of a slow method. This is
+		primarily helpful when running big loops over data.
+	 */
+	protected $return_insert_id 	= TRUE;
+
+	// The default type of element data is returned as.
+	protected $return_type 			= "object";
+
+	// Items that are always removed from data arrays prior to
+	// any inserts or updates.
+	protected $protected_attributes = array();
+
+	protected $validation_rules 		= array();
+	protected $insert_validation_rules 	= array();
+	protected $skip_validation 			= FALSE;
 }
 ';
 
