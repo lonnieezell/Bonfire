31d57ebbf56d996ff74da65d2a6c30154ba5db0b
Lonnie Ezell <lonnieje@gmail.com>
2013-10-24 23:56:35 -0500

    Module Builder's model now creates the $validation_rules array so it is ready to do form_validation within the model.

--- bonfire/modules/builder/views/files/model.php
+++ bonfire/modules/builder/views/files/model.php
@@ -58,11 +58,126 @@ class '.ucfirst($controller_name).'_model extends BF_Model {
 	// any inserts or updates.
 	protected $protected_attributes = array();
 
-	protected $validation_rules 		= array();
+	/*
+		You may need to move certain rules (like required) into the
+		$insert_validation_rules array and out of the standard validation array.
+		That way it is only required during inserts, not updates which may only
+		be updating a portion of the data.
+	 */
+	protected $validation_rules 		= {validation_rules}
 	protected $insert_validation_rules 	= array();
 	protected $skip_validation 			= FALSE;
+
+	//--------------------------------------------------------------------
+
 }
 ';
+
+//--------------------------------------------------------------------
+// Validation Rules
+//--------------------------------------------------------------------
+
+$rules = 'array(';
+
+$last_field = 0;
+for ($counter = 1; $field_total >= $counter; $counter++)
+{
+	// only build on fields that have data entered.
+
+	// Due to the required if rule if the first field is set the the others must be
+	if (set_value("view_field_label$counter") == NULL)
+	{
+		continue; 	// move onto next iteration of the loop
+	}
+
+	// we set this variable as it will be used to place the comma after the last item to build the insert db array
+	$last_field = $counter;
+
+	if($db_required == 'new' && $table_as_field_prefix === TRUE)
+	{
+			$field_name = $module_name_lower . '_' . set_value("view_field_name$counter");
+	}
+	elseif($db_required == 'new' && $table_as_field_prefix === FALSE)
+	{
+			$field_name = set_value("view_field_name$counter");
+	}
+	else
+	{
+			$field_name = set_value("view_field_name$counter");
+	}
+
+	$form_name = $module_name_lower . '_' . set_value("view_field_name$counter");
+	$rules .= '
+		array(
+			"field"		=> "'. $form_name .'",
+			"label"		=> "'. set_value("view_field_label$counter") .'",
+			"rules"		=> "';
+
+	// set a friendly variable name
+	$validation_rules = $this->input->post('validation_rules'.$counter);
+
+	// rules have been selected for this fieldset
+	$rule_counter = 0;
+
+	if (is_array($validation_rules))
+	{
+		// add rules such as trim|required
+		foreach ($validation_rules as $key => $value)
+		{
+			if ($rule_counter > 0)
+			{
+				$rules .= '|';
+			}
+
+			if ($value == 'unique')
+			{
+				$prefix = $this->db->dbprefix;
+				$rules .= $value.'['.$prefix.$table_name.'.'.$field_name.','.$prefix.$table_name.'.'.$primary_key_field.']';
+			}
+			else
+			{
+				$rules .= $value;
+			}
+			$rule_counter++;
+		}
+	}
+
+	$db_field_type = set_value("db_field_type".$counter);
+
+	if ($db_field_type != 'ENUM' && $db_field_type != 'SET' && set_value("db_field_length_value$counter") != NULL)
+	{
+		if ($rule_counter > 0)
+		{
+			$rules .= '|';
+		}
+
+		if ($db_field_type == 'DECIMAL' || $db_field_type == 'FLOAT' || $db_field_type == 'DOUBLE')
+		{
+			list($len, $decimal) = explode(",", set_value("db_field_length_value$counter"));
+			$max = $len;
+
+			if (isset($decimal) && $decimal != 0)
+			{
+				$max = $len + 1;		// Add 1 to allow for the
+			}
+			$rules .= 'max_length['.$max.']';
+		}
+		else
+		{
+			$rules .= 'max_length['.set_value("db_field_length_value$counter").']';
+		}
+	}
+
+	$rules .= '"
+		),';
+}
+
+$rules .= '
+	);';
+
+$model = str_replace('{validation_rules}', $rules, $model);
+
+unset($rules);
 
 echo $model;
 ?>
