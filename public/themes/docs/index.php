a492705ef311e5e58f681363b09409fd88a18d51
Mat Whitney <mwhitney@mail.sdsu.edu>
2013-10-29 09:07:31 -0700

    Updated docs to use assets library
    Also changed the default theme to github. I prefer dark themes, but I
    can't even read the code in the pojoaque theme.

--- public/themes/docs/index.php
+++ public/themes/docs/index.php
@@ -2,31 +2,31 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-
     <title>Docs - <?php e($this->settings_lib->item('site.title')); ?></title>
-    <?php echo Assets::css(); ?>
-    <link rel="stylesheet" href="<?= base_url() ?>/themes/docs/css/pojoaque.css">
+    <?php
+    $jqueryVersion = '1.7.2';
+    $highlightScript = '$("pre code").each(function(i, e) {hljs.highlightBlock(e)});';
+
+    Assets::add_js('highlight.min.js');
+    Assets::add_js($highlightScript, 'inline');
+
+    Assets::add_css('github');
+    echo Assets::css();
+    ?>
 </head>
 <body>
-
     <div class="sidebar">
         <div class="inner">
             <?php echo $sidebar; ?>
         </div>
     </div>
-
     <div class="main">
         <div class="inner">
             <?php echo Template::content(); ?>
         </div>
     </div>
-
-    <script type="text/javascript" src="/themes/admin/js/jquery-1.7.2.min.js"></script>
-    <script type="text/javascript" src="/themes/docs/js/highlight.min.js"></script>
-    <script>
-        $(document).ready(function() {
-          $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
-        });
-    </script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/<?php echo $jqueryVersion; ?>/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo js_path(); ?>jquery-<?php echo $jqueryVersion; ?>.min.js"><\/script>')</script>
+    <?php echo Assets::js(); ?>
 </body>
 </html>
