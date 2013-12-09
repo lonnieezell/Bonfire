fb25b594e67595247c068d07795ad389a00a3e76
Mat Whitney <mwhitney@mail.sdsu.edu>
2013-12-09 07:57:09 -0800

    Partial Fix for #947, remove short tag
    - Removed short tag, since it's not necessarily supported in PHP 5.3.
    - Fixed issue with HTTPS detection if server sets HTTPS to 'OFF' instead
    of 'off'.
    - I know bootstrap doesn't use them most of the time, but we really
    shouldn't drop semicolons from our JavaScript

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    $install_url = 'http'.((empty($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off')?'':'s') .'://'. $_SERVER['SERVER_NAME'] .'/public/';
+
+$install_url = 'http' . ((empty($_SERVER['HTTPS']) || strtolower($_SERVER['HTTPS']) == 'off')?'':'s') . '://' . $_SERVER['SERVER_NAME'] . '/public/';
+
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
@@ -42,14 +44,13 @@
             <p>If you feel like you can contribute either by <a href="https://trello.com/b/I54dfqR4/bonfire-roadmap">adding features</a> or <a href="https://github.com/ci-bonfire/Bonfire/issues?state=open">fixing issues</a> please fork <a href="https://github.com/ci-bonfire/Bonfire">the repo</a>, start your work in a new branch, and submit pull requests for review.</p>
             <p><em>"Let's make this the best kick-start to any CodeIgniter project."</em> ~ The CI-Bonfire Team</p>
             <div class="continue">
-                <a class="btn btn-primary" href="<?= $install_url ?>">Continue &raquo;</a>
+                <a class="btn btn-primary" href="<?php echo $install_url; ?>">Continue &raquo;</a>
             </div>
         </div>
-
         <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="public/assets/js/jquery-1.7.2.min.js"><\/script>')</script>
-
+        <script>window.jQuery || document.write('<script src="public/assets/js/jquery-1.7.2.min.js"><\/script>');
+        </script>
         <!-- This would be a good place to use a CDN version of jQueryUI if needed -->
         <script type="text/javascript" src="public/assets/js/bootstrap.min.js" ></script>
     </body>
