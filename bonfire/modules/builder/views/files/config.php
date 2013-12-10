3ec149e9ee1e44ceff4ad0c9b6234fa69dea6420
Fernando Cordeiro <fernando@fernandocordeiro.com>
2013-12-09 22:57:49 -0200

    Froze development for config translation as it needs some work.

--- bonfire/modules/builder/views/files/config.php
+++ bonfire/modules/builder/views/files/config.php
@@ -4,11 +4,12 @@ $build_config = '<?php if ( ! defined(\'BASEPATH\')) exit(\'No direct script acc
 
 
 $build_config .= PHP_EOL . '
-$module_name_translate = lang("'.$module_name_lower.'_module_name");
-$module_description_translate = lang("'.$module_name_lower.'_module_description");
+// $module_name_translate = lang("'.$module_name_lower.'_module_name");
+// $module_description_translate = lang("'.$module_name_lower.'_module_description");
+
 $config[\'module_config\'] = array(
-	\'description\'	=> $module_description_translate,
-	\'name\'		=> $module_name_translate,
+	\'description\'	=> '.$module_description.',
+	\'name\'		=> '.$module_name.',
 	\'version\'		=> \'0.0.1\',
 	\'author\'		=> \'' . $username . '\'
 );';
