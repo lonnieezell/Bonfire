93ddda41fec1aa93ded1d0d69d350ca21c427f07
Fernando Cordeiro <fernando@fernandocordeiro.com>
2013-12-09 23:08:10 -0200

    Fixed Language for Portuguese_BR

---
+++ bonfire/codeigniter/language/portuguese_br/imglib_lang.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['imglib_source_image_required'] = "Especifique a origem da imagem nas configurações.";
+$lang['imglib_gd_required'] = "Para uso deste recurso, é necessária a biblioteca de imagens GD.";
+$lang['imglib_gd_required_for_props'] = "Para determinar as propriedades da imagem, o servidor deverá suportar a biblioteca de imagens GD.";
+$lang['imglib_unsupported_imagecreate'] = "O servidor não suporta a biblioteca GD necessária para processar este tipo de imagem.";
+$lang['imglib_gif_not_supported'] = "Geralmente, não há suporte para imagens GIF em virtude de restrições de licenças. Prefira imagens JPG ou PNG.";
+$lang['imglib_jpg_not_supported'] = "Não há suporte para imagens JPG.";
+$lang['imglib_png_not_supported'] = "Não há suporte para imagens PNG.";
+$lang['imglib_jpg_or_png_required'] = "O protocolo de redimensionamento de imagens especificado nas configurações funciona apenas com imagens JPG ou PNG.";
+$lang['imglib_copy_error'] = "Ocorreu um erro durante a tentativa de substituição do ficheiro. Confirme se há permissão de escrita para o directório.";
+$lang['imglib_rotate_unsupported'] = "O servidor não tem suporte para rotação de imagens.";
+$lang['imglib_libpath_invalid'] = "O caminho para a biblioteca de imagens não está correcto. Especifique o caminho correcto nas configurações de imagem.";
+$lang['imglib_image_process_failed'] = "Falha no processamento da imagem. Verifique se o servidor tem suporte para o protocolo escolhido e se o caminho para a biblioteca de imagens está correcto.";
+$lang['imglib_rotation_angle_required'] = "Para rodar a imagem, insira ângulo de rotação.";
+$lang['imglib_writing_failed_gif'] = "Imagem GIF.";
+$lang['imglib_invalid_path'] = "O caminho para a imagem está incorrecto.";
+$lang['imglib_copy_failed'] = "Falha na cópia da imagem.";
+$lang['imglib_missing_font'] = "Não foi possível encontrar uma fonte para ser utilizada.";
+$lang['imglib_save_failed'] = "Não foi possível guardar a imagem. Confirme se há permissão de escrita para a imagem e para o directório.";
+
+/* End of file imglib_lang.php */
+/* Location: ./system/language/portuguese/imglib_lang.php */
