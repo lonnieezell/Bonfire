8a20cc31bc5425c35a5e7d2152ae91fe418d8195
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-02-11 09:25:44 -0800

    Roles/Permissions: Remove form_has_error()
    - Remove form_has_error() calls from Roles and Permissions views
    - Add some language entries for roles
    - Performance boost on permission matrix for large sets of
    roles/permissions by caching '*.Manage' permissions in the view

--- bonfire/modules/roles/views/settings/_sub_nav.php
+++ bonfire/modules/roles/views/settings/_sub_nav.php
@@ -1,14 +1,19 @@
+<?php
+
+$testSegment = $this->uri->segment(4);
+$rolesUrl = site_url(SITE_AREA . '/settings/roles');
+
+?>
 <ul class="nav nav-pills">
-	<li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/settings/roles') ?>"><?php echo lang('role_roles'); ?></a>
+	<li<?php echo $testSegment == '' ? ' class="active"' : ''; ?>>
+		<a href="<?php echo $rolesUrl; ?>"><?php echo lang('role_roles'); ?></a>
 	</li>
-	<?php if(has_permission('Bonfire.Roles.Add')):?>
-	<li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/settings/roles/create') ?>" id="create_new"><?php echo lang('role_new_role'); ?></a>
+	<?php if (has_permission('Bonfire.Roles.Add')) : ?>
+	<li<?php echo $testSegment == 'create' ? ' class="active"' : ''; ?>>
+		<a href='<?php echo "{$rolesUrl}/create"; ?>' id='create_new'><?php echo lang('role_new_role'); ?></a>
 	</li>
 	<?php endif;?>
-	<li <?php echo $this->uri->segment(4) == 'permission_matrix' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/settings/roles/permission_matrix') ?>"><?php echo lang('matrix_header'); ?></a>
+	<li<?php echo $testSegment == 'permission_matrix' ? ' class="active"' : ''; ?>>
+		<a href='<?php echo "{$rolesUrl}/permission_matrix"; ?>'><?php echo lang('matrix_header'); ?></a>
 	</li>
 </ul>
-
