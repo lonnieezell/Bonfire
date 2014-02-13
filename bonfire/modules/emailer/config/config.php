b05128895432dbcdf3b5a81585892348c9f1a6b5
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-02-13 12:39:37 -0800

    Fix Emailer Menu Localization
    Doubled up on the name of the lang entries

--- bonfire/modules/emailer/config/config.php
+++ bonfire/modules/emailer/config/config.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
 $config['module_config'] = array(
-	'name'			=> 'lang:bf_menu_email_queue',
+	'name'			=> 'lang:bf_menu_emailer',
 	'description'	=> 'Queues emails to be sent in bursts throughout the day.',
 	'menus'	=> array(
 		'settings'	=> 'emailer/settings/menu',
