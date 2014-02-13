7e5e719a0de8768104b8c46bb4d556315a66b5a9
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-02-13 11:58:51 -0800

    Database Tools localization support
    The config files might help...

--- bonfire/modules/migrations/config/config.php
+++ bonfire/modules/migrations/config/config.php
@@ -1,9 +1,9 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php defined('BASEPATH') || exit('No direct script access allowed');
 
 $config['module_config'] = array(
 	'description'	=> 'Helps maintain database versioning.',
 	'author'		=> 'Bonfire Team',
 	'menu_topic'	=> array(
-		'developer'		=> 'Database Tools'
+		'developer' => 'lang:db_tools',
 	)
 );
