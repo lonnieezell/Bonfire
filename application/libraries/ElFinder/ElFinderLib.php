863cb8abc82b51745f3fc3f6b496745cf65361d2
ulisse73 <ulisse73@quipo.it>
2014-03-01 18:37:05 +0100

    Added ElFinder connector libraries.

---
+++ application/libraries/ElFinder/ElFinderLib.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+error_reporting(E_ALL | E_STRICT);
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'elFinderConnector.class.php';
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'elFinder.class.php';
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'elFinderVolumeDriver.class.php';
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'elFinderVolumeLocalFileSystem.class.php';
+// Required for MySQL storage connector
+// include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeMySQL.class.php';
+// Required for FTP connector support
+// include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeFTP.class.php';
+
+/**
+ * Simple function to demonstrate how to control file access using "accessControl" callback.
+ * This method will disable accessing files/folders starting from  '.' (dot)
+ *
+ * @param  string  $attr  attribute name (read|write|locked|hidden)
+ * @param  string  $path  file path relative to volume root directory started with directory separator
+ * @return bool|null
+ **/
+function access($attr, $path, $data, $volume) {
+	return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
+		? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
+		:  null;                                    // else elFinder decide it itself
+}
+
+
+class ElFinderLib {
+
+    function __construct($opts) {
+	$connector = new elFinderConnector(new elFinder($opts), true);
+	$connector->run();
+    }
+
+
+}
+/* End of file Elf_lib.php */
+/* Location: ./application/libraries/ElFinder/ElfinderLib.php */
