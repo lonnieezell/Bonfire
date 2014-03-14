d02954c03f5501b98b5ef549aa74f5d6568562ea
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-03-14 12:46:24 -0700

    Logs Cleanup: lang entry naming, CSS, use of lang
    - Renamed all logs module lang entries prefixed with 'log_' to 'logs_'
    - Added lang entries for hard-coded strings in the views as well as
    log_activity and log_message calls
    - Moved content of style attributes/tags to module css
    - Cleaned up formatting in JavaScript, changed applied styles from
    'display: block' to 'display: inherit'

--- bonfire/modules/logs/language/portuguese/logs_lang.php
+++ bonfire/modules/logs/language/portuguese/logs_lang.php
@@ -1,42 +1,44 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-/*
-	Copyright (c) 2011 Lonnie Ezell
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+/**
+ * Bonfire
+ *
+ * An open source project to allow developers to jumpstart their development of
+ * CodeIgniter applications
+ *
+ * @package   Bonfire
+ * @author    Bonfire Dev Team
+ * @copyright Copyright (c) 2011 - 2014, Bonfire Dev Team
+ * @license   http://opensource.org/licenses/MIT
+ * @link      http://cibonfire.com
+ * @since     Version 1.0
+ */
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
-*/
+/**
+ * Logs module language file (English)
+ *
+ * Localization strings used by Bonfire's Logs module
+ *
+ * @package    Bonfire\Modules\Logs\Language\English
+ * @author     Bonfire Dev Team
+ * @link       http://cibonfire.com/docs/guides
+ */
 
-$lang['log_no_logs'] 			= 'Não existem logs.';
-$lang['log_not_enabled']		= 'Logging não está activo.';
-$lang['log_the_following']		= 'Logs disponíveis:';
-$lang['log_what_0']				= '0 - Sem log';
-$lang['log_what_1']				= '1 - Msg Error(incluindo PHP Errors)';
-$lang['log_what_2']				= '2 - Msg Debug';
-$lang['log_what_3']				= '3 - Msg Information';
-$lang['log_what_4']				= '4 - Todas as Msgs';
-$lang['log_what_note']			= 'O Tipo de Log escolhido inclui o log anterior, ie, log do tipo 3 inclui 1 e 2.';
+$lang['logs_no_logs'] 			= 'Não existem logs.';
+$lang['logs_not_enabled']		= 'Logging não está activo.';
+$lang['logs_the_following']		= 'Logs disponíveis:';
+$lang['logs_what_0']			= '0 - Sem log';
+$lang['logs_what_1']			= '1 - Msg Error(incluindo PHP Errors)';
+$lang['logs_what_2']			= '2 - Msg Debug';
+$lang['logs_what_3']			= '3 - Msg Information';
+$lang['logs_what_4']			= '4 - Todas as Msgs';
+$lang['logs_what_note']			= 'O Tipo de Log escolhido inclui o log anterior, ie, log do tipo 3 inclui 1 e 2.';
 
-$lang['log_save_button']		= 'Gravar definições de LOG';
-$lang['log_delete_button']		= 'Apagar ficheiros de LOG';
+$lang['logs_save_button']		= 'Gravar definições de LOG';
+$lang['logs_delete_button']		= 'Apagar ficheiros de LOG';
 
-$lang['log_big_file_note']		= 'Atenção ao tamanho dos ficheiros de LOG quando são escolhidos valores elevados.';
-$lang['log_delete_note']		= '<h3>Apagar os ficheiros de LOG?</h3><p>Opção não reversível.</p>';
+$lang['logs_big_file_note']		= 'Atenção ao tamanho dos ficheiros de LOG quando são escolhidos valores elevados.';
+$lang['logs_delete_note']		= '<h3>Apagar os ficheiros de LOG?</h3><p>Opção não reversível.</p>';
 
-$lang['log_not_found']			= 'Log não encontrado ou vazio.';
-$lang['log_show_all_entries']	= 'Mostrar todas as entradas';
-$lang['log_show_errors']		= 'Apenas errors';
+$lang['logs_not_found']			= 'Log não encontrado ou vazio.';
+$lang['logs_show_all_entries']	= 'Mostrar todas as entradas';
+$lang['logs_show_errors']		= 'Apenas errors';
