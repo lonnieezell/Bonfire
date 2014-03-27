ad28e3406cf9f57f9fac7daa0888306748140f3d
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-03-27 09:14:17 -0700

    Removed modified config files

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'bonfire_base3';
+$db['default']['database'] = '';
 $db['default']['port']     = '3306';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'bf_';
