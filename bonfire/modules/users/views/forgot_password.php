b936d8edfab2dd8f095aba0b8eac6a357ce3e40d
boblennes <boblennes@users.noreply.github.com>
2014-04-05 08:40:32 -0500

    Update forgot_password.php

--- bonfire/modules/users/views/forgot_password.php
+++ bonfire/modules/users/views/forgot_password.php
@@ -1,6 +1,13 @@
-<div class="page-header">
-	<h1><?php echo lang('us_reset_password'); ?></h1>
-</div>
+<div class="container">
+	<div class="row">
+	<div class="col-xs-9 col-sm-8 col-md-6 col-xs-offset-1 col-sm-offset-2 col-md-offset-3" style="min-width:260px;">
+	<div class="well well-sm">
+
+
+
+<?php echo form_open($this->uri->uri_string(), array('class' => "form-horizontal", 'autocomplete' => 'off')); ?>
+     <fieldset>
+        <legend class="text-center"><?php echo lang('us_reset_password'); ?></legend>
 
 <?php if (validation_errors()) : ?>
 	<div class="alert alert-danger fade in">
@@ -8,29 +15,29 @@
 	</div>
 <?php endif; ?>
 
-<div class="alert alert-info fade in">
+	<div class="alert alert-info fade in">
 	<?php echo lang('us_reset_note'); ?>
-</div>
+	</div>
 
-<div class="row">
-	<div class="col-md-12">
-
-<?php echo form_open($this->uri->uri_string(), array('class' => "form-horizontal", 'autocomplete' => 'off')); ?>
-
+        <!-- Email input-->
 	<div class="form-group <?php echo iif( form_error('email') , 'error'); ?>">
-		<label class="control-label required" for="email"><?php echo lang('bf_email'); ?></label>
-		<div class="controls">
-			<input class="col-md-6" type="text" name="email" id="email" value="<?php echo set_value('email') ?>" />
+		<label class="col-sm-3 control-label required" for="email"><?php echo lang('bf_email'); ?></label>
+		<div class="col-sm-9">
+			<input class="form-control" type="text" name="email" id="email" value="<?php echo set_value('email') ?>" />
 		</div>
 	</div>
-
-	<div class="form-group">
-		<div class="controls">
-			<input class="btn btn-primary" type="submit" name="send" value="<?php e(lang('us_send_password')); ?>" />
+  
+        <!-- Form actions -->
+        <div class="form-group">
+        	<div class="col-md-12 text-right">
+			<input class="btn btn-primary btn-lg" type="submit" name="send" value="<?php e(lang('us_send_password')); ?>" />
 		</div>
-	</div>
+        </div>
 
-<?php echo form_close(); ?>
+		</fieldset>
+		<?php echo form_close(); ?>
 
+		</div>
+		</div>
 	</div>
 </div>
