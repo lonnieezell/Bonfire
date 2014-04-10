4742edf921d915ead12b6224aae14065754f6fa2
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-04-10 11:02:30 -0700

    Improved docs configuration, fix sidebar for app/dev docs when not using a toc file
    - The sidebar was generating links starting with
    'application/application/' or 'developer/developer/' if a toc file
    wasn't used to specify the links. The duplication was caused by a
    modification made to allow module documents to reside in either the
    application or bonfire directory. This has been resolved. See changelog
    for known issue resulting from this change.
    - Added 'docs.toc_file' config option to allow changing the name of the
    toc file (though it still has to use the same format). It will default
    to '_toc.ini' if the option is not present.
    - Added 'docs.permitted_environments' config option, which is an array
    of environments in which the docs will be displayed. If the config entry
    is not present or the array is empty, the docs module will display as
    usual. If one or more environments are listed in the array and the
    current environment is not listed, the docs module will redirect the
    user to the site's base URL.

--- bonfire/modules/docs/config/docs.php
+++ bonfire/modules/docs/config/docs.php
@@ -1,20 +1,32 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php defined('BASEPATH') || exit('No direct script access allowed');
 
 $config['docs.theme'] = 'docs';
 
 /*
- * Sets the default group (developer, application, or modules) that the
- * docs will redirect to if no area is provided.
+ * Sets the default group (developer, application, or modules) that the docs
+ * will redirect to if no area is provided.
  */
 $config['docs.default_group'] = 'developer';
 
 /*
- * Should we display the developer docs in environments other than
- * the develop environment?
+ * If true, the 'developer' docs will be displayed in environments other than
+ * the development environment.
  */
-$config['docs.show_dev_docs']   = true;
+$config['docs.show_dev_docs'] = true;
 
 /*
- * If TRUE, the 'application' specific documentation will be shown.
+ * If true, the 'application' specific documentation will be shown.
  */
-$config['docs.show_app_docs']   = true;
+$config['docs.show_app_docs'] = true;
+
+/*
+ * The name of the file containing the table of contents.
+ */
+$config['docs.toc_file'] = '_toc.ini';
+
+/*
+ * Environments in which displaying the docs is permitted. If the environment
+ * is not included in the array, an error message will be displayed and the user
+ * will be redirected to the site's base URL.
+ */
+$config['docs.permitted_environments'] = array('development', 'testing', 'production');
--- bonfire/modules/docs/controllers/docs.php
+++ bonfire/modules/docs/controllers/docs.php
@@ -2,17 +2,20 @@
 
 class Docs extends Base_Controller
 {
-    protected $docsDir = 'docs';
-    protected $docsExt = '.md';
+    protected $docsDir      = 'docs';
+    protected $docsExt      = '.md';
+    protected $docsGroup    = null;
     protected $docsTypeApp  = 'application';
     protected $docsTypeBf   = 'developer';
     protected $docsTypeMod  = 'module';
 
-    protected $docsGroup    = null;
-
     protected $ignoreFiles  = array('_404.md');
+    protected $tocFile;
 
-    //--------------------------------------------------------------------
+    private $showAppDocs;
+    private $showDevDocs;
+
+    //--------------------------------------------------------------------------
 
     /**
      * Constructor
@@ -31,10 +34,22 @@ class Docs extends Base_Controller
 
         $this->docsGroup = $this->uri->segment(2);
 
+        // Is displaying docs permitted for this environment?
+        if (config_item('docs.permitted_environments')
+            && ! in_array(ENVIRONMENT, config_item('docs.permitted_environments'))
+        ) {
+            Template::set_message(lang('docs_env_disabled'), 'error');
+            redirect();
+        }
+
         // Was a doc group provided?
         if ( ! $this->docsGroup) {
             redirect('docs/' . config_item('docs.default_group'));
         }
+
+        $this->showAppDocs = config_item('docs.show_app_docs');
+        $this->showDevDocs = config_item('docs.show_dev_docs');
+        $this->tocFile = config_item('docs.toc_file') ?: '_toc.ini';
 
         // Make sure we can still get to the search method.
         if ($this->docsGroup == 'search') {
@@ -42,16 +57,16 @@ class Docs extends Base_Controller
         }
         // Are we allowed to show developer docs in this environment?
         elseif ($this->docsGroup == 'developer'
-                && ! config_item('docs.show_dev_docs')
+                && ! $this->showDevDocs
                 && ENVIRONMENT != 'development'
-               ) {
-            if (config_item('docs.show_app_docs')) {
-                Template::set_message('Unable to load developer documentation.', 'warning');
+        ) {
+            if ($this->showAppDocs) {
+                Template::set_message(lang('docs_not_allowed_dev'), 'warning');
 
                 redirect('docs/application');
             }
 
-            show_error('Unable to load the docs you requested.');
+            show_error(lang('docs_not_allowed'));
         }
 
         Template::set_theme(config_item('docs.theme'), 'docs');
@@ -88,127 +103,133 @@ class Docs extends Base_Controller
     public function search()
     {
         $this->benchmark->mark('search_start');
-
         $this->load->library('docs/docsearch');
 
         $terms = $this->input->post('search_terms');
         if ($terms) {
             $search_folders = array();
-
-            if (config_item('docs.show_app_docs')) {
+            if ($this->showAppDocs) {
                 $search_folders[] = APPPATH . $this->docsDir;
             }
 
-            if (config_item('docs.show_dev_docs')) {
+            if ($this->showDevDocs) {
                 $search_folders[] = BFPATH . $this->docsDir;
             }
 
-            $results = $this->docsearch->search($terms, $search_folders);
-            Template::set('results', $results);
+            Template::set('results', $this->docsearch->search($terms, $search_folders));
         }
 
         $this->benchmark->mark('search_end');
-        Template::set('search_time', $this->benchmark->elapsed_time('search_start', 'search_end'));
 
+        Template::set('search_time', $this->benchmark->elapsed_time('search_start', 'search_end'));
         Template::set('search_terms', $terms);
         Template::render();
     }
 
-    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------------
     // Private Methods
-    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------------
 
     /**
      * Builds a TOC for the sidebar out of files found in the following folders:
-     *
      *      - application/docs
      *      - bonfire/docs
      *      - {module}/docs
      *
-     * @return string The rendered HTML.
+     * @return string The HTML for the sidebar.
      */
     private function build_sidebar()
     {
         $data = array();
-        $this->load->helper('directory');
-        $this->load->helper('file');
 
-        // Application Specific Docs?
+        // Get the list of docs based on the current docs group
+        // (application-specific or developer docs)
         if ($this->docsGroup == 'application') {
             $data['docs'] = $this->get_folder_files(APPPATH . $this->docsDir, $this->docsTypeApp);
-        }
-        // Developer Specific Docs?
-        elseif ($this->docsGroup == 'developer') {
+        } elseif ($this->docsGroup == 'developer') {
             $data['docs'] = $this->get_folder_files(BFPATH . $this->docsDir, $this->docsTypeBf);
         }
 
-        // Modules with Docs?
+        // Get the docs for the modules
         $data['module_docs'] = $this->get_module_docs();
+
+        // Set the remaining data for the view
         $data['docsDir'] = $this->docsDir;
         $data['docsExt'] = $this->docsExt;
 
-        $sidebar = $this->load->view('docs/_sidebar', $data, true);
-
-        return $this->post_process($sidebar);
+        return $this->post_process($this->load->view('docs/_sidebar', $data, true));
     }
 
     /**
-     * Retrieves the list of files in a folder and preps the name and
-     * filename so that it's ready for creating the HTML.
+     * Retrieves the list of files in a folder and preps the name and filename
+     * so it's ready for creating the HTML.
      *
-     * @param  String $folder The path to the folder to retrieve
+     * @param  String $folder The path to the folder to retrieve.
      * @param  String $type   The type of documentation being retrieved
-     *                        ('application', 'bonfire', or the name of the module)
+     * ('application', 'bonfire', or the name of the module).
      * @param  Array  $ignoredFolders   A list of sub-folders we should ignore.
      *
-     * @return Array  An associative array @see parse_ini_file for format details
+     * @return Array  An associative array @see parse_ini_file for format
+     * details.
      */
     private function get_folder_files($folder, $type, $ignoredFolders = array())
     {
-        $tocFile = '/_toc.ini';
-        $toc = array();
+        if ( ! is_dir($folder)) {
+            return array();
+        }
 
-        if (is_dir($folder)) {
-            // If a file called _toc.ini file exists in the folder, use it to
-            // build the links.
-            if (is_file($folder . $tocFile)) {
-                $toc = parse_ini_file($folder . $tocFile, true);
+        // If the toc file exists in the folder, use it to build the links.
+        if (is_file("{$folder}/{$this->tocFile}")) {
+            return parse_ini_file("{$folder}/{$this->tocFile}", true);
+        }
+
+        // If the toc file does not exist, build the links by listing the files
+        // in the directory (and any sub-directories)
+        $this->load->helper('directory');
+        $map = directory_map($folder);
+
+        // If directory_map can not open the directory or find any files inside
+        // the directory, return an empty array.
+        if (empty($map)) {
+            return array();
+        }
+
+        // If these docs are located in the /application/docs or /bonfire/docs
+        // directory, just use $this->docsGroup for the root.
+        // Module docs need $this->docsGroup and $type.
+        $tocRoot = $this->docsGroup;
+        if ($this->docsGroup != strtolower($type)) {
+            $tocRoot .= '/' . strtolower($type);
+        }
+
+        $toc = array();
+        foreach ($map as $new_folder => $files) {
+            // Is this a folder that should be ignored?
+            if (is_string($new_folder) && in_array($new_folder, $ignoredFolders)) {
+                continue;
             }
-            // If no toc file exists, build it from the files themselves.
-            else {
-                $map = directory_map($folder);
-                if ( ! is_array($map)) {
-                    return array();
+
+            // If $files isn't an array, then make it one so that all situations
+            // may be dealt with cleanly.
+            if ( ! is_array($files)) {
+                $files = array($files);
+            }
+
+            foreach ($files as $file) {
+                if (in_array($file, $this->ignoreFiles)) {
+                    continue;
                 }
 
-                foreach ($map as $new_folder => $files) {
-                    // Is this a folder that should be ignored?
-                    if (is_string($new_folder) && in_array($new_folder, $ignoredFolders)) {
-                        continue;
-                    }
+                // The title for the index is the passed $type. Otherwise,
+                // build the title from the file's name.
+                if (strpos($file, 'index') === false) {
+                    $title = str_replace($this->docsExt, '', $file);
+                    $title = str_replace('_', ' ', $title);
+                    $title = ucwords($title);
 
-                    // If $files isn't an array, then make it one so that all
-                    // situations may be dealt with cleanly.
-                    if ( ! is_array($files)) {
-                        $files = array($files);
-                    }
-
-                    foreach ($files as $file) {
-                        if (in_array($file, $this->ignoreFiles)) {
-                            continue;
-                        }
-
-                        // The file isn't the index, so build out a title
-                        if (strpos($file, 'index') === false) {
-                            $title = str_replace($this->docsExt, '', $file);
-                            $title = str_replace('_', ' ', $title);
-                            $title = ucwords($title);
-
-                            $toc[$this->docsGroup . '/' . strtolower($type) . '/' . $file] = $title;
-                        } else {
-                            $toc[$this->docsGroup . '/' . strtolower($type)] = $type;
-                        }
-                    }
+                    $toc["{$tocRoot}/{$file}"] = $title;
+                } else {
+                    $toc[$tocRoot] = $type;
                 }
             }
         }
