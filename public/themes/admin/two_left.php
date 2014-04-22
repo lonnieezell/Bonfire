e3c71ef815127eacf8a2638d598679aefa731282
Lonnie Ezell <lonnieje@gmail.com>
2014-04-21 22:08:30 -0500

    Minor stuff.

--- public/themes/admin/two_left.php
+++ public/themes/admin/two_left.php
@@ -11,17 +11,19 @@ Assets::add_js(array(
 echo theme_view('header');
 
 ?>
-<div class="container-fluid body">
-	<div class="row-fluid">
-		<div class="span2">
-			<?php Template::block('sidebar'); ?>
-		</div>
-		<div class="span10">
-			<?php
+<div class="body">
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span2">
+                <?php Template::block('sidebar'); ?>
+            </div>
+            <div class="span10">
+                <?php
                 echo Template::message();
                 echo isset($content) ? $content : Template::content();
-            ?>
-		</div>
-	</div>
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
 <?php echo theme_view('footer'); ?>
