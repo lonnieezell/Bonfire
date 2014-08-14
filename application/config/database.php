7911ec1d8aedfda62c73141744dedf2a0cdb43f3
mbacklas <mbacklas@users.noreply.github.com>
2014-08-14 14:17:39 -0400

    config: remove lonnies database password
    config: remove lonnies database password

--- application/config/database.php
+++ application/config/database.php
@@ -42,10 +42,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'xavier13';
-$db['default']['database'] = 'bonfire';
-$db['default']['port']     = '3306';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
+$db['default']['port']     = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'bf_';
 $db['default']['pconnect'] = TRUE;
