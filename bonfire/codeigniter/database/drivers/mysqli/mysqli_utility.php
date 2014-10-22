dfbc5062dfcc9d2a59f5afcec80dd4582ab04d04
Mat Whitney <mwhitney@mail.sdsu.edu>
2014-10-22 11:18:44 -0700

    Revert da48d5debd20967cd04733987e074b5c0189efd5

--- bonfire/codeigniter/database/drivers/mysqli/mysqli_utility.php
+++ bonfire/codeigniter/database/drivers/mysqli/mysqli_utility.php
@@ -1,4 +1,4 @@
-<?php  defined('BASEPATH') || exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * CodeIgniter
  *
@@ -22,15 +22,15 @@
  * @author		ExpressionEngine Dev Team
  * @link		http://codeigniter.com/user_guide/database/
  */
-class CI_DB_mysqli_utility extends CI_DB_utility
-{
+class CI_DB_mysqli_utility extends CI_DB_utility {
+
 	/**
 	 * List databases
 	 *
 	 * @access	private
 	 * @return	bool
 	 */
-    public function _list_databases()
+	function _list_databases()
 	{
 		return "SHOW DATABASES";
 	}
@@ -46,7 +46,7 @@ class CI_DB_mysqli_utility extends CI_DB_utility
 	 * @param	string	the table name
 	 * @return	object
 	 */
-    public function _optimize_table($table)
+	function _optimize_table($table)
 	{
 		return "OPTIMIZE TABLE ".$this->db->_escape_identifiers($table);
 	}
@@ -62,7 +62,7 @@ class CI_DB_mysqli_utility extends CI_DB_utility
 	 * @param	string	the table name
 	 * @return	object
 	 */
-    public function _repair_table($table)
+	function _repair_table($table)
 	{
 		return "REPAIR TABLE ".$this->db->_escape_identifiers($table);
 	}
@@ -76,118 +76,10 @@ class CI_DB_mysqli_utility extends CI_DB_utility
 	 * @param	array	Preferences
 	 * @return	mixed
 	 */
-    public function _backup($params = array())
+	function _backup($params = array())
 	{
 		// Currently unsupported
-        // return $this->db->display_error('db_unsuported_feature');
-
-        if (count($params) == 0) {
-            return false;
-        }
-
-        // Extract the prefs for simplicity
-        extract($params);
-
-        // Build the output
-        $output = '';
-        foreach ((array)$tables as $table) {
-            // Is the table in the "ignore" list?
-            if (in_array($table, (array)$ignore, true)) {
-                continue;
-            }
-
-            // Get the table schema
-            $query = $this->db->query("SHOW CREATE TABLE `{$this->db->database}`.`{$table}`");
-
-            // No result means the table name was invalid
-            if ($query === false) {
-                continue;
-            }
-
-            // Write out the table schema
-            $output .= "#{$newline}# TABLE STRUCTURE FOR: {$table}{$newline}#{$newline}{$newline}";
-
-            if ($add_drop == true) {
-                $output .= "DROP TABLE IF EXISTS {$table};{$newline}{$newline}";
-            }
-
-            $i = 0;
-            $result = $query->result_array();
-            foreach ($result[0] as $val) {
-                if ($i++ % 2) {
-                    $output .= "{$val};{$newline}{$newline}";
-                }
-            }
-
-            // If inserts are not needed we're done...
-            if ($add_insert == false) {
-                continue;
-            }
-
-            // Grab all the data from the current table
-            $query = $this->db->query("SELECT * FROM {$table}");
-
-            if ($query->num_rows() == 0) {
-                continue;
-            }
-
-            // Fetch the field names and determine if the field is an integer type.
-            // We use this info to decide whether to surround the data with quotes.
-
-            $i = 0;
-            $field_str = '';
-            $is_int = array();
-            while ($field = mysqli_fetch_field($query->result_id)) {
-
-                $is_int[$i] = (in_array(
-                    $field->type,
-                    // array('tinyint', 'smallint', 'mediumint', 'int', 'bigint'),
-                    array(MYSQLI_TYPE_TINY, MYSQLI_TYPE_SHORT, MYSQLI_TYPE_INT24, MYSQLI_TYPE_LONG, MYSQLI_TYPE_LONGLONG),
-                    true
-                )
-                ) ? true : false;
-
-                // Create a string of field names
-                $field_str .= "`{$field->name}`, ";
-                ++$i;
-            }
-
-            // Trim off the end comma
-            $field_str = preg_replace("/, $/", "", $field_str);
-
-            // Build the insert string
-            foreach ($query->result_array() as $row) {
-                $val_str = '';
-                $i = 0;
-                foreach ($row as $v) {
-                    // Is the value NULL?
-                    if ($v === null) {
-                        $val_str .= 'NULL';
-                    } else {
-                        // Escape the data if it's not an integer
-                        if ($is_int[$i] == false) {
-                            $val_str .= $this->db->escape($v);
-                        } else {
-                            $val_str .= $v;
-                        }
-                    }
-
-                    // Append a comma
-                    $val_str .= ', ';
-                    ++$i;
-                }
-
-                // Remove the comma at the end of the string
-                $val_str = preg_replace("/, $/", "", $val_str);
-
-                // Build the INSERT string
-                $output .= "INSERT INTO {$table} ({$field_str}) VALUES ({$val_str});{$newline}";
-            }
-
-            $output .= $newline . $newline;
-        }
-
-        return $output;
+		return $this->db->display_error('db_unsuported_feature');
 	}
 }
 
